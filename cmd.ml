9172c2c4d5969e6b640ae967a505642c270dfa5a
Rodolphe Lepigre <rodolphe.lepigre@univ-savoie.fr>
2018-01-25 14:18:28 +0100

    Forgot to add "cmd.ml"...

---
+++ cmd.ml
@@ -0,0 +1,31 @@
+(** Commands. *)
+
+open Files
+open Terms
+
+(** Representation of a toplevel command. *)
+type cmd =
+  (** Static symbol declaration. *)
+  | NewSym of string * term
+  (** Definable symbol declaration. *)
+  | NewDef of string * term
+  (** Rewriting rules declaration. *)
+  | Rules  of (Ctxt.t * def * term * term * rule) list
+  (** Quick definition. *)
+  | Defin  of string * term * term
+  (** Import an external signature. *)
+  | Import of module_path
+  (** Set debugging flags. *)
+  | Debug  of bool * string
+  (** Set the verbosity level. *)
+  | Verb   of int
+  (** Type-checking command. *)
+  | Check  of term * term
+  (** Type inference command. *)
+  | Infer  of term
+  (** Normalisation command. *)
+  | Eval   of term
+  (** Convertibility command. *)
+  | Conv   of term * term
+  (** Unimplemented command. *)
+  | Other  of string
