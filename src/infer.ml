960d37d92f643d71359d410169cee94a89424454
Frédéric Blanqui <frederic.blanqui@inria.fr>
2018-03-21 16:08:48 +0100

    some progress on infer.ml

--- src/infer.ml
+++ src/infer.ml
@@ -18,8 +18,29 @@ let pp_problem : out_channel -> problem -> unit = fun oc p ->
   if p = [] then output_string oc "∅"
   else List.pp pp_constr ", " oc p
 
-exception Not_typable
-  
+(** [unbind_tbinder c t f] returns the triple [(x,u,c')] where [(x,u)] is the
+    result of unbinding [f] and [c'] the extension of [c] with [x] mapped
+    to [t]. *)
+let unbind_tbinder (c:ctxt) (t:term) (f:tbinder) : tvar * term * ctxt =
+  let (x,u) = Bindlib.unbind mkfree f in
+  let c = if Bindlib.binder_occur f then add_tvar x t c else c in
+  x,u,c
+
+let unbind_tbinder2 (c:ctxt) (t:term) (f:tbinder) (g:tbinder)
+    : tvar * term * term * ctxt =
+  let (x,u,v) = Bindlib.unbind2 mkfree f g in
+  let c =
+    if Bindlib.binder_occur f || Bindlib.binder_occur g
+    then add_tvar x t c else c in
+  x,u,v,c
+
+type error =
+  | E_not_a_sort of term
+  | E_not_a_product of term
+  | E_not_convertible of term * term
+
+exception Error of error
+    
 let rec infer : problem -> ctxt -> term -> problem * term =
   fun p c t ->
   if !debug_type then log "INFR" "%a; %a ⊢ %a : ?" pp_problem p pp_ctxt c pp t;
@@ -36,19 +57,17 @@ let rec infer : problem -> ctxt -> term -> problem * term =
     | Prod(t,f) ->
        begin
 	 let p = check p c t Type in
-	 let (x,u) = Bindlib.unbind mkfree f in
-	 let c = if Bindlib.binder_occur f then add_tvar x t c else c in
+	 let x,u,c = unbind_tbinder c t f in 
 	 let p, typ_u = infer p c u in
 	 match typ_u with
 	 | Type | Kind -> p, typ_u
-	 | _ -> err "[%a] is not a sort...\n" pp typ_u; raise Not_typable
+	 | _ -> raise (Error (E_not_a_sort typ_u))
        end
     (* Abstraction *)
     | Abst(t,f) ->
        begin
 	 let p = check p c t Type in
-         let (x,u) = Bindlib.unbind mkfree f in
-	 let c = if Bindlib.binder_occur f then add_tvar x t c else c in
+	 let x,u,c = unbind_tbinder c t f in
 	 let p, typ_u = infer p c u in
 	 p, prod x t u
        end
@@ -59,7 +78,7 @@ let rec infer : problem -> ctxt -> term -> problem * term =
 	 let p, typ_t = infer p c t in
 	 match typ_t with
 	 | Prod(a,f) -> add_constr c a typ_u p, Bindlib.subst f u
-	 | _ -> err "[%a] is not a product...\n" pp typ_t; raise Not_typable
+	 | _ -> raise (Error (E_not_a_product typ_t))
        end
     (* Metavariable *)
     | Meta(m, ts) ->
@@ -86,11 +105,27 @@ and check : problem -> ctxt -> term -> term -> problem =
       
 and add_constr : ctxt -> term -> term -> problem -> problem =
   fun c t u p ->
-    (c,t,u)::p
+    match t, u with
+    | Symb(Sym(s1)), Symb(Sym(s2)) when s1 == s2 -> p
+    | Symb(Def(s1)), Symb(Def(s2)) when s1 == s2 -> p
+    | Meta(m1,a1), Meta(m2,a2) when m1==m2 && a1==a2 -> p
+    | Type, Type
+    | Kind, Kind -> p
+    | Vari x, Vari y ->
+       if Bindlib.eq_vars x y then p
+       else raise (Error (E_not_convertible (t,u)))
+    | Prod(a,f), Prod(b,g)
+    | Abst(a,f), Abst(b,g) ->
+       let p = add_constr c a b p in
+       let x,u,v,c = unbind_tbinder2 c a f g in
+       add_constr c u v p
+    | Wild, _
+    | _, Wild
+    | ITag _, _
+    | _, ITag _ -> assert false
+    | _, _ -> raise (Error (E_not_convertible (t,u)))
       
 let infer : ctxt -> term -> term option = fun c t ->
-  try
-    let p, typ_t = infer [] c t in
-    if p = [] then Some typ_t else None
-  with Not_typable -> None
+  let p, typ_t = infer [] c t in
+  if p = [] then Some typ_t else None
     
