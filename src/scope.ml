ba2a5277dc9e69042803b73105c52ac9f2c1663a
Frédéric Blanqui <frederic.blanqui@inria.fr>
2018-03-23 17:46:06 +0100

    changed variable names and comments in scope.ml

--- src/scope.ml
+++ src/scope.ml
@@ -144,16 +144,17 @@ let to_term : ?env:env -> Sign.t -> p_term -> term =
     and an array of variables corresponding to wildcards. *)
 type patt = def * tbox list * tvar array
 
-(** [to_patt env sign t] transforms the parsing level term [t] into a pattern.
-    Note that [t] may contain wildcards. The function also checks that it  has
-    a definable symbol as a head term, and gracefully fails otherwise. *)
+(** [to_patt env sign t] transforms the parsing level term [t] into a
+    pattern. Wildcards are replaced by fresh *variables* (not
+    metavariables). The function also checks that it has a definable
+    symbol as a head term, and gracefully fails otherwise. *)
 let to_patt : env -> Sign.t -> p_term -> patt = fun env sign t ->
   let wildcards = ref [] in
   let counter = ref 0 in
   let new_wildcard () =
-    let x = "#" ^ string_of_int !counter in
-    let x = Bindlib.new_var mkfree x in
-    incr counter; wildcards := x :: !wildcards; Bindlib.box_of_var x
+    let s = "#" ^ string_of_int !counter in
+    let v = Bindlib.new_var mkfree s in
+    incr counter; wildcards := v :: !wildcards; Bindlib.box_of_var v
   in
   let t = Bindlib.unbox (scope (Some new_wildcard) env sign t) in
   let (head, args) = get_args t in
@@ -162,63 +163,61 @@ let to_patt : env -> Sign.t -> p_term -> patt = fun env sign t ->
   | Symb(Sym(s)) -> fatal "%s is not a definable symbol...\n" s.sym_name
   | _            -> fatal "%a is not a valid pattern...\n" pp t
 
-(* NOTE wildcards are replaced by fresh variables named with a natural number,
-   prefixed with the ['#'] character.  This means that wildcards are syntactic
-   sugar for fresh variables with a single occurence (in the pattern). This is
-   useful as the corresponding variables may appear in constraints, when rules
-   are type-checked. *)
-
 (** [scope_rule sign r] scopes a parsing level reduction rule, producing every
     element that is necessary to check its type and print error messages. This
-    includes the context the symbol, the LHS / RHS as terms and the rule. *)
+    includes the context, the symbol, the LHS / RHS as terms and the rule. *)
 let scope_rule : Sign.t -> p_rule -> ctxt * def * term * term * rule =
+  (* Reminder: p_rule = (strloc * p_term option) list * p_term * p_term. *)
+  (* Reminder: rule =
+  { lhs   : (term, term list) Bindlib.mbinder (** Left-hand side (pattern). *)
+  ; rhs   : tmbinder (** Right-hand side. *)
+  ; arity : int (** Minimal number of argument for the rule to apply. *) } *)
   fun sign (xs_ty_map,t,u) ->
-    (*Reminder: p_rule = (strloc * p_term option) list * p_term * p_term*)
     let xs = List.map fst xs_ty_map in
     (* Scoping the LHS and RHS. *)
-    let fn x = x.elt, (Bindlib.new_var mkfree x.elt,_Type) (*FIXME?*) in
+    let fn x = x.elt, (Bindlib.new_var mkfree x.elt, _Type) in
+    (* FIXME? We set the type of [x] to [Type] because a type is
+       required by [scope] in its [env] argument. *)
     let env = List.map fn xs in
+    (* Reminder: env = (string * (tvar * tbox)) list. *)
     let (s, l, wcs) = to_patt env sign t in
-    (*Reminder: patt = def * tbox list * tvar array*)
+    (* Reminder: patt = def * tbox list * tvar array. *)
     let arity = List.length l in
     let l = Bindlib.box_list l in
     let u = to_tbox ~env sign u in
     (* Building the definition. *)
     let xs = Array.of_list (List.map (fun (_,(v,_)) -> v) env) in
     let lhs =
+      (* Bind rule variables and wildcard variables in the LHS. *)
       let lhs = Bindlib.bind_mvar xs l in
       let lhs = Bindlib.bind_mvar wcs lhs in
       let lhs = Bindlib.unbox lhs in
+      (* Substitute the wildcard variables by Wild. *)
       Bindlib.msubst lhs (Array.map (fun _ -> Wild) wcs)
     in
+    (* Bind the rule variables in the RHS. *)
     let rhs = Bindlib.unbox (Bindlib.bind_mvar xs u) in
     (* Constructing the typing context. *)
-    let xs = Array.append xs wcs in
+    let xs_wcs = Array.append xs wcs in
     let xs_ty_map = List.map (fun (n,a) -> (n.elt,a)) xs_ty_map in
-    let ty_map = List.map (fun (n,(x,_)) -> (x, List.assoc n xs_ty_map)) env in
-    let fn x = Bindlib.name_of x, (x, _Type) (*FIXME*)
-    in
+    let ty_map = List.map (fun (n,(v,_)) -> (v, List.assoc n xs_ty_map)) env in
+    let fn v = Bindlib.name_of v, (v, _Type) in
+    (*FIXME? We set the type of [x] to [Type]*)
     let add_var (env, ctx) x =
       let a =
         try
           match snd (List.find (fun (y,_) -> Bindlib.eq_vars y x) ty_map) with
           | None    -> raise Not_found
-          | Some(a) -> to_tbox ~env sign a (* FIXME order ? *)
+          | Some(a) -> to_tbox ~env sign a
         with Not_found ->
-          (* FIXME order (temporary hack.
-          let fn (_,x) = Bindlib.box_of_var x in
-          let vars = List.map fn vars in
-          Bindlib.unbox (_Meta (new_meta ()) (Array.of_list vars))
-          *)
-          _Meta (new_meta Type 0) (Array.map Bindlib.box_of_var xs)
-      (* We use dummy values for the context and type since they are
-         not used in the current type-checking algorithm. *)
+          _Meta (new_meta Type 0) (Array.map Bindlib.box_of_var xs_wcs)
+         (* FIXME? We set the type of [x] some metavariable applied to
+            [xs_wcs]. *)
       in
       (fn x :: env, add_tvar x (Bindlib.unbox a) ctx)
     in
-    let wcs = Array.to_list wcs in
-    let wcs = List.map fn wcs in
-    let (_, ctx) = Array.fold_left add_var (wcs, empty_ctxt) xs in
+    let wcs = List.map fn (Array.to_list wcs) in
+    let _, ctx = Array.fold_left add_var (wcs, empty_ctxt) xs_wcs in
     (* Constructing the rule. *)
     let t = add_args (Symb(Def s)) (Bindlib.unbox l) in
     let u = Bindlib.unbox u in
