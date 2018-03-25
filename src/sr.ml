53b781bd22d2183be7a17e9e39aac5c40ed6f372
Rodolphe Lepigre <rodolphe.lepigre@inria.fr>
2018-03-25 19:22:47 +0200

    One more bug fix for rule subject reduction.

--- src/sr.ml
+++ src/sr.ml
@@ -81,7 +81,7 @@ let check_rule : Sign.t -> def -> rule -> unit = fun sign s rule ->
     match unfold t with
     | Vari(x)     -> _Vari x
     | Symb(s)     -> _Symb s
-    | Abst(a,t)   -> _Abst (to_m a) (Bindlib.binder_name t) (to_m_binder t)
+    | Abst(a,t)   -> _Abst (lift a) (Bindlib.binder_name t) (to_m_binder t)
     | Appl(t,u)   -> _Appl (to_m t) (to_m u)
     | Patt(i,n,m) ->
         begin
