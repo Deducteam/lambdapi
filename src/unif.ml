164cbb92471f8fa56acebbc05ff776eaa574259f
Frédéric Blanqui <frederic.blanqui@inria.fr>
2018-03-28 15:55:56 +0200

    unif: changed log string in set_meta

--- src/unif.ml
+++ src/unif.ml
@@ -11,7 +11,7 @@ let set_meta : meta -> tmbinder -> unit = fun m v ->
   if !debug_unif then
     begin
       let (xs,v) = Bindlib.unmbind mkfree v in
-      log "inst" "%a[%a] ← %a" pp_meta m (Array.pp pp_tvar ",") xs pp v
+      log "UNIF" "%a[%a] ← %a" pp_meta m (Array.pp pp_tvar ",") xs pp v
     end;
   m.meta_value := Some(v)
 
