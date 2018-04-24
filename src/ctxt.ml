f9270807d87b6ff2ea724fdd3529ba81ba2a2598
Frédéric Blanqui <frederic.blanqui@inria.fr>
2018-04-24 09:11:26 +0200

    fixed typo in comment

--- src/ctxt.ml
+++ src/ctxt.ml
@@ -39,7 +39,7 @@ let unbind2 : t -> term -> tbinder -> tbinder -> t * term * term =
     let (x,b1,b2) = Bindlib.unbind2 mkfree b1 b2 in
     if occurs then (add x a ctx, b1, b2) else (ctx, b1, b2)
 
-(** [to_prod ctx t] builds a product type by abstractig over the context [ctx]
+(** [to_prod ctx t] builds a product type by abstracting over the context [ctx]
     in the term [t]. *)
 let to_prod : t -> term -> term = fun ctx t ->
   match ctx with
