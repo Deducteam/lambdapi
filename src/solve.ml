d80081025a8efd1c4368dcca1ca4c9fab7330bd2
Rodolphe Lepigre <rodolphe.lepigre@inria.fr>
2018-06-28 12:42:57 +0200

    Another version of unification (not used yet).

--- src/solve.ml
+++ src/solve.ml
@@ -11,9 +11,9 @@ let can_instantiate : bool ref = ref true
 (** [instantiate m ts v] check whether [m] can be instantiated to
     solve the unification problem [m[ts] = v]. Actually make the
     instantiation if it is possible. *)
-let instantiate (m:meta) (ts:term array) (v:term) : bool =
-  (!can_instantiate || internal m) && distinct_vars ts && not (occurs m v) &&
-  let bv = Bindlib.bind_mvar (to_tvars ts) (lift v) in
+let instantiate : meta -> term array -> term -> bool = fun m ar v ->
+  (!can_instantiate || internal m) && distinct_vars ar && not (occurs m v) &&
+  let bv = Bindlib.bind_mvar (to_tvars ar) (lift v) in
   Bindlib.is_closed bv && (set_meta m (Bindlib.unbox bv); true)
 
 (** [eq_vari t u] checks that [t] and [u] are both variables, and the they are
@@ -24,84 +24,88 @@ let eq_vari : term -> term -> bool = fun t u ->
   | (_      , _      ) -> false
 
 (*
-let solve_unif : (term * term) list -> (term * term) list =
+let inst = instantiate
+
+let unify : (term * term) list -> (term * term) list =
   let eq_vars = Array.for_all2 eq_vari in
-  let rec solve acc l =
+  let rec unify acc l =
     match l with
-    | []                       -> acc
-    | (t1,t2)::l when t1 == t2 -> solve acc l
-    | (t1,t2)::l               ->
+    | []         -> acc
+    | (t1,t2)::l ->
     match (unfold t1, unfold t2) with
     (* Leaves. *)
     | (Type       , Type       )
-    | (Kind       , Kind       )                          -> solve acc l
-    | (Vari(x)    , Vari(y)    ) when Bindlib.eq_vars x y -> solve acc l
-    | (Symb(s)    , Symb(r)    ) when s == r              -> solve acc l
+    | (Kind       , Kind       )                          -> unify acc l
+    | (Vari(x)    , Vari(y)    ) when Bindlib.eq_vars x y -> unify acc l
+    | (Symb(s)    , Symb(r)    ) when s == r              -> unify acc l
     (* Binders (abstractions and products). *)
     | (Prod(a1,b1), Prod(a2,b2))
-    | (Abst(a1,b1), Abst(a2,b2)) -> solve acc ((a1,a2)::(unbind2 b1 b2)::l)
-    (* Metavariables (reflexivity). *)
-    | (Meta(m1,a1), Meta(m2,a2)) when m1 == m2 && eq_vars a1 a2 -> solve acc l
-    | (Meta(m,ar) , v          ) when instantiate m ar v -> solve acc l (* FIXME recompute. *)
-    | (v          , Meta(m,ar) ) when instantiate m ar v -> solve acc l (* FIXME recompute. *)
-    (* May need normalization. *)
-    | (Symb(s)    , _          ) when not (Sign.is_const s) -> solve ((t1,t2)::acc) l
-    | (_          , Symb(s)    ) when not (Sign.is_const s) -> solve ((t1,t2)::acc) l
-    | (Meta(_,_)  , _          )
-    | (_          , Meta(_,_)  )
+    | (Abst(a1,b1), Abst(a2,b2)) -> unify acc ((a1,a2)::(unbind2 b1 b2)::l)
+    (* Metavariable (reflexivity). *)
+    | (Meta(m1,a1), Meta(m2,a2)) when m1 == m2 && eq_vars a1 a2 -> unify acc l
+    (* Metavariable (instantiation). *)
+    | (Meta(m,ar) , t          )
+    | (t          , Meta(m,ar) ) ->
+        if inst m ar t then unify acc l else
+        let t = Eval.whnf t in
+        if inst m ar t then unify acc l else
+        let t = Eval.snf t in
+        if inst m ar t then unify acc l else
+        unify ((Meta(m,ar), t)::acc) l
+    (* May need normalization (one side not in WHNF). *)
+    | (Symb(s)    , _          ) when not (Sign.is_const s) ->
+        unify_whnf acc l t1 t2
+    | (_          , Symb(s)    ) when not (Sign.is_const s) ->
+        unify_whnf acc l t1 t2
     | (Appl(_,_)  , _          )
-    | (_          , Appl(_,_)  ) -> solve ((t1,t2)::acc) l
+    | (_          , Appl(_,_)  ) ->
+        unify_whnf acc l t1 t2
     (* Definitely not convertible. *)
     | (_          , _          ) ->
         fatal_no_pos "[%a] and [%a] are not convertible." pp t1 pp t2
-  in
-  solve []
-
-let solve_whnf : (term * term) list -> (term * term) list =
-  let rec solve acc l =
-    match l with
-    | []                       -> acc
-    | (t1,t2)::l when t1 == t2 -> solve acc l
-    | (t1,t2)::l               ->
+  and unify_whnf acc l t1 t2 =
     let (h1, ts1) = Eval.whnf_stk t1 [] in
     let (h2, ts2) = Eval.whnf_stk t2 [] in
+    if ts1 = [] && ts2 = [] then unify acc ((h1,h2)::l) else
     match (h1, h2) with
-    (* Cases where [ts1] and [ts2] due to typing / WHNF. *)
-    | (Type       , Type       )
-    | (Kind       , Kind       ) -> solve acc l
-    | (Prod(a1,b1), Prod(a2,b2))
-    | (Abst(a1,b1), Abst(a2,b2)) ->
-        solve acc ((solve_unif [(a1,a2); unbind2 b1 b2]) @ acc)
-    | (Vari(x)    , Vari(y)    ) when Bindlib.eq_vars x y && List.same_length ts1 ts2 ->
-        let u = List.fold_left2 (fun l t1 t2 -> (!t1,!t2)::l) [] ts1 ts2 in
-        solve acc (solve_unif u @ l)
-    | (Symb(s1)   , Symb(s2)   ) when s1 == s2 && ts1 = [] && ts2 = [] ->
-        solve acc l
-    | (Symb(s1)   , Symb(s2)   ) when s1 == s2 && Sign.is_const s1 && List.same_length ts1 ts2 ->
-        let u = List.fold_left2 (fun l t1 t2 -> (!t1,!t2)::l) [] ts1 ts2 in
-        solve acc (solve_unif u @ l)
-    | (Meta(m1,a1), Meta(m2,a2)) when m1 == m2 && ts1 = [] && ts2 = [] && Array.for_all2 eq_vari a1 a2 ->
-        solve acc l
-    | (Meta(m,ts) , _          ) when ts1 = [] && instantiate m ts t2 ->
-        solve acc l (* FIXME recompute *)
-    | (_          , Meta(m,ts) ) when ts2 = [] && instantiate m ts t1 ->
-        solve acc l (* FIXME recompute *)
-
-    | (Meta(_,_)  , _          )
-    | (_          , Meta(_,_)  ) -> solve ((t1,t2)::acc) l
+    (* Metavariable with no arguments. *)
+    | (Meta(_,_)  , _          ) when ts1 = [] ->
+        unify acc ((h1, Eval.to_term h2 ts2)::l)
+    | (_          , Meta(_,_)  ) when ts2 = [] ->
+        unify acc ((h2, Eval.to_term h1 ts1)::l)
+    (* Non-necessarily injective head. *)
+    | (Vari(x)    , Vari(y)    ) when Bindlib.eq_vars x y ->
+        begin
+          try
+            let fn l t1 t2 = (!t1,!t2)::l in
+            unify acc (List.fold_left2 fn l ts1 ts2)
+          with Invalid_argument(_) ->
+            let t1 = Eval.to_term h1 ts1 in
+            let t2 = Eval.to_term h2 ts2 in
+            fatal_no_pos "[%a] and [%a] are not convertible." pp t1 pp t2
+        end
+    | (Symb(s1)   , Symb(s2)   ) when s1 == s2 && Sign.is_const s1 ->
+        begin
+          try
+            let fn l t1 t2 = (!t1,!t2)::l in
+            unify acc (List.fold_left2 fn l ts1 ts2)
+          with Invalid_argument(_) ->
+            let t1 = Eval.to_term h1 ts1 in
+            let t2 = Eval.to_term h2 ts2 in
+            fatal_no_pos "[%a] and [%a] are not convertible." pp t1 pp t2
+        end
     | (Symb(s)    , _          ) when not (Sign.is_const s) ->
-        if Eval.eq_modulo t1 t2 then solve acc l
-        else solve ((t1,t2)::acc) l
+        if Eval.eq_modulo t1 t2 then unify acc l
+        else unify ((Eval.to_term h1 ts1, Eval.to_term h2 ts2)::acc) l
     | (_          , Symb(s)    ) when not (Sign.is_const s) ->
-        if Eval.eq_modulo t1 t2 then solve acc l
-        else solve ((t1,t2)::acc) l
+        if Eval.eq_modulo t1 t2 then unify acc l
+        else unify ((Eval.to_term h1 ts1, Eval.to_term h2 ts2)::acc) l
     (* Definitely not convertible. *)
     | (_          , _          ) ->
         fatal_no_pos "[%a] and [%a] are not convertible." pp t1 pp t2
   in
-  solve []
+  unify []
 *)
-
 
 (** Representation of a set of problems. *)
 type problems =
@@ -351,10 +355,18 @@ let msg (a,b) =
     log "type" "Cannot solve constraint [%a] ~ [%a]\n" pp a pp b
 
 (** [has_type c t u] returns [true] iff [t] has type [u] in context [c]. *)
-let has_type (c:Ctxt.t) (t:term) (a:term) : bool =
+let has_type : Ctxt.t -> term -> term -> bool = fun c t a ->
   if !debug_type then log "type" "check [%a] [%a]" pp t pp a;
   let unif_problems = ref [] in
-  let conv_fun a b = unif_problems := (a,b) :: !unif_problems in
+  let conv_fun a b =
+    if not (Eval.eq_modulo a b) then
+      begin
+        if !debug_type then log "type" "  requires [%a] ~ [%a]" pp a pp b;
+        unif_problems := (a,b) :: !unif_problems;
+        wrn "Hello!\n"
+      end
+    else if !debug_type then log "type" "  immediate"
+  in
   Typing.(check {conv_fun} c t a);
   let problems = {no_problems with unif_problems = !unif_problems} in
   match solve true default_strategy problems with
