7458450eab03d8bf5001c52e5ce5e19c4cccc919
Rodolphe Lepigre <rodolphe.lepigre@inria.fr>
2018-06-30 09:57:59 +0200

    Faster parsing (positions computed when printing).

--- src/pos.ml
+++ src/pos.ml
@@ -3,14 +3,21 @@
 
 
 (** Type of a position, corresponding to a continuous range of characters in a
-    (utf8 encoded) source file. For denoting a zero-length position is to have
-    [end_col] equal to [start_col - 1]. *)
+    source file. Elements of this type are constructed by [Earley],  using the
+    following [locate] function. *)
 type pos =
-  { fname       : string option (** File name for the position.       *)
-  ; start_line  : int (** Line number of the starting point.          *)
-  ; start_col   : int (** Column number (utf8) of the starting point. *)
-  ; end_line    : int (** Line number of the ending point.            *)
-  ; end_col     : int (** Column number (utf8) of the ending point.   *) }
+  { start_buf : Input.buffer (** Buffer for the start position.   *)
+  ; start_pos : int          (** Position for the start position. *)
+  ; end_buf   : Input.buffer (** Buffer for the end position.     *)
+  ; end_pos   : int          (** Position for the end position.   *) }
+
+(** [locate buf1 pos1 buf2 pos2] builds a [pos] structure,  given two [Earley]
+    input buffers. This function is used by Earley to generate the position of
+    elements during parsing.
+    @see <https://github.com/rlepigre/earley/> Earley *)
+let locate : Input.buffer -> int -> Input.buffer -> int -> pos =
+  fun buf1 pos1 buf2 pos2 ->
+    {start_buf = buf1; start_pos = pos1; end_buf = buf2; end_pos = pos2}
 
 (** Convenient short name for an optional position. *)
 type popt = pos option
@@ -39,40 +46,22 @@ let none : 'a -> 'a loc =
 
 (** [to_string pos] transforms [pos] into a readable string. *)
 let to_string : pos -> string = fun p ->
+  let open Printf in
   let fname =
-    match p.fname with
-    | None       -> ""
-    | Some fname -> Printf.sprintf "%s, " fname
+    match Input.filename p.start_buf with
+    | ""    -> ""
+    | fname -> fname ^ ", "
   in
-  if p.start_line <> p.end_line then
-    Printf.sprintf "%s%d:%d-%d:%d" fname
-      p.start_line p.start_col p.end_line p.end_col
-  else if p.start_col = p.end_col then
-    Printf.sprintf "%s%d:%d" fname p.start_line p.start_col
-  else
-    Printf.sprintf "%s%d:%d-%d" fname p.start_line p.start_col p.end_col
+  let line1 = Input.line_num p.start_buf in
+  let col1  = Input.utf8_col_num p.start_buf p.start_pos in
+  let line2 = Input.line_num p.end_buf in
+  let col2  = Input.utf8_col_num p.end_buf p.end_pos in
+  if line1 <> line2 then sprintf "%s%d:%d-%d:%d" fname line1 col1 line2 col2
+  else if col1 = col2 then sprintf "%s%d:%d" fname line1 col1
+  else sprintf "%s%d:%d-%d" fname line1 col1 col2
 
 (** [print oc pos] prints the optional position [pos] to [oc]. *)
 let print : Format.formatter -> pos option -> unit = fun ch p ->
   match p with
   | None   -> Format.pp_print_string ch "unknown location"
   | Some p -> Format.pp_print_string ch (to_string p)
-
-open Input
-
-(** [locate buf1 pos1 buf2 pos2] builds a position structure, given two Earley
-    input buffers. This function is used by Earley to generate the position of
-    elements during parsing.
-    @see <https://github.com/rlepigre/earley/> Earley *)
-let locate : buffer -> int -> buffer -> int -> pos =
-  fun buf1 pos1 buf2 pos2 ->
-    let fname =
-      match filename buf1 with
-      | ""    -> None
-      | fname -> Some fname
-    in
-    let start_line = line_num buf1 in
-    let end_line   = line_num buf2 in
-    let start_col  = utf8_col_num buf1 pos1 in
-    let end_col    = utf8_col_num buf2 pos2 in
-    { fname ; start_line ; start_col ; end_line ; end_col }
