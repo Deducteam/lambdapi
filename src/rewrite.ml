c019d209b8db06da863ba26d86545b266c5cf6ef
arisPapadop <ap7415@ic.ac.uk>
2018-07-16 14:08:02 +0200

    Started working on match_subst. Not much is wokring though.

--- src/rewrite.ml
+++ src/rewrite.ml
@@ -7,7 +7,10 @@ open Proofs
 
 (****************************************************************************)
 
-(* Trying things out. *)
+(** [find_term] just expands the AST of a given term in a very trivial way. It
+    is only here for me to try some stuff out. *)
+
+(* TODO: Remove this when testing syntax stuff is finished. *)
 let rec find_term = fun t ->
   match t with
   | Vari _           -> print_string "Vari "
@@ -29,6 +32,8 @@ let rec find_term = fun t ->
 let fail_to_match = fun n ->
   match n with
   | 0 -> fatal_no_pos "Can only use rewrite with equalities."
+  | 1 -> fatal_no_pos "Cannot rewrite under products."
+  | 2 -> fatal_no_pos "Cannot rewrite under abstractions."
   | _ -> fatal_no_pos "Incorrect error code."
 
 (* Minimal unwrapper. *)
@@ -92,6 +97,34 @@ let instances : term -> term -> term list = fun t s ->
       | Meta _       ->  if term_match cur s then cur::acc else acc
       | _            -> acc
     in instances_aux t []
+
+(** [match_subst] is given the type of the current goal, the left hand side of
+    the lemma rewrite was called on and some term it returns the type of the
+    new goal, where all occurrences of left are replaced by the new term.
+    Use: This will be called with a fresh Vari term to build a product term,
+    using the bindlib lilfting functions. *)
+let match_subst : term -> term -> term -> term = fun g_type l t ->
+  let rec matching_aux : term -> term = fun cur ->
+    if term_match cur p then s else match cur with
+    | Vari x       -> if term_match cur s then cur::acc else acc
+    | Type | Kind  -> acc
+    | Symb sym     -> if term_match cur s then cur::acc else acc
+    | Prod(t1, _)  -> fail_to_match 1  (* For now we do not "mess" with any *)
+    | Abst(t1, _)  -> fail_to_match 2  (* terms conaining Prod or Abst.     *)
+    | Appl(t1, t2) ->
+      let rest = instances_aux t2 (instances_aux t1 acc) in
+      if term_match cur s then cur :: rest else rest
+    | Meta _       ->  if term_match cur s then cur::acc else acc
+    | _            -> acc
+  in matching_aux l
+    (********OLD STUFF*********)
+    | BinOp (t1, op, t2) ->
+        let t1' = matching_aux t1 and t2' = matching_aux t2 in
+      BinOp(t1', op, t2')
+    | Var x -> if p = Var x || p = Any then s else cur
+    | Lit i -> if p = Any then s else cur
+    | _ -> cur
+in matching_aux t
 
 (****************************************************************************)
 (* TODO:
