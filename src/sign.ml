6a081e85cef811c9a5bbfe646cbbf12634aae471
Rodolphe Lepigre <rodolphe.lepigre@inria.fr>
2018-10-06 13:34:18 +0200

    Dependency transitive closure in [src/sign.ml].

--- src/sign.ml
+++ src/sign.ml
@@ -201,3 +201,23 @@ let add_rule : t -> sym -> pp_hint -> rule -> unit = fun sign sym hint r ->
       with Not_found -> assert false
     in
     sign.deps := PathMap.add sym.sym_path ((sym.sym_name,r)::m) !(sign.deps)
+
+(** [dependencies sign] returns an association list containing (the transitive
+    closure of) the dependencies of the signature [sign].  Note that the order
+    of the list gives one possible loading order for the signatures. Note also
+    that [sign] itself appears at the end of the list. *)
+let rec dependencies : t -> (module_path * t) list = fun sign ->
+  (* Recursively compute dependencies for the immediate dependencies. *)
+  let fn p _ l = dependencies (PathMap.find p !loaded) :: l in
+  let deps = PathMap.fold fn !(sign.deps) [[(sign.path, sign)]] in
+  (* Minimize and put everything together. *)
+  let rec minimize acc deps =
+    let not_here (p,_) =
+      let has_p = List.exists (fun (q,_) -> p = q) in
+      not (List.exists has_p acc)
+    in
+    match deps with
+    | []      -> List.rev acc
+    | d::deps -> minimize ((List.filter not_here d) :: acc) deps
+  in
+  List.concat (minimize [] deps)
