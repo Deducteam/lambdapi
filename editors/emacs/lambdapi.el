2ddd28141b9107291375a6730f53ee6854e61e30
Rodolphe Lepigre <rodolphe.lepigre@inria.fr>
2018-10-11 01:29:58 +0200

    Emacs mode has support for both syntaxes.

--- editors/emacs/lambdapi.el
+++ editors/emacs/lambdapi.el
@@ -1,5 +1,6 @@
 ;;; This is the LambdaPi emacs mode
 (provide 'lambdapi-mode)
+(provide 'lambdapi-legacy-mode)
 
 ;; Syntax table
 (defvar lambdapi-mode-syntax-table nil "Syntax table for LambdaPi.")
@@ -33,7 +34,7 @@
   ("\\\\" ?λ) ("\\lambda" ?λ)
   (":="   ?≔) ("\\defeq"  ?≔))
 
-;; Main function creating the mode
+;; Main function creating the mode (lambdapi)
 (define-derived-mode lambdapi-mode fundamental-mode "LambdaPi"
   "A mode for editing LambdaPi files."
   (set-syntax-table lambdapi-mode-syntax-table)
@@ -43,12 +44,47 @@
   (setq-default indent-tabs-mode nil)
   (set-input-method "LambdaPi"))
 
+;; Syntax table (legacy syntax)
+(defvar lambdapi-mode-legacy-syntax-table nil "Syntax table for LambdaPi.")
+
+(setq lambdapi-mode-legacy-syntax-table
+  (let ((synTable (make-syntax-table)))
+    (modify-syntax-entry ?\( ". 1" synTable)
+    (modify-syntax-entry ?\) ". 4" synTable)
+    (modify-syntax-entry ?; ". 23" synTable)
+    synTable))
+
+;; Keywords (legacy syntax)
+(defconst lambdapi-legacy-font-lock-keywords
+  (list (cons (concat "\\<" (regexp-opt '(
+           "def" "thm" "inj"
+        )) "\\>") 'font-lock-keyword-face)
+        (cons (concat "#" (regexp-opt '(
+           "REQUIRE" "EVAL" "INFER" "ASSERT" "ASSERTNOT"
+        )) "\\>") 'font-lock-preprocessor-face))
+  "Keyword highlighting for the LambdaPi mode (legacy syntax).")
+
+
+;; Main function creating the mode (legacy syntax)
+(define-derived-mode lambdapi-legacy-mode fundamental-mode "LambdaPi (legacy)"
+  "A mode for editing LambdaPi files (in legacy syntax)."
+  (set-syntax-table lambdapi-mode-legacy-syntax-table)
+  (setq-local font-lock-defaults '(lambdapi-legacy-font-lock-keywords))
+  (setq-local comment-start "(;")
+  (setq-local comment-end ";)")
+  (setq-default indent-tabs-mode nil))
+
 ;; LSP mode
 (require 'eglot)
-(add-to-list 'eglot-server-programs '(lambdapi-mode . ("lp-lsp" "--std")))
+(add-to-list 'eglot-server-programs
+  '(lambdapi-mode . ("lp-lsp" "--std")))
+(add-to-list 'eglot-server-programs
+  '(lambdapi-legacy-mode . ("lp-lsp" "--std")))
 (add-hook 'lambdapi-mode-hook 'eglot-ensure)
+(add-hook 'lambdapi-legacy-mode-hook 'eglot-ensure)
 
 ;; Register mode the the ".lp" extension
 (add-to-list 'auto-mode-alist '("\\.lp\\'" . lambdapi-mode))
+(add-to-list 'auto-mode-alist '("\\.dk\\'" . lambdapi-legacy-mode))
 
 ;;; lambdapi.el ends here
