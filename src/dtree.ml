3dcc8ff266308d2c848da0e3a1e3b4bd08d199e0
Gabriel HONDET <hondet@clac.lsv.ens-cachan.fr>
2019-02-15 17:38:51 +0100

    (+) signature checking

--- src/dtree.ml
+++ src/dtree.ml
@@ -1,5 +1,6 @@
 open Terms
 open Timed
+open Extra
 
 (* TODO
    define more clearly what is a constructor and what is not, i.e.  clarify
@@ -7,30 +8,50 @@ open Timed
    a branch).
 *)
 
+(** Type of the leaves of the tree.  See {!file:terms.ml}, {!recfield:rhs}. *)
+type action = (term_env, term) Bindlib.mbinder
+
 (** Tree type.  Leaves are the targets of the rewriting, i.e. when a leaf is
-    reached, the matched pattern is rewrote to the the leaf.  The
+    reached,  the  matched  pattern  is  rewrote  to  the  the  leaf.  The
     {!const:Node}[(i, t)] constructor allows to perform a switch among the
     subtrees in [t] while the [i] option allows to choose on which term of the
-    stack the matching is to be done. *)
-type t = Leaf of (term_env, term) Bindlib.mbinder
+    stack the matching is to be done.  {!const:Fail} is a matching failure. *)
+type t = Leaf of action
        | Node of int option * t list
+       | Fail
 
 (** [is_sig s] returns whether the list of symbols [s] contains a
     signature. *)
-let is_sig : term list -> bool = fun tes -> false
+let is_sig : term list -> bool = fun r ->
+  let symofterms = List.map
+      (fun s -> match s with Symb(s', _) -> s' | _ -> assert false)
+      (List.filter (fun x -> match x with
+      | Symb(_, _) -> true | _ -> false) r) in
+  let op_sym = List.map_find (fun x -> match x with
+      | Symb(s, _) -> Some s
+      | _          -> None) r in
+  let a_sym = match op_sym with
+    | Some s -> s
+    | None -> failwith "no symbol found" in
+  let sign = Files.PathMap.find a_sym.sym_path !Sign.loaded in
+  let sign_sym = StrMap.map fst !(sign.sign_symbols) in
+  let count = StrMap.fold (fun _ s acc ->
+      if List.mem s symofterms then succ acc else acc) sign_sym 0 in
+  count = StrMap.cardinal sign_sym
 
 module Pattmat =
 struct
-  (** Type of a matrix of patterns *)
-  type t = term list array
+  (** Type of a matrix of patterns.  The type is changed (from rule list) as the
+      matrix is made to be shrunk; keeping the rule list type would be
+      deceitful semantically. *)
+  type t = (term list * action) list
 
   (** Type used to describe a column of a matrix. *)
   type column = term list
 
   (** [make r] creates  the  initial pattern  matrix from a  list of rewriting
       rules. *)
-  let make : rule list -> t = fun rs ->
-    [| [ Patt(None, "", [||]) ] |]
+  let make : rule list -> t = List.map (fun r -> r.lhs, r.rhs)
 
   (** [specialize t m] specializes the matrix [m] when matching against term
       [t]. *)
@@ -54,26 +75,31 @@ struct
   let filter_on_cons : t -> int list = fun m -> []
 
   (** [select m i] keeps the columns of [m] whose index are in i. *)
-  let select : t -> int list -> t = fun m indexes -> [| |]
+  let select : t -> int list -> t = fun m indexes -> m
 end
 
-(** [grow m] creates a pattern matching tree from the pattern matching
-    matrix [m]. *)
+(** [grow m]  creates a pattern matching tree from the pattern matching matrix
+    [m].  Counterpart of the compilation_scheme in Maranget's article. *)
 let rec grow : Pattmat.t -> t = fun m ->
-  let cols = Pattmat.filter_on_cons m in
-  let selected = Pattmat.pick_best (Pattmat.select m cols) in
-  let syms = List.filter (fun x -> match x with
-        Symb(_, _) -> true
-      | _          -> false) selected in
-  let rec specmats : term list -> Pattmat.t list = function
-    | []    -> []
-    | s::ss -> Pattmat.specialize s m::specmats ss in
-  let ms = specmats syms in
-  let children = List.map grow (ms @ if is_sig syms
-                                     then []
-                                     else [Pattmat.default m]) in
-  Node(None, children)
+  if List.length m = 0 then Fail else
+    let fline = fst @@ List.hd m in
+    if fline = List.map (fun _ -> Wild) fline then
+      Leaf(snd @@ List.hd m) else
+    let cols = Pattmat.filter_on_cons m in
+    let selected = Pattmat.pick_best (Pattmat.select m cols) in
+    let syms = List.filter (fun x -> match x with
+        | Symb(_, _) -> true
+        | _          -> false) selected in
+    let rec specmats : term list -> Pattmat.t list = function
+      | []    -> []
+      | s::ss -> Pattmat.specialize s m::specmats ss in
+    let ms = specmats syms in
+    let children = List.map grow (ms @ if is_sig syms
+                                  then []
+                                  else [Pattmat.default m]) in
+    Node(None, children)
 (* TODO
-   + termination? leaves are never created,
-   + what about vars and metavars
+   + what about vars and metavars -> this seem to concern
+     the tree walk and matching the pattern, not the growing of
+     the tree
 *)
