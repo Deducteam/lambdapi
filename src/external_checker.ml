7b95afb8069164fdb239c5b931639594471c9f7d
Guillaume GENESTIER <genestier@lsv.fr>
2019-02-22 16:41:08 +0100

    Consistency in comment

--- src/external_checker.ml
+++ src/external_checker.ml
@@ -15,7 +15,7 @@ open Console
     assumed that [cmd] corresponds to a command that outputs either ["YES"],
     ["NO"] or ["MAYBE"] as the first line of its standard output. The
     exception [Fatal] may be raised if [cmd] does not behave as expected.
-    [checked_property] is for printing purpose *)
+    [property] is for printing purpose *)
 let check : string -> Sign.t -> Sign.t pp -> logger -> string -> bool option =
   fun cmd sign translator logging property ->
   (* Run the command. *)
