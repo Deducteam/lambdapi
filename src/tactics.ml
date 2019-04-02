8902fec18988cfca2fedcfc488de01878101f56f
Frédéric Blanqui <frederic.blanqui@inria.fr>
2019-04-02 14:17:41 +0200

    use proof environment in queries

--- src/tactics.ml
+++ src/tactics.ml
@@ -14,29 +14,32 @@ let log_tact = log_tact.logger
 
 (** [handle_query ss ps q] *)
 let handle_query : sig_state -> Proof.t option -> p_query -> unit =
-  fun ss _ps q ->
-  let scope_basic ss t = fst (scope_term StrMap.empty ss Env.empty t) in
+  fun ss ps q ->
+  let env =
+    match ps with
+    | None -> Env.empty
+    | Some ps -> fst (Proof.focus_goal ps)
+  in
+  let scope t = fst (scope_term StrMap.empty ss env t) in
   match q.elt with
   | P_query_assert(must_fail, asrt)  ->
      let result =
        match asrt with
-       | P_assert_typing(t,a) ->
-          let t = scope_basic ss t in
-          let a = scope_basic ss a in
-          Typing.sort_type ss.builtins Ctxt.empty a;
-          (try Typing.check ss.builtins Ctxt.empty t a with _ -> false)
-       | P_assert_conv(a,b)   ->
-          let t = scope_basic ss a in
-          let u = scope_basic ss b in
-          match (Typing.infer ss.builtins Ctxt.empty t,
-                 Typing.infer ss.builtins Ctxt.empty u) with
+       | P_assert_typing(pt,pa) ->
+          let t = scope pt and a = scope pa and ctxt = Ctxt.of_env env in
+          Typing.sort_type ss.builtins ctxt a;
+          (try Typing.check ss.builtins ctxt t a with _ -> false)
+       | P_assert_conv(pt,pu)   ->
+          let t = scope pt and u = scope pu and ctxt = Ctxt.of_env env in
+          match (Typing.infer ss.builtins ctxt t,
+                 Typing.infer ss.builtins ctxt u) with
           | (Some(a), Some(b)) ->
              if Eval.eq_modulo a b then Eval.eq_modulo t u else
                fatal q.pos "Infered types not convertible (in assertion)."
           | (None   , _      ) ->
-             fatal a.pos "Type cannot be infered (in assertion)."
+             fatal pt.pos "Type cannot be infered (in assertion)."
           | (_      , None   ) ->
-             fatal b.pos "Type cannot be infered (in assertion)."
+             fatal pu.pos "Type cannot be infered (in assertion)."
      in
      if result = must_fail then fatal q.pos "Assertion failed."
   | P_query_debug(e,s)     ->
@@ -51,24 +54,22 @@ let handle_query : sig_state -> Proof.t option -> p_query -> unit =
        try Console.set_flag id b with Not_found ->
          wrn q.pos "Unknown flag \"%s\"." id
      end
-  | P_query_infer(t, cfg)            ->
+  | P_query_infer(pt, cfg)            ->
      (* Infer the type of [t]. *)
-     let t_pos = t.pos in
-     let t = scope_basic ss t in
+     let t = scope pt in
      let a =
-       match Typing.infer ss.builtins Ctxt.empty t with
+       match Typing.infer ss.builtins (Ctxt.of_env env) t with
        | Some(a) -> Eval.eval cfg a
-       | None    -> fatal t_pos "Cannot infer the type of [%a]." pp t
+       | None    -> fatal pt.pos "Cannot infer the type of [%a]." pp t
      in
      out 3 "(infr) %a : %a\n" pp t pp a
-  | P_query_normalize(t, cfg)        ->
+  | P_query_normalize(pt, cfg)        ->
      (* Infer a type for [t], and evaluate [t]. *)
-     let t_pos = t.pos in
-     let t = scope_basic ss t in
+     let t = scope pt in
      let v =
-       match Typing.infer ss.builtins Ctxt.empty t with
+       match Typing.infer ss.builtins (Ctxt.of_env env) t with
        | Some(_) -> Eval.eval cfg t
-       | None    -> fatal t_pos "Cannot infer the type of [%a]." pp t
+       | None    -> fatal pt.pos "Cannot infer the type of [%a]." pp t
      in
      out 3 "(eval) %a\n" pp v
 
