4c82ac16929c6eb1baf71db64882472732675bfb
Gabriel Hondet <gabrielhondet@gmail.com>
2019-04-05 11:18:11 +0200

    (+) use profiling

--- examples/proto_bench/run.bash
+++ examples/proto_bench/run.bash
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-TIME_PATCH="timing.patch"
+TIME_PATCH="timing.diff"
+PROF_PATCH="gprof.diff"
 
 # Patch to add timing
 echo "Patching code"
 cd ../..
 git apply "examples/proto_bench/$TIME_PATCH"
+git apply "examples/proto_bench/$PROF_PATCH"
 
 echo "Compiling"
 make
@@ -14,7 +16,10 @@ lp="../../_build/install/default/bin/lambdapi"
 for t in *.lp; do
     echo "Checking $t..."
     exec "$lp" "$t"
+    echo "Profiling"
+    gprof "$lp" gmon.out > "$t"'.prof'
 done
 cd "../.."
 git apply --reverse "examples/proto_bench/$TIME_PATCH"
+git apply --reverse "examples/proto_bench/$PROF_PATCH"
 
