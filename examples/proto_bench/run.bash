743cab29eca4f666401a3444c402910ce9d0a345
Gabriel Hondet <gabrielhondet@gmail.com>
2019-04-05 13:51:15 +0200

    (*) fixed run bash

--- examples/proto_bench/run.bash
+++ examples/proto_bench/run.bash
@@ -12,14 +12,16 @@ git apply "examples/proto_bench/$PROF_PATCH"
 echo "Compiling"
 make
 cd "examples/proto_bench"
+
 lp="../../_build/install/default/bin/lambdapi"
 for t in *.lp; do
     echo "Checking $t..."
-    exec "$lp" "$t"
+    $lp "$t"
     echo "Profiling"
     gprof "$lp" gmon.out > "$t"'.prof'
 done
 cd "../.."
+echo "Reverting patches"
 git apply --reverse "examples/proto_bench/$TIME_PATCH"
 git apply --reverse "examples/proto_bench/$PROF_PATCH"
 
