dc972a227df1cf225c5d1be08a310bc6216a4cfc
Gabriel Hondet <gabrielhondet@gmail.com>
2019-04-25 15:31:18 +0200

    (*) fail if tree building fail while drawing

--- src/sign.ml
+++ src/sign.ml
@@ -295,7 +295,7 @@ let add_rules : t -> (sym * pp_hint * rule loc) list -> unit = fun sign rs ->
        symb.sym_tree := (capacity, tree) ;
        if Pervasives.(!write_trees)
        then begin try Dtree.to_dot symb.sym_name (Lazy.force tree)
-         with _ -> () end
+         with e -> raise e end
     | _     -> () in
   rs |> List.map (fun (e, _, _) -> e) |> List.sort_uniq Basics.sym_cmp
      |> List.iter build_tree
