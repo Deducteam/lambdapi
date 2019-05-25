ce6863ffa115598a23944c02e0622304e620fdb0
gabrielhdt <gabrielhondet@gmail.com>
2019-05-25 16:12:40 +0200

    (~) don't use unix, corrections, asserts

--- tools/generate_tests.ml
+++ tools/generate_tests.ml
@@ -1,8 +1,6 @@
 #!/usr/bin/env ocaml
-#use "topfind"
-#require "unix"
 
-open Unix
+module F = Filename
 module P = Printf
 
 let p_thump = ref 20
@@ -24,25 +22,27 @@ let natural_prelude = "require open tests.OK.nat"
 (** [large_thump n] builds a tree of depth one but with many
     branches. *)
 let thump n =
-  let ochan = open_out (testdir ^ "large_thump.lp") in
+  let fname = F.concat testdir "thump.lp" in
+  let ochan = open_out fname in
   P.fprintf ochan "%s\n" natural_prelude ;
   for i = 0 to n do
     P.fprintf ochan "symbol s%d : N\n" i
   done ;
-  P.fprintf ochan "symbol f : N ⇒ N\n" ;
-  P.fprintf ochan "rule f s0 → s0\n" ;
-  for i = 1 to n do
-    P.fprintf ochan "and f s%d → 0\n" i
+  P.fprintf ochan "symbol thump : N ⇒ N\n" ;
+  P.fprintf ochan "rule thump s0 → s0\n" ;
+  for i = 0 to n do
+    P.fprintf ochan "and thump s%d → 0\n" i
   done ;
-  for i = 1 to n do
-    P.fprintf ochan "assert f s%d ≡ 0\n" i
+  for i = 0 to n do
+    P.fprintf ochan "assert thump s%d ≡ 0\n" i
   done ;
   close_out ochan
 
 (** [comb n] creates a comb, that is an unbalanced tree with each node
     having a child on [s] and a rule on [z]. *)
 let comb n =
-  let ochan = open_out (testdir ^ "comb.lp") in
+  let fname = F.concat testdir "comb.lp" in
+  let ochan = open_out fname in
   P.fprintf ochan "%s\n" natural_prelude ;
   P.fprintf ochan "symbol comb : N ⇒ N\n" ;
   P.fprintf ochan "rule comb 0 → 0\n" ;
