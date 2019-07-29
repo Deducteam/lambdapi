825d14daa5e0317fe0b20fa48f13d43ee4f1ed4c
Frédéric Blanqui <frederic.blanqui@inria.fr>
2019-07-29 14:50:36 +0200

    typing: fix log function

--- src/typing.ml
+++ src/typing.ml
@@ -6,17 +6,18 @@ open Console
 open Terms
 open Print
 open Unif
+open Infer
 
 (** [check builtins ctx t a] tells whether [t] has type [a] in context
    [ctx]. *)
 let check : sym StrMap.t -> Ctxt.t -> term -> term -> bool =
   fun builtins ctx t a ->
-  if !log_enabled then log_solv "check [%a] [%a]" pp t pp a;
+  if !log_enabled then log_type "check [%a] [%a]" pp t pp a;
   let to_solve = Infer.check ctx t a in
   match solve builtins true {no_problems with to_solve} with
   | None     -> false
   | Some(cs) ->
-      let fn (a,b) = log_solv (red "Cannot solve [%a] ~ [%a].") pp a pp b in
+      let fn (a,b) = log_type (red "Cannot solve [%a] ~ [%a].") pp a pp b in
       if !log_enabled then List.iter fn cs; cs = []
 
 (** [infer_constr builtins ctx t] tries to infer a type [a], together with
@@ -25,7 +26,7 @@ let check : sym StrMap.t -> Ctxt.t -> term -> term -> bool =
 let infer_constr
     : sym StrMap.t -> Ctxt.t -> term -> (unif_constrs * term) option =
   fun builtins ctx t ->
-  if !log_enabled then log_solv "infer_constr [%a]" pp t;
+  if !log_enabled then log_type "infer_constr [%a]" pp t;
   let (a, to_solve) = Infer.infer ctx t in
   Option.map (fun cs -> (cs, a))
     (solve builtins true {no_problems with to_solve})
@@ -39,7 +40,7 @@ let infer : sym StrMap.t -> Ctxt.t -> term -> term option =
   | None       -> None
   | Some([],a) -> Some(a)
   | Some(cs,_) ->
-      let fn (a,b) = log_solv (red "Cannot solve [%a] ~ [%a].") pp a pp b in
+      let fn (a,b) = log_type (red "Cannot solve [%a] ~ [%a].") pp a pp b in
       if !log_enabled then List.iter fn cs; None
 
 (** [sort_type builtins ctx t] checks that the type of the term [t] in context
@@ -47,7 +48,7 @@ let infer : sym StrMap.t -> Ctxt.t -> term -> term option =
    raised. *)
 let sort_type : sym StrMap.t -> Ctxt.t -> term -> unit =
   fun builtins ctx t ->
-  if !log_enabled then log_solv "sort_type [%a]" pp t;
+  if !log_enabled then log_type "sort_type [%a]" pp t;
   match infer builtins ctx t with
   | None    -> fatal_no_pos "Unable to infer a sort for [%a]." pp t
   | Some(a) ->
