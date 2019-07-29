15bc339ae5ff26284bb97b36691d7c9748a6ae2a
Gabriel Hondet <gabrielhondet@gmail.com>
2019-07-29 17:34:30 +0200

    (~) cosmetics

--- src/dtree.ml
+++ src/dtree.ml
@@ -152,7 +152,7 @@ let to_dot : string -> t -> unit = fun fname tree ->
   let ochan = open_out (fname ^ ".gv") in
   let ppf = F.formatter_of_out_channel ochan in
   let nodecount = ref 0 in
-  F.fprintf ppf "graph {@[<v>" ;
+  F.fprintf ppf "graph {@[<v>";
   let pp_dotterm : dot_term pp = fun oc dh -> match dh with
     | DotAbst(v) -> F.fprintf oc "λ%a" Print.pp_tvar v
     | DotDefa    -> F.fprintf oc "*"
@@ -160,63 +160,66 @@ let to_dot : string -> t -> unit = fun fname tree ->
     | DotCons(Vari(s)) -> F.fprintf oc "%s" s
     | DotCons(Abst)    -> assert false
     | DotSuccess -> F.fprintf oc "✓"
-    | DotFailure -> F.fprintf oc "✗" in
+    | DotFailure -> F.fprintf oc "✗"
+  in
   let pp_tcstr : term tree_constraint pp = fun oc -> function
     | TcstrEq(i, j)        -> F.fprintf oc "@%d≡<sub>v</sub>@%d" i j
     | TcstrFreeVars(vs, i) ->
         F.fprintf oc "%a@@<sub>v</sub>%d" (F.pp_print_list P.pp_tvar)
-          (Array.to_list vs) i in
+          (Array.to_list vs) i
+  in
   (* [write_tree n u v] writes tree [v] obtained from tree number [n] with a
      switch on [u] ({!constructor:None} if default). *)
   let rec write_tree : int -> dot_term -> t -> unit = fun father_l swon ->
     function
     | Leaf(_, a)       ->
-        incr nodecount ;
+        incr nodecount;
         let _, acte = Bindlib.unmbind a in
-        F.fprintf ppf "@ %d [label=\"%a\"];" !nodecount P.pp acte ;
+        F.fprintf ppf "@ %d [label=\"%a\"];" !nodecount P.pp acte;
         F.fprintf ppf "@ %d -- %d [label=<%a>];"
           father_l !nodecount pp_dotterm swon
     | Node(ndata)      ->
         let { swap ; children ; store ; abstraction ; default } = ndata in
-        incr nodecount ;
+        incr nodecount;
         let tag = !nodecount in
         (* Create node *)
         F.fprintf ppf "@ %d [label=\"@%d\"%s];"
-          tag swap (if store then " shape=\"box\"" else "") ;
+          tag swap (if store then " shape=\"box\"" else "");
         (* Create edge *)
         F.fprintf ppf "@ %d -- %d [label=<%a>];"
-          father_l tag pp_dotterm swon ;
-        TcMap.iter (fun s e -> write_tree tag (DotCons(s)) e) children ;
+          father_l tag pp_dotterm swon;
+        TcMap.iter (fun s e -> write_tree tag (DotCons(s)) e) children;
         Option.iter (fun (v, t) -> write_tree tag (DotAbst(v)) t)
-          abstraction ;
-        Option.iter (write_tree tag DotDefa) default ;
+          abstraction;
+        Option.iter (write_tree tag DotDefa) default;
     | Condition(cdata) ->
         let { ok ; condition ; fail } = cdata in
-        incr nodecount ;
+        incr nodecount;
         let tag = !nodecount in
         F.fprintf ppf "@ %d [label=<%a> shape=\"diamond\"];"
-          tag pp_tcstr condition ;
+          tag pp_tcstr condition;
         F.fprintf ppf "@ %d -- %d [label=<%a>];"
-          father_l tag pp_dotterm swon ;
-        write_tree tag DotSuccess ok ;
-        write_tree tag DotFailure fail ;
+          father_l tag pp_dotterm swon;
+        write_tree tag DotSuccess ok;
+        write_tree tag DotFailure fail
     | Fail             ->
-        incr nodecount ;
-        F.fprintf ppf "@ %d [label=<!>];" !nodecount ;
-        F.fprintf ppf "@ %d -- %d [label=\"!\"];" father_l !nodecount in
+        incr nodecount;
+        F.fprintf ppf "@ %d [label=<!>];" !nodecount;
+        F.fprintf ppf "@ %d -- %d [label=\"!\"];" father_l !nodecount
+  in
   begin match tree with
   (* First step must be done to avoid drawing a top node. *)
-  | Node({ swap ; store ; children ;
+  | Node({ swap ; store ; children;
            default ; abstraction }) ->
       F.fprintf ppf "@ 0 [label=\"@%d\"%s];"
-        swap (if store then " shape=\"box\"" else "") ;
-      TcMap.iter (fun sw c -> write_tree 0 (DotCons(sw)) c) children ;
-      Option.iter (fun (v, t) -> write_tree 0 (DotAbst(v)) t) abstraction ;
+        swap (if store then " shape=\"box\"" else "");
+      TcMap.iter (fun sw c -> write_tree 0 (DotCons(sw)) c) children;
+      Option.iter (fun (v, t) -> write_tree 0 (DotAbst(v)) t) abstraction;
       Option.iter (fun t -> write_tree 0 DotDefa t) default
   | Leaf(_)                         -> ()
   | _                               -> assert false
-  end ;
-  F.fprintf ppf "@.}@\n@?" ;
+  end;
+  F.fprintf ppf "@.}@\n@?";
   close_out ochan
 
 (** {3 Binary constraints nodes} *)
@@ -317,14 +320,16 @@ struct
       let lhs = Array.of_list r.Terms.lhs in
       let nonlin = NlScorable.empty in
       let freevars = FvScorable.empty in
-      { lhs ; rhs = r.Terms.rhs ; nonlin ; freevars ; env_builder = [] } in
+      { lhs ; rhs = r.Terms.rhs ; nonlin ; freevars ; env_builder = [] }
+    in
     let size = (* Get length of longest rule *)
       if rs = [] then 0 else
       List.max ~cmp:Int.compare
         (List.map (fun r -> List.length r.Terms.lhs) rs) in
     let positions = Occur.args_of size Occur.empty
                     |> List.map (fun x -> (x, 0))
-                    |> ReductionStack.of_list in
+                    |> ReductionStack.of_list
+    in
     (* [|>] is reverse application, can be thought of as a Unix pipe | *)
     { clauses = List.map r2r rs ; slot = 0 ; positions }
 
@@ -346,7 +351,8 @@ struct
       | Patt(Some(_),_,_)::xs -> loop (ncons, nst + 1) xs
       | Patt(_,_,e)      ::xs
         when e <> [||]        -> loop (ncons, nst + 1) xs
-      | _ :: xs               -> loop acc xs in
+      | _ :: xs               -> loop acc xs
+    in
     let nc, ns = loop (0, 0) ts in
     float_of_int nc /. (float_of_int ns)
 
@@ -368,7 +374,8 @@ struct
       clauses [r]. *)
   let discard_cons_free : clause list -> int array = fun clauses ->
     let ncols = List.max ~cmp:Int.compare
-      (List.map (fun { lhs ; _ } -> Array.length lhs) clauses) in
+        (List.map (fun { lhs ; _ } -> Array.length lhs) clauses)
+    in
     let switchable = List.init ncols (can_switch_on clauses) in
     let switchable2ind i e = if e then Some(i) else None in
     switchable |> List.filteri_map switchable2ind |> Array.of_list
@@ -395,9 +402,11 @@ struct
       let slots_uniq = List.sort_uniq Int.compare slots in
       List.same_length slots slots_uniq
       && not @@ List.exists (fun s -> NlScorable.constrained s nonlin)
-          slots_uniq in
+        slots_uniq
+    in
     let depths = ReductionStack.to_list positions |> List.map snd
-                 |> Array.of_list in
+                 |> Array.of_list
+    in
     let ripe lhs =
       (* [ripe l] returns whether [lhs] can be applied. *)
       let de = Array.sub depths 0 (Array.length lhs) in
@@ -407,9 +416,11 @@ struct
          variables. *)
         match pt with
         | Patt(_, _, e) -> Array.length e = de
-        | _             -> false in
+        | _             -> false
+      in
       Array.for_all2 check lhs de
-      && nonl lhs in
+      && nonl lhs
+    in
     NlScorable.is_empty nonlin && FvScorable.is_empty freevars
     && (lhs = [||] || ripe lhs)
 
@@ -425,19 +436,23 @@ struct
       let nlcstrs = List.map (fun r -> r.nonlin) m.clauses in
       let rnl = match NlScorable.choose nlcstrs with
         | Some(c, i) -> (Nl(c), i)
-        | None       -> (Unavailable, 0.) in
+        | None       -> (Unavailable, 0.)
+      in
       let fvcstrs = List.map (fun r -> r.freevars) m.clauses in
       let rfv = match FvScorable.choose fvcstrs with
         | Some(c, i) -> (Fv(c), i)
-        | None       -> (Unavailable, 0.) in
+        | None       -> (Unavailable, 0.)
+      in
       let rs = match choose m with
         | None       -> (Unavailable, 0.)
-        | Some(c, i) -> (Sp(c), i) in
+        | Some(c, i) -> (Sp(c), i)
+      in
       let r = [| rnl ; rfv ; rs |] in
       let best =
         if Array.for_all (fun (x, _) -> x = Unavailable) r then
           (Unavailable, 0.) else
-          Array.max ~cmp:(fun (_, x) (_, y) -> Pervasives.compare x y) r in
+          Array.max ~cmp:(fun (_, x) (_, y) -> Pervasives.compare x y) r
+      in
       match fst best with
       | Nl(c)          -> NlConstrain(c)
       | Fv(c)          -> FvConstrain(c)
@@ -453,11 +468,12 @@ struct
       match h with
       | Symb({ sym_name ; sym_path ; _ }, _) ->
           Some(Symb({ c_mod = sym_path ; c_sym = sym_name
-                       ; c_ari = arity }), e)
+                    ; c_ari = arity }), e)
       | Abst(_, _)                           -> Some(Abst, e)
       | Vari(x)                              ->
           Some(Vari(Bindlib.name_of x), e)
-      | _                                    -> None in
+      | _                                    -> None
+    in
     let tc_fst_cmp (tca, _) (tcb, _) = tc_compare tca tcb in
     List.filter_map keep_treecons telst |> List.sort_uniq tc_fst_cmp
 
@@ -486,15 +502,18 @@ struct
           then FvScorable.instantiate slot
               (Array.map to_tvar e)
               r.freevars
-          else r.freevars in
+          else r.freevars
+        in
         let nonlin =
           match i with
           | Some(i) -> NlScorable.instantiate slot i r.nonlin
-          | None    -> r.nonlin in
+          | None    -> r.nonlin
+        in
         let env_builder =
           match i with
           | Some(i) -> (slot, (i, Array.map to_tvar e)) :: r.env_builder
-          | None    -> r.env_builder in
+          | None    -> r.env_builder
+        in
         { r with env_builder ; nonlin ; freevars }
     | _             -> r
 
@@ -510,12 +529,14 @@ struct
       let occ, depth = m in
       let _, _, nargs = get_args_len pat in
       let replace = Occur.args_of nargs occ
-                  |> List.map (fun x -> (x, depth)) in
+                    |> List.map (fun x -> (x, depth))
+      in
       ReductionStack.restruct l replace r in
     let ph, pargs, lenp = get_args_len pat in
     let insert r e = Array.concat [ Array.sub r.lhs 0 ci
                                   ; e
-                                  ; Array.drop (ci + 1) r.lhs ] in
+                                  ; Array.drop (ci + 1) r.lhs ]
+    in
     let filtrans r =
       let insert = insert r in
       let h, args, lenh = get_args_len r.lhs.(ci) in
@@ -530,7 +551,8 @@ struct
           let e = Array.make arity (Patt(None, "", [||])) in
           Some({ r with lhs = insert e })
       | _         , Abst(_, _)    -> None
-      | _         , _             -> assert false in
+      | _         , _             -> assert false
+    in
     (pos, List.filter_map filtrans rs)
 
   (** [default c s r] computes the default clauses from [r] that remain to be
@@ -540,7 +562,8 @@ struct
     fun ci pos rs ->
     let pos =
       let l, _, r = ReductionStack.destruct pos ci in
-      ReductionStack.restruct l [] r in
+      ReductionStack.restruct l [] r
+    in
     let transf r =
       match r.lhs.(ci) with
       | Patt(_, _, _)           ->
@@ -564,7 +587,8 @@ struct
     let pos = ReductionStack.restruct l [(occ, depth + 1)] r in
     let insert r e = [ Array.sub r.lhs 0 ci
                      ; [| e |]
-                     ; Array.drop (ci + 1) r.lhs ] in
+                     ; Array.drop (ci + 1) r.lhs ]
+    in
     let transf (r:clause) =
       let ph, pargs = get_args r.lhs.(ci) in
       match ph with
@@ -577,7 +601,8 @@ struct
           let lhs = Array.concat (insert r pat) in
           Some({ r with lhs })
       | Symb(_, _) | Vari(_) -> None
-      | _                    -> assert false in
+      | _                    -> assert false
+    in
     (pos, List.filter_map transf clauses)
 
   (** [nl_succeed c r] computes the clause list from [r] that verify a
@@ -585,7 +610,8 @@ struct
   let nl_succeed : NlScorable.cstr -> clause list -> clause list = fun c ->
     let f r =
       let nonlin = NlScorable.remove c r.nonlin in
-      { r with nonlin } in
+      { r with nonlin }
+    in
     List.map f
 
   (** [nl_fail c r] computes the clauses not failing a non-linearity
@@ -599,7 +625,8 @@ struct
   let fv_succeed : FvScorable.cstr -> clause list -> clause list = fun c ->
     let f r =
       let freevars = FvScorable.remove c r.freevars in
-      { r with freevars } in
+      { r with freevars }
+    in
     List.map f
 
   (** [fv_fail c r] computes the clauses not failing a free variable
@@ -615,8 +642,9 @@ module Cm = ClauseMat
     variables with no non linear constraints. *)
 let harvest : term array -> action -> (int * Cm.binding_data) list -> int ->
   t = fun lhs rhs env_builder slot ->
-  let default_node = { swap = 0 ; store = false ; children = TcMap.empty ;
-                       abstraction = None ; default = None } in
+  let default_node = { swap = 0 ; store = false ; children = TcMap.empty
+                     ; abstraction = None ; default = None }
+  in
   let rec loop lhs env_builder slot = match lhs with
     | []                        -> Leaf(env_builder, rhs)
     | Patt(Some(i), _, e) :: ts ->
@@ -672,17 +700,21 @@ let rec compile : Cm.t -> t =
       harvest lhs rhs env_builder slot
   | NlConstrain(constr)                 ->
       let ok = let nclauses = Cm.nl_succeed constr clauses in
-               compile { patterns with Cm.clauses = nclauses } in
+        compile { patterns with Cm.clauses = nclauses }
+      in
       let fail = let nclauses = Cm.nl_fail constr clauses in
-                 compile {patterns with Cm.clauses = nclauses } in
+        compile {patterns with Cm.clauses = nclauses }
+      in
       let vi, vj = NlScorable.export constr in
       let condition = TcstrEq(vi, vj) in
       Condition({ ok ; condition ; fail })
   | FvConstrain(constr)                 ->
       let ok = let clauses = Cm.fv_succeed constr clauses in
-        compile { patterns with Cm.clauses } in
+        compile { patterns with Cm.clauses }
+      in
       let fail = let clauses = Cm.fv_fail constr clauses in
-        compile { patterns with Cm.clauses } in
+        compile { patterns with Cm.clauses }
+      in
       let slot, vars = FvScorable.export constr in
       let condition = TcstrFreeVars(vars, slot) in
       Condition({ ok ; condition ; fail })
@@ -699,12 +731,14 @@ let rec compile : Cm.t -> t =
               updated in
           let ncm = { Cm.clauses ; Cm.slot ; Cm.positions } in
           TcMap.add tr_cons (compile ncm) acc in
-        List.fold_left f TcMap.empty cons in
+        List.fold_left f TcMap.empty cons
+      in
       (* Default child *)
       let default =
         let positions, clauses = Cm.default swap positions updated in
         let ncm = { Cm.clauses ; Cm.slot ; Cm.positions } in
-        if Cm.is_empty ncm then None else Some(compile ncm) in
+        if Cm.is_empty ncm then None else Some(compile ncm)
+      in
       (* Abstraction specialisation*)
       let abstraction =
         if List.for_all (fun (x, _) -> x <> Abst) cons then None else
@@ -712,7 +746,8 @@ let rec compile : Cm.t -> t =
         incr varcount ;
         let positions, clauses = Cm.abstract swap var positions updated in
         let ncm = { Cm.clauses ; Cm.slot ; Cm.positions } in
-        Some(var, compile ncm) in
+        Some(var, compile ncm)
+      in
       Node({ swap ; store ; children ; abstraction ; default })
 
 (** [update_dtree s] updates decision tree of symbol [s]. *)
