a11d5a3afac2738c6ec649e369e43c1e7582724f
Frédéric Blanqui <frederic.blanqui@inria.fr>
2019-11-15 10:19:51 +0100

    console: undo addition

--- src/console.ml
+++ src/console.ml
@@ -25,8 +25,6 @@ let blu fmt = colorize "34" fmt
 let mag fmt = colorize "35" fmt
 let cya fmt = colorize "36" fmt
 
-let space fmt = " " ^^ fmt
-
 (** [r_or_g cond fmt] colors the format [fmt] in green if [cond] is [true] and
     in red otherwise. *)
 let r_or_g cond = if cond then gre else red
