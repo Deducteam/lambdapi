c54fa28c19e9dab62ef295e5339d32274e64e35c
Frédéric Blanqui <frederic.blanqui@inria.fr>
2019-11-16 22:51:59 +0100

    indentation

--- src/extra.ml
+++ src/extra.ml
@@ -73,7 +73,7 @@ module Option =
       | (_       , _       ) -> false
 
     (** [pp pp_elt oc o] prints on the channel [oc] the element [e] with
-       [pp_elt] if [o = Some e]. *)
+        [pp_elt] if [o = Some e]. *)
     let pp : 'a pp -> 'a option pp = fun pp_elt oc o ->
       match o with
       | None   -> ()
@@ -85,7 +85,7 @@ module List =
     include List
 
     (** [pp pp_elt sep oc l] prints the list [l] on the channel [oc] using
-       [sep] as separator, and [pp_elt] for printing the elements. *)
+        [sep] as separator, and [pp_elt] for printing the elements. *)
     let pp : 'a pp -> string -> 'a list pp = fun pp_elt sep oc l ->
       match l with
       | []    -> ()
@@ -223,11 +223,8 @@ module Array =
         using [sep] as separator, and [pp_e] for printing the elements. *)
     let pp : 'a pp -> string -> 'a array pp = fun pp_elt sep oc a ->
       let n = Array.length a in
-      if n > 0 then
-        begin
-          pp_elt oc a.(0);
-          for i=1 to n-1 do Format.fprintf oc "%s%a" sep pp_elt a.(i) done
-        end
+      if n > 0 then pp_elt oc a.(0);
+      for i=1 to n-1 do Format.fprintf oc "%s%a" sep pp_elt a.(i) done
 
     (** [equal eq a1 a2] tests the equality of [a1] and [a2],  comparing their
         elements with [eq]. *)
