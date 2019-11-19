aff3cd98910a969e70759f569c31c69dd0beca48
gabrielhdt <gabrielhondet@gmail.com>
2019-11-19 15:46:27 +0100

    [*] update

--- editors/emacs/lambdapi.el
+++ editors/emacs/lambdapi.el
@@ -1,24 +1,37 @@
-;;; This is the LambdaPi emacs mode
-(provide 'lambdapi-mode)
-(provide 'lambdapi-legacy-mode)
+;;; lambdapi-mode --- A major mode to edit Dedukti files
+
+;; Author: Rodolphe Lepigre, Gabriel Hondet
+;; Package-Requires: ((quail))
+
+;;; Commentary:
+
+;;; Code:
 
 ;; Syntax table
 (defvar lambdapi-mode-syntax-table nil "Syntax table for LambdaPi.")
 
 (setq lambdapi-mode-syntax-table
-  (let ((synTable (make-syntax-table)))
-    (modify-syntax-entry ?\/ ". 12b" synTable)
-    (modify-syntax-entry ?\n "> b" synTable)
-    synTable))
+  (let ((syn-table (make-syntax-table)))
+    (modify-syntax-entry ?\/ ". 12b" syn-table)
+    (modify-syntax-entry ?\n "> b" syn-table)
+    syn-table))
 
 ;; Keywords
 (defconst lambdapi-font-lock-keywords
-  (list (cons (concat "\\<" (regexp-opt '(
-    "require" "open" "as" "let" "in" "symbol" "const" "injective" "rule" "and"
-    "definition" "theorem" "assert" "assertnot" "set" "proof" "qed" "admit"
-    "abort" "focus" "print" "proofterm" "refine" "apply" "simpl" "assume"
-    "rewrite" "refl" "sym" "type" "compute"
-  )) "\\>") 'font-lock-keyword-face))
+  (list
+   (cons
+    (concat
+     "\\<"
+     (regexp-opt
+      '("require" "open" "as"
+        "let" "in"
+        "symbol" "constant" "injective" "protected" "private"
+        "definition" "theorem" "rule" "and"
+        "assert" "assertnot" "set" "proof" "qed" "admit"
+        "abort" "focus" "print" "proofterm" "refine" "apply" "simpl" "intro"
+        "rewrite" "reflexivity" "symmetry" "type" "compute"))
+     "\\>")
+    'font-lock-keyword-face))
   "Keyword highlighting for the LambdaPi mode.")
 
 ;; Unicode support
@@ -34,13 +47,21 @@
   ("\\\\" ?λ) ("\\lambda" ?λ)
   (":=" ?≔) ("\\defeq"  ?≔)
   ("==" ?≡) ("\\equiv"  ?≡)
-  ("&&" ?∧) ("\\wedge"  ?∧)
-  ("||" ?∨) ("\\vee"    ?∨)
-  ("~~" ?¬) ("\\neg"    ?¬)
-  ("??" ?∃) ("\\exists" ?∃))
+  ("&&" ?∧) ("\\wedge"  ?∧) ("/\\" ?∧)
+  ("||" ?∨) ("\\vee"    ?∨) ("\\/" ?∨)
+  ("~~" ?¬) ("\\not"    ?¬)
+  ("??" ?∃) ("\\exists" ?∃)
+  ("`a" ?α) ("`b" ?β) ("`c" ?γ) ("`d" ?δ)
+  ("`e" ?ε) ("`h" ?η) ("`i" ?ι) ("`k" ?κ)
+  ("`m" ?μ) ("`n" ?ν) ("`o" ?ω) ("`p" ?π)
+  ("`r" ?ρ) ("`s" ?σ) ("`t" ?τ) ("`u" ?υ)
+  ("`w" ?ω) ("`x" ?χ) ("`y" ?υ) ("`z" ?ζ)
+  ("`C" ?Γ) ("`D" ?Δ) ("`G" ?Γ) ("`L" ?Λ)
+  ("`O" ?Ω) ("`P" ?Π) ("`S" ?Σ) ("`W" ?Ω))
 
 ;; Main function creating the mode (lambdapi)
-(define-derived-mode lambdapi-mode fundamental-mode "LambdaPi"
+;;;###autoload
+(define-derived-mode lambdapi-mode prog-mode "LambdaPi"
   "A mode for editing LambdaPi files."
   (set-syntax-table lambdapi-mode-syntax-table)
   (setq-local font-lock-defaults '(lambdapi-font-lock-keywords))
@@ -53,25 +74,31 @@
 (defvar lambdapi-mode-legacy-syntax-table nil "Syntax table for LambdaPi.")
 
 (setq lambdapi-mode-legacy-syntax-table
-  (let ((synTable (make-syntax-table)))
-    (modify-syntax-entry ?\( ". 1" synTable)
-    (modify-syntax-entry ?\) ". 4" synTable)
-    (modify-syntax-entry ?\; ". 23" synTable)
-    synTable))
+  (let ((syn-table (make-syntax-table)))
+    (modify-syntax-entry ?\( ". 1" syn-table)
+    (modify-syntax-entry ?\) ". 4" syn-table)
+    (modify-syntax-entry ?\; ". 23" syn-table)
+    syn-table))
 
 ;; Keywords (legacy syntax)
 (defconst lambdapi-legacy-font-lock-keywords
-  (list (cons (concat "\\<" (regexp-opt '(
-           "def" "thm" "inj"
-        )) "\\>") 'font-lock-keyword-face)
-        (cons (concat "#" (regexp-opt '(
-           "REQUIRE" "EVAL" "INFER" "ASSERT" "ASSERTNOT"
-        )) "\\>") 'font-lock-preprocessor-face))
+  (list
+   (cons
+    (concat
+     "\\<"
+     (regexp-opt '("def" "thm" "inj"))
+     "\\>") 'font-lock-keyword-face)
+   (cons
+    (concat
+     "#"
+     (regexp-opt '("REQUIRE" "EVAL" "INFER" "ASSERT" "ASSERTNOT"))
+     "\\>") 'font-lock-preprocessor-face))
   "Keyword highlighting for the LambdaPi mode (legacy syntax).")
 
 
 ;; Main function creating the mode (legacy syntax)
-(define-derived-mode lambdapi-legacy-mode fundamental-mode "LambdaPi (legacy)"
+;;;###autoload
+(define-derived-mode lambdapi-legacy-mode prog-mode "LambdaPi (legacy)"
   "A mode for editing LambdaPi files (in legacy syntax)."
   (set-syntax-table lambdapi-mode-legacy-syntax-table)
   (setq-local font-lock-defaults '(lambdapi-legacy-font-lock-keywords))
@@ -80,16 +107,23 @@
   (setq-default indent-tabs-mode nil))
 
 ;; LSP mode
-(require 'eglot)
-(add-to-list 'eglot-server-programs
-  '(lambdapi-mode . ("lp-lsp" "--std")))
-(add-to-list 'eglot-server-programs
-  '(lambdapi-legacy-mode . ("lp-lsp" "--std")))
-(add-hook 'lambdapi-mode-hook 'eglot-ensure)
-(add-hook 'lambdapi-legacy-mode-hook 'eglot-ensure)
+
+(if (not (version<= emacs-version "26"))
+    (progn
+      (require 'eglot)
+      (add-to-list 'eglot-server-programs
+                   '(lambdapi-mode . ("lp-lsp" "--std")))
+      (add-to-list 'eglot-server-programs
+                   '(lambdapi-legacy-mode . ("lp-lsp" "--std")))
+      (add-hook 'lambdapi-mode-hook 'eglot-ensure)
+      (add-hook 'lambdapi-legacy-mode-hook 'eglot-ensure)))
 
 ;; Register mode the the ".lp" extension
+;;;###autoload
 (add-to-list 'auto-mode-alist '("\\.lp\\'" . lambdapi-mode))
+;;;###autoload
 (add-to-list 'auto-mode-alist '("\\.dk\\'" . lambdapi-legacy-mode))
 
+(provide 'lambdapi-mode)
+(provide 'lambdapi-legacy-mode)
 ;;; lambdapi.el ends here
