d01a435ab64ec31279abde91a36b20929453d160
Gabriel Hondet <gabrielhondet@gmail.com>
2020-02-05 15:08:18 +0100

    quoting correctly

--- tools/rec_to_lp/dk_hs_ocaml.sh
+++ tools/rec_to_lp/dk_hs_ocaml.sh
@@ -7,18 +7,18 @@ dksrc='DEDUKTI'
 mlsrc='OCAML'
 hssrc='HASKELL'
 
-files="(cd "${dksrc}" || exit && ls)"
+files="(cd ${dksrc} || exit && ls)"
 
 if [[ ! -f "$mlsrc" ]]; then
  svn co 'https://scm.gforge.inria.fr/anonscm/svn/rec/2019-CONVECS/OCAML'
 fi
-for lpf in files; do
+for lpf in $files; do
   root=${lpf%.lp}
   t_lp="$(time lambdapi --verbose 0 "${dksrc}/${lpf}")"
   t_ghcrun="$(time ghcrun "${hssrc}/${root}.hs")"
   t_ocaml="$(time ocaml "${mlsrc}/${root}.ml")"
   # For ghc and ocamlopt, we time compilation and execution
-  t_ghc="$(time (cd "${hssrc}" || exit && ghc "${root}.hs" && ./${root}))"
+  t_ghc="$(time (cd "${hssrc}" || exit && ghc "${root}.hs" && "./${root}"))"
   t_mlopt="$(time (cd "${mlsrc}" || exit && ocamlopt "${root}.ml" && \
           ./a.out))"
   echo -e "${root}: ${t_lp}/${t_ghcrun}/${t_ocaml}/${t_ghc}/${t_mlopt}"
