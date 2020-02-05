662cbede5db56b62de24ea31af3140407ffa024c
Gabriel Hondet <gabrielhondet@gmail.com>
2020-02-05 14:23:42 +0100

    safety

--- tools/rec_to_lp/translate_nc.sh
+++ tools/rec_to_lp/translate_nc.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 ## This scripts translates all the haskell files from the HASKELL to a DEDUKTI
 ## directory with the dedukti(3) syntax.
+## NOTE tests using conditional rewriting are not translated
+
+set -euf -o pipefail
 
 src="HASKELL"
 out="DEDUKTI"
+if [[ ! -f HASKELL ]]; then
+  svn co 'https://scm.gforge.inria.fr/anonscm/svn/rec/2019-CONVECS/HASKELL'
+fi
 mkdir -p "$out"
 (cd "$src" || exit
 for f in *.hs; do
-  if ! grep '==\|=/=' "$f"; then
+  if ! grep '==\|=/=' "$f"; then # skip conditional rewriting
     ../rec_hs_to_lp.awk "$f" > "../${out}/${f%.hs}.lp"
   fi
 done)
