b0197d381df212261f67a8d67f8eac1be93037f1
Gabriel Hondet <gabrielhondet@gmail.com>
2020-02-05 15:04:26 +0100

    bash time

--- tools/rec_to_lp/dk_hs_ocaml.sh
+++ tools/rec_to_lp/dk_hs_ocaml.sh
@@ -2,8 +2,7 @@
 ## Compare timing of tests files between Haskell, OCaml and Dedukti3
 set -euf -o pipefail
 
-timecmd='/usr/bin/time -f %e'
-
+export TIMEFORMAT="%E" # Only seconds
 dksrc='DEDUKTI'
 mlsrc='OCAML'
 hssrc='HASKELL'
@@ -15,12 +14,12 @@ if [[ ! -f "$mlsrc" ]]; then
 fi
 for lpf in files; do
   root=${lpf%.lp}
-  t_lp="$($timecmd lambdapi --verbose 0 "${dksrc}/${lpf}")"
-  t_ghcrun="$($timecmd ghcrun "${hssrc}/${root}.hs")"
-  t_ocaml="$($timecmd ocaml "${mlsrc}/${root}.ml")"
+  t_lp="$(time lambdapi --verbose 0 "${dksrc}/${lpf}")"
+  t_ghcrun="$(time ghcrun "${hssrc}/${root}.hs")"
+  t_ocaml="$(time ocaml "${mlsrc}/${root}.ml")"
   # For ghc and ocamlopt, we time compilation and execution
-  t_ghc="$($timecmd (cd "${hssrc}" || exit && ghc "${root}.hs" && ./${root}))"
-  t_mlopt="$($timecmd (cd "${mlsrc}" || exit && ocamlopt "${root}.ml" && \
+  t_ghc="$(time (cd "${hssrc}" || exit && ghc "${root}.hs" && ./${root}))"
+  t_mlopt="$(time (cd "${mlsrc}" || exit && ocamlopt "${root}.ml" && \
           ./a.out))"
   echo -e "${root}: ${t_lp}/${t_ghcrune}/${t_ocaml}/${t_ghc}/${t_mlopt}"
 done
