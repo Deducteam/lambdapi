af376d6324527f356d3f3f3e7c782af35bd0e1ee
Rehan MALAK <rehan.malak@inria.fr>
2020-03-26 10:35:11 +0100

    introduce unicode ⊢
      assert n m (o:N) ⊢ f n m o ≡ f n m o
      assert n (m:N) ⊢ f n m : N ⇒ N

--- src/core/parser.ml
+++ src/core/parser.ml
@@ -107,8 +107,8 @@ let declared_id = Prefix.grammar declared_ids
 (** The following should not appear as substrings of binary operators, as they
     would introduce ambiguity in the parsing. *)
 let forbidden_in_ops =
-  [ "("; ")"; "."; "λ"; "∀"; "&"; "["; "]"; "{"; "}"; "?"; ":"; "→"; "⇒"
-  ; "@"; ","; ";"; "\""; "\'"; "≔"; "//"; " "; "\r"; "\n"; "\t"; "\b" ]
+  [ "("; ")"; "."; "λ"; "∀"; "&"; "["; "]"; "{"; "}"; "?"; ":"; "→"; "⊢"
+  ; "⇒"; "@"; ","; ";"; "\""; "\'"; "≔"; "//"; " "; "\r"; "\n"; "\t"; "\b" ]
   @ List.init 10 string_of_int
 
 (** [get_ops loc p] loads the unary and binary operators associated to  module
@@ -480,12 +480,12 @@ let parser query =
       Pos.in_pos _loc (P_query_flag(s, b))
   | mf:assert_must_fail a:assertion ->
       Pos.in_pos _loc (P_query_assert(mf,a))
-  | mf:assert_must_fail "{" ps:arg* "}" t:term "≡" u:term ->
+  | mf:assert_must_fail ps:arg* "⊢" t:term "≡" u:term ->
       let ps_t = Pos.in_pos _loc (P_Abst(ps,t)) in
       let ps_u = Pos.in_pos _loc (P_Abst(ps,u)) in
       let assert_conv = P_assert_conv(ps_t,ps_u) in
       Pos.in_pos _loc (P_query_assert(mf,assert_conv))
-  | mf:assert_must_fail "{" ps:arg* "}" t:term ":" u:term ->
+  | mf:assert_must_fail ps:arg* "⊢" t:term ":" u:term ->
       let ps_t = Pos.in_pos _loc (P_Abst(ps,t)) in
       let ps_u = Pos.in_pos _loc (P_Prod(ps,u)) in
       let assert_typing = P_assert_typing(ps_t,ps_u) in
