dce215369d516dc41e4c97baf9a3d07310b3ea70
gabrielhdt <gabrielhondet@gmail.com>
2020-04-01 08:37:16 +0200

    no stars

--- src/core/eval.ml
+++ src/core/eval.ml
@@ -184,11 +184,11 @@ and tree_walk : dtree -> ctxt -> stack -> (term * stack) option =
   let vars = Array.make capacity Kind in (* dummy terms *)
   let bound = Array.make capacity TE_None in
   (* [walk tree stk cursor vars_id id_vars] where [stk] is the stack of terms
-   * to match and [cursor] the cursor indicating where to write in the [vars]
-   * array described in {!module:Terms} as the environment of the RHS during
-   * matching. [vars_id] maps the free variables contained in the term to the
-   * indexes defined during tree build, and [id_vars] is the inverse mapping
-   * of [vars_id]. *)
+     to match and [cursor] the cursor indicating where to write in the [vars]
+     array described in {!module:Terms} as the environment of the RHS during
+     matching. [vars_id] maps the free variables contained in the term to the
+     indexes defined during tree build, and [id_vars] is the inverse mapping
+     of [vars_id]. *)
   let rec walk tree stk cursor vars_id id_vars =
     let open Tree_types in
     match tree with
