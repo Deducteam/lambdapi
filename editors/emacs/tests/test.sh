98fc0c5d5c0462baf1fb8d511294aabc14294813
gabrielhdt <gabrielhondet@gmail.com>
2020-04-03 08:06:04 +0200

    test script installs company

--- editors/emacs/tests/test.sh
+++ editors/emacs/tests/test.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
+# TODO: add switch to test or not company
 
+set -euf -o pipefail
 ## Open a minimal emacs with lambdapi mode
 ## First argument is the version
 
+elpa='https://elpa.gnu.org/packages'
 confurl='https://sanemacs.com/sanemacs.el'
 conf='init.el'
 curl "$confurl" > "${conf}"
+# Add elpa repo
+sed -i "/(add-to-list 'package-archives.*)$/a \
+(add-to-list 'package-archives '(\"gnu\" . \"${elpa}\"))\\n" \
+"$conf"
+# Add company installation
+{ printf "(use-package company :pin gnu)\\n" ;
+  printf "(use-package company-math :pin gnu)\\n" ;
+  printf "(global-company-mode)\\n"; } >> "${conf}"
 (cd .. || exit
  make dist
  emacs --quick -l "tests/${conf}" \
