c036c5b580b0f8e9985b1f979d4c0a11e6927eaa
gabrielhdt <gabrielhondet@gmail.com>
2020-05-16 09:37:21 +0200

    Add test facilities

---
+++ editors/emacs/test.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+## Test the lambdapi-mode built from '*.el' files.
+## The script  downloads a  fresh and basic  configuration, creates  a temporary
+## directory and launches emacs in it. You  can create a new 'foo.lp' to try the
+## mode.
+##
+## Takes the version of the mode as first argument
+set -eu
+tmp="$(mktemp -d)"
+cp "dist/lambdapi-mode-$1.tar" "${tmp}"
+(cd "${tmp}" || exit 1
+ curl https://sanemacs.com/sanemacs.el > sanemacs.el
+ echo '(use-package eglot)' >> sanemacs.el
+ echo '(use-package math-symbol-lists)' >> sanemacs.el
+ emacs --quick -l sanemacs.el \
+     --eval "(package-install-file \"lambdapi-mode-$1.tar\")")
+rm -rf "${tmp}"
