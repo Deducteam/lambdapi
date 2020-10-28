e6f2bda4a5a1978688a55fc5cc96013f56ae3fc7
Rehan MALAK <rehan.malak@inria.fr>
2020-10-28 17:26:19 +0100

    query compute with TypeCheckInstanciation and unif_hint prbml

--- src/core/queries.ml
+++ src/core/queries.ml
@@ -3,7 +3,6 @@
 open Console
 open Pos
 open Syntax
-open Unif
 open Terms
 open Print
 
@@ -16,24 +15,28 @@ let handle_query : Sig_state.t -> Proof.t option -> p_query -> unit =
     | Some(ps) -> fst (Proof.focus_goal q.pos ps)
   in
   let scope = Scope.scope_term Public ss env in
+  let type_check = Unif.TypeCheckInstanciation in
   match q.elt with
   | P_query_assert(must_fail, asrt)  ->
       let result =
         match asrt with
         | P_assert_typing(pt,pa) ->
           let t = scope pt and a = scope pa and ctxt = Env.to_ctxt env in
-          Typing.sort_type ~type_check:Unif.TypeCheckInstanciation ctxt a;
+          Typing.sort_type ~type_check ctxt a;
           (try
-             Typing.check ~type_check:Unif.TypeCheckInstanciation ctxt t a
+             Typing.check ~type_check ctxt t a
            with _ -> false)
         | P_assert_conv(pt,pu)   ->
-          let t = scope pt and u = scope pu in
-          let infer = Typing.infer (Env.to_ctxt env) in
+          let t = scope pt and u = scope pu and ctxt = Env.to_ctxt env in
+          let infer =
+            Typing.infer ~type_check ctxt
+          in
           match (infer t, infer u) with
           | (Some(a), Some(b)) ->
-              let pb = {empty_problem with to_solve = [[], a, b]} in
+              let pb = Unif.{empty_problem with to_solve = [[], a, b]} in
+              let cs = Unif.solve ~type_check pb in
               begin
-                match solve pb with
+                match cs with
                 | None -> fatal q.pos "Infered types are not convertible."
                 | Some [] -> Eval.eq_modulo [] t u
                 | Some cs ->
@@ -66,7 +69,7 @@ let handle_query : Sig_state.t -> Proof.t option -> p_query -> unit =
       (* Infer the type of [t]. *)
       let t = scope pt in
       let a =
-        match Typing.infer (Env.to_ctxt env) t with
+        match Typing.infer ~type_check (Env.to_ctxt env) t with
         | Some(a) -> Eval.eval cfg [] a
         | None    -> fatal pt.pos "Cannot infer the type of [%a]." pp_term t
       in
@@ -75,7 +78,7 @@ let handle_query : Sig_state.t -> Proof.t option -> p_query -> unit =
       (* Infer a type for [t], and evaluate [t]. *)
       let t = scope pt in
       let v =
-        match Typing.infer (Env.to_ctxt env) t with
+        match Typing.infer ~type_check (Env.to_ctxt env) t with
         | Some(_) -> Eval.eval cfg [] t
         | None    -> fatal pt.pos "Cannot infer the type of [%a]." pp_term t
       in
