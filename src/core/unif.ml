190ff140475acde270e85cf758f8816d778d82d3
Rehan MALAK <rehan.malak@inria.fr>
2020-10-30 23:29:53 +0100

    remove remaining globals

--- src/core/unif.ml
+++ src/core/unif.ml
@@ -170,8 +170,6 @@ let instantiation : ctxt -> meta -> term array -> term ->
         Some (Bindlib.bind_mvar vs (lift u))
   else None
 
-let main_constr : constr list Stdlib.ref = Stdlib.ref []
-
 let mymem eq x l =
   let rec mem x = function
       [] -> false
@@ -185,8 +183,9 @@ let g_type_check = Stdlib.ref NoTypeCheck
 
 (** [instantiate ctx m ts u] check whether, in a problem [m[ts]=u], [m] can be
     instantiated and, if so, instantiate it. *)
-let instantiate : ctxt -> meta -> term array -> term -> bool =
-  fun ctx m ts u ->
+let instantiate : ?type_check:type_check -> ctxt -> meta -> term array ->
+  term -> constr list -> bool =
+  fun ?(type_check=NoTypeCheck) ctx m ts u initial ->
   let type_meta_app m ts =
     let rec aux a ts =
       match (Eval.whnf ctx a), ts with
@@ -200,10 +199,10 @@ let instantiate : ctxt -> meta -> term array -> term -> bool =
   | Some(bu) when Bindlib.is_closed bu ->
     let m_app = type_meta_app m (Array.to_list ts) in
     let constrs = Infer.check ctx u m_app in
-    let is_new_constr constr = not (mymem constr Stdlib.(!main_constr)) in
+    let is_new_constr constr = not (mymem constr initial) in
     let there_is_new_constr = List.exists is_new_constr constrs in
     begin
-      match (there_is_new_constr,Stdlib.(!g_type_check)) with
+      match (there_is_new_constr,type_check) with
       | false,_ ->
         if !log_enabled then log_unif (gre "no new constraints");
         if !log_enabled then log_unif (yel "%a ≔ %a") pp_meta m pp_term u;
@@ -220,16 +219,18 @@ let instantiate : ctxt -> meta -> term array -> term -> bool =
 
 (** [solve p] tries to solve the unification problem [p] and
     returns the constraints that could not be solved. *)
-let rec solve : problem -> constr list = fun p ->
+let rec solve : ?type_check:type_check -> problem -> constr list =
+  fun ?type_check p ->
   match p with
   | { to_solve = []; unsolved = []; _ } -> []
   | { to_solve = []; unsolved = cs; recompute = true } ->
-     solve {empty_problem with to_solve = cs}
+     solve ?type_check {empty_problem with to_solve = cs}
   | { to_solve = []; unsolved = cs; _ } -> cs
-  | { to_solve = (c,t,u)::to_solve; _ } -> solve_aux c t u {p with to_solve}
+  | { to_solve = (c,t,u)::to_solve; _ } -> solve_aux ?type_check c t u {p with to_solve}
 
-and solve_aux : ctxt -> term -> term -> problem -> constr list =
-  fun ctx t1 t2 p ->
+and solve_aux : ?type_check:type_check -> ctxt -> term -> term -> problem -> constr list =
+  fun ?type_check ctx t1 t2 p ->
+  let initial = (ctx,t1,t2)::p.to_solve in
   let t1 = Eval.whnf ctx t1 in
   let t2 = Eval.whnf ctx t2 in
   let (h1, ts1) = Basics.get_args t1 in
@@ -492,9 +493,11 @@ and solve_aux : ctxt -> term -> term -> problem -> constr list =
            end
        end
 
-  | (Meta(m,ts) , _          ) when ts1 = [] && instantiate ctx m ts t2 ->
+  | (Meta(m,ts) , _          )
+    when ts1 = [] && instantiate ctx m ts t2 initial ->
      solve {p with recompute = true}
-  | (_          , Meta(m,ts) ) when ts2 = [] && instantiate ctx m ts t1 ->
+  | (_          , Meta(m,ts) )
+    when ts2 = [] && instantiate ctx m ts t1 initial ->
      solve {p with recompute = true}
 
   | (Meta(m,ts)  , Prod(_,_) )
@@ -530,8 +533,7 @@ and solve_aux : ctxt -> term -> term -> problem -> constr list =
    constraints. *)
 let solve : ?type_check:type_check -> problem -> constr list option =
   fun ?(type_check=TypeCheckInstanciation) p ->
-  Stdlib.(g_type_check := type_check);
-  try Some (solve p) with Unsolvable -> None
+  try Some (solve ~type_check p) with Unsolvable -> None
 
 (** [eq c t u] tries to unify the terms [t] and [u] in context [c], by
    instantiating their metavariables. *)
