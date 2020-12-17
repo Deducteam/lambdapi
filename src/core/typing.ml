96c4ff03658f1fc8300d75faa1214d1c94ba75d9
Frédéric Blanqui <frederic.blanqui@inria.fr>
2020-12-17 18:25:35 +0100

    fix #393

--- src/core/typing.ml
+++ src/core/typing.ml
@@ -10,12 +10,14 @@ open Print
 (** [check ctx t a] tells whether [t] has type [a] in context [ctx]. *)
 let check : ?type_check:type_check -> ctxt -> term -> term -> bool =
   fun ?(type_check=TypeCheckInstanciation) ctx t a ->
-  let to_solve = Infer.check ctx t a in
-  match solve_noexn ~type_check {empty_problem with to_solve} with
-  | None     -> false
-  | Some([]) -> true
-  | Some(cs) ->
-      List.iter (fatal_msg "Cannot solve %a.\n" pp_constr) cs; false
+  try
+    let to_solve = Infer.check ctx t a in
+    match solve_noexn ~type_check {empty_problem with to_solve} with
+    | None     -> false
+    | Some([]) -> true
+    | Some(cs) ->
+        List.iter (fatal_msg "Cannot solve %a.\n" pp_constr) cs; false
+  with Infer.Invalid_Abst -> false
 
 (** [infer_constr ctx t] tries to infer a type [a], together with
    unification constraints [cs], for the term [t] in context [ctx].  The
@@ -23,9 +25,11 @@ let check : ?type_check:type_check -> ctxt -> term -> term -> bool =
 let infer_constr :
   ?type_check:type_check -> ctxt -> term -> (term * constr list) option =
   fun ?(type_check=TypeCheckInstanciation) ctx t ->
-  let (a, to_solve) = Infer.infer ctx t in
-  Option.map (fun cs -> (a, cs))
-    (solve_noexn ~type_check {empty_problem with to_solve})
+  try
+    let (a, to_solve) = Infer.infer ctx t in
+    Option.map (fun cs -> (a, cs))
+      (solve_noexn ~type_check {empty_problem with to_solve})
+  with Infer.Invalid_Abst -> None
 
 (** [infer ctx t] tries to infer a type [a] for [t] in the context
    [ctx]. The function returns [Some(a)] in case of success, and [None]
@@ -48,5 +52,4 @@ let sort_type : ?type_check:type_check -> ctxt -> term -> unit =
   | Some(a) ->
   match unfold a with
   | Type | Kind -> ()
-  | a           ->
-      fatal None "%a has type %a (not a sort)." pp_term t pp_term a
+  | a -> fatal None "%a has type %a (not a sort)." pp_term t pp_term a
