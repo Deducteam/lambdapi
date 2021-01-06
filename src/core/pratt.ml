b2d2c1eb54100b9ea997eff89583e8f513da7a43
gabrielhdt <gabrielhondet@gmail.com>
2021-01-06 07:56:06 +0100

    use a get_ident rather than pattern matching
    externalise handling of pterms

--- src/core/pratt.ml
+++ src/core/pratt.ml
@@ -30,11 +30,19 @@ module Pratt : sig
       into [(@(+(@(+,3,5)),2)]. Note that it doesn't recurse into terms such
       as abstractions or arrows. *)
 end = struct
+
+  (** [get_ident t] extracts a string and a position from [t] if it is an
+      identifier. *)
+  let get_ident : p_term -> (string * Pos.popt) option = fun {elt; pos} ->
+    match elt with
+    | P_Iden({elt=(_,s); _}, _) -> Some(s, pos)
+    | _ -> None
+
   (** [lbp t] returns the left binding power of term [t] (which is 0 if [t] is
       not an operator). *)
-  let lbp : p_term -> priority = fun {elt=t; _} ->
-    match t with
-    | P_Iden({elt=(_,s); _}, _) ->
+  let lbp : p_term -> priority = fun pt ->
+    match get_ident pt with
+    | Some(s,_) ->
         begin match StrHtbl.find_opt operators s with
           | Some (Binary(_, _, bp, _))
           | Some (Unary(_, bp, _)) -> bp
@@ -47,8 +55,8 @@ end = struct
 
   (** [is_binop t] returns [true] iff term [t] is a binary operator. *)
   let is_binop : p_term -> bool = fun t ->
-    match t.elt with
-    | P_Iden({elt = (_, s); _}, _) -> (
+    match get_ident t with
+    | Some(s,_) -> (
         match StrHtbl.find_opt operators s with
         | Some(Binary(_)) -> true
         | _ -> false )
@@ -58,11 +66,11 @@ end = struct
       is not an operator, [nud] is the identity. Otherwise, the output is a
       production rule. *)
   let rec nud : p_term Stream.t -> p_term -> p_term = fun strm t ->
-    match t.elt with
-    | P_Iden({elt = (_, s); pos}, _) ->
+    match get_ident t with
+    | Some(s,p) ->
         begin match StrHtbl.find_opt operators s with
           | Some(Unary((_, rbp, _) as op)) ->
-              Pos.make pos (P_UnaO(op, expression ~rbp strm))
+              Pos.make p (P_UnaO(op, expression ~rbp strm))
           | _ -> t
         end
     | _ -> t
@@ -70,8 +78,8 @@ end = struct
   (** [led left t] is the production of term [t] with left context
       [left]. *)
   and led : p_term Stream.t -> p_term -> p_term -> p_term = fun strm left t ->
-    match t.elt with
-    | P_Iden({elt = (_, s); pos}, _) ->
+    match get_ident t with
+    | Some(s,p) ->
         begin match StrHtbl.find_opt operators s with
           | Some(Binary((_, assoc, bp, _) as op)) ->
               let rbp =
@@ -80,7 +88,7 @@ end = struct
                 else bp
               in
               (* REVIEW: and for non associative? *)
-              Pos.make pos (P_BinO(left, op, expression ~rbp strm))
+              Pos.make p (P_BinO(left, op, expression ~rbp strm))
           | _ -> assert false (* [t] must be an operator. *)
         end
     | _ -> assert false (* [t] must be an operator. *)
@@ -93,21 +101,21 @@ end = struct
       match Stream.peek strm with
       | None -> left
       | Some(pt) when is_binop pt ->
-        (* If [pt] has a higher left binding power than the binding power of
-           the previous operator in the stream. *)
-        if lbp pt > rbp then
-          (* Performed before to execute side effect on stream. *)
-          let next = Stream.next strm in
-          aux (led strm left next)
-        else
-          left
+          (* If [pt] has a higher left binding power than the binding power of
+             the previous operator in the stream. *)
+          if lbp pt > rbp then
+            (* Performed before to execute side effect on stream. *)
+            let next = Stream.next strm in
+            aux (led strm left next)
+          else
+            left
       | Some(_) -> (* argument of an application *)
-        let next = Stream.next strm in
-        let right = nud strm next in
-        let pos =
-          let open Lplib.Option.Infix in
-          Pos.(Option.pure cat <*> next.pos <*> right.pos) in
-        aux (Pos.make pos (P_Appl(left, right)))
+          let next = Stream.next strm in
+          let right = nud strm next in
+          let pos =
+            let open Lplib.Option.Infix in
+            Pos.(Option.pure cat <*> next.pos <*> right.pos) in
+          aux (Pos.make pos (P_Appl(left, right)))
 
     in
     let next = Stream.next strm in
