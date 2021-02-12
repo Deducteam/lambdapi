88be0ef8c12976e78802e1205ac491e59afcc037
Ashish Barnawal <ashbrnwl.2000@gmail.com>
2021-02-13 01:24:10 +0530

    emacs interactive mode

--- editors/emacs/lambdapi-proofs.el
+++ editors/emacs/lambdapi-proofs.el
@@ -10,35 +10,48 @@
 
 (defconst lp-goal-line-prefix "--------------------------------------------------------------------------------")
 
-(defun lp-focus-goal (goalno &optional proofbuf lineNo)
+(defconst lambdapi-terminators '(";" "begin")
+  "List of terminators for interactive mode")
+
+(defface lambdapi-proof-face
+  '((((background dark)) :background "darkgreen")
+    (((background light)) :background "darkseagreen2"))
+  "Face for evaluated region")
+
+(defface lambdapi-proof-error-face
+  '((((background dark)) :background "brown")
+    (((background light)) :background "indianred1"))
+  "Face for regions with error")
+
+(defun lp-highlight-till (pos)
+  "Highlight till pos"
+  (save-restriction
+    (widen)
+    (hlt-unhighlight-region (point-min) (point-max) 'lambdapi-proof-face)
+    (hlt-highlight-region
+     (point-min) (min (1+ pos) (point-max))
+     'lambdapi-proof-face)))
+
+(defun lp-focus-goal (goalno &optional proofbuf proofpos)
   "Focus on 'goalno'th goal (zero-indexed). proofbuf is the buffer
 containing the corresponding proof for *Goals* buffer"
   (interactive "nEnter Goal Number: ")
   (if (null proofbuf)
-      (setq proofbuf (current-buffer)))
+      (setq proofbuf (plist-get proof-line-position :buffer)
+            proofpos (plist-get proof-line-position :pos)))
   (select-window (get-buffer-window proofbuf))
   (with-current-buffer proofbuf
-    (goto-line (or lineNo (plist-get proof-line-position :line)))
-    (goto-char (line-end-position))
+    (goto-char proofpos)
     ;; Don't focus the goalno 0
     (if (not (eq goalno 0))
         (progn
-          (newline)
-          (insert (format "focus %S" goalno))
+          (forward-char)
+          (insert (format "\nfocus %S;" goalno))
           (smie-indent-line)
           (eglot--signal-textDocument/didChange)
-          ;; if interactive mode is on move the proofline
-          ;; to the newly inserting focus k
-          (if interactive-goals
-              (move-proof-line `(lambda (_) ,(1+  lineNo)))
-            (progn
-              ;; otherwise just update the goals
-              ;; we need to call interactively because we
-              ;; need its behaviour similar to C-c C-c by user i.e.
-              ;; to display goals without changing proof-line-position
-              (call-interactively #'lp-display-goals)))))))
+          (lp-move-proof-line `(lambda (_) ,(1- (point))))))))
 
-(defun lp-make-goal-clickable (goalstr goalNo proofbuf lineNo)
+(defun lp-make-goal-clickable (goalstr goalNo proofbuf proofpos)
   "Returns goalstr with text properties added making the string call
 lp-focus-goal with goalNo and proofbuf on click. Also makes the string
 bold if goalNo is 0"
@@ -46,72 +59,73 @@ bold if goalNo is 0"
     (define-key goalkeymap [mouse-1]
       `(lambda ()
          (interactive)
-         (lp-focus-goal ,goalNo ,proofbuf ,lineNo)))
+         (lp-focus-goal ,goalNo ,proofbuf ,proofpos)))
     (add-text-properties
      0 (length goalstr)
      `(face      ,(pcase goalNo
                     (0 'bold)
                     (_ 'default))
-       mouse-face highlight
-       help-echo ,(pcase goalNo
-                    (0 "current goal")
-                    (_ "click to focus"))
-       keymap    ,goalkeymap)
+                 mouse-face highlight
+                 help-echo ,(pcase goalNo
+                              (0 "current goal")
+                              (_ "click to focus"))
+                 keymap    ,goalkeymap)
      goalstr)
     goalstr))
 
 (defun lp-format-string-hyps-goal (goal)
   "Return the string associated to the hypotheses of a single typing goal"
   (let ((tog (plist-get goal :typeofgoal)))
-	(let ((hs (plist-get goal :hyps)))
-	  (mapcar (lambda (hyp)
-		    (let ((name (plist-get hyp :hname))
-			  (type (plist-get hyp :htype)))
-		      (format "%s: %s\n" name type)))
-		  (reverse hs)))))
+    (let ((hs (plist-get goal :hyps)))
+      (mapcar (lambda (hyp)
+                (let ((name (plist-get hyp :hname))
+                      (type (plist-get hyp :htype)))
+                  (format "%s: %s\n" name type)))
+              (reverse hs)))))
 
-(defun lp-format-string-goal (goal goalNo proofbuf proofline)
+;; TODO : replace proofline
+(defun lp-format-string-goal (goal goalNo proofbuf proofpos)
   "Return the string associated to a single goal. Adds text-properties to
 make it clickable"
   (let ((tog (plist-get goal :typeofgoal)))
     (if (string= tog "Typ")
-	(let* ((id (plist-get goal :gid))
+        (let* ((id (plist-get goal :gid))
                (type (plist-get goal :type))
                (clk-text (lp-make-goal-clickable
                           (format "%s: %s" id type)
-                          goalNo proofbuf proofline)))
-	  (format "%s\n%s\n\n"
-		  lp-goal-line-prefix clk-text))
+                          goalNo proofbuf proofpos)))
+          (format "%s\n%s\n\n"
+                  lp-goal-line-prefix clk-text))
       (let* ((constr (plist-get goal :constr))
              (clk-text (lp-make-goal-clickable
                         (format "%s" constr)
-                        goalNo proofbuf proofline)))
+                        goalNo proofbuf proofpos)))
         (format "%s\n%s\n\n"
                 lp-goal-line-prefix clk-text)))))
 
-(defun display-goals (goals)
+(defun lp-display-goals (goals)
   "Display GOALS returned by the LSP server in the dedicated Emacs buffer."
-  (let ((goalsbuf   (get-buffer-create "*Goals*"))
-        (proofbuf   (plist-get proof-line-position :buffer))
-        (proofline  (plist-get proof-line-position :line)))
-        (with-current-buffer goalsbuf
-          (read-only-mode -1)
-          (if (> (length goals) 0)
-              (let* ((fstgoal (elt goals 0))
-                     (hypsstr (lp-format-string-hyps-goal fstgoal))
-                     ;; map each goal to formatted goal string
-                     (goalsstr (cl-mapcar `(lambda (goal goalNo)
-                                             (lp-format-string-goal
-                                              goal goalNo ,proofbuf ,proofline))
-                                          goals
-                                          (cl-loop for x below (length goals)
-                                                   collect x))))
-                (erase-buffer)
-                (goto-char (point-max))
-                (mapc 'insert hypsstr)
-                (mapc 'insert goalsstr))
-            (erase-buffer))
-          (read-only-mode 1))))
+  (let ((goalsbuf (get-buffer-create "*Goals*"))
+        (proofbuf (plist-get proof-line-position :buffer))
+        (proofpos (plist-get proof-line-position :pos)))
+    (with-current-buffer goalsbuf
+      (read-only-mode -1)
+      (if (> (length goals) 0)
+          (let* ((fstgoal (elt goals 0))
+                 (hypsstr (lp-format-string-hyps-goal fstgoal))
+                 ;; map each goal to formatted goal string
+                 (goalsstr (cl-mapcar `(lambda (goal goalNo)
+                                         (lp-format-string-goal
+                                          goal goalNo ,proofbuf ,proofpos))
+                                      goals
+                                      (cl-loop for x below (length goals)
+                                               collect x))))
+            (erase-buffer)
+            (goto-char (point-max))
+            (mapc 'insert hypsstr)
+            (mapc 'insert goalsstr))
+        (erase-buffer))
+      (read-only-mode 1))))
 
 (defun lp-display-logs (logs)
   "Display logs in *lp-logs* buffer"
@@ -120,8 +134,8 @@ make it clickable"
       (read-only-mode +1)
       (with-silent-modifications
         (set (make-local-variable 'window-point-insertion-type) t)
-	(erase-buffer)
-	(insert logs)
+        (erase-buffer)
+        (insert logs)
         ;; TODO: fix performance issue
         ;; See: https://emacs.stackexchange.com/a/38608/30239
         (let ((ansi-color-apply-face-function
@@ -140,18 +154,17 @@ make it clickable"
             (beginning-of-line)
             (recenter -1))))))
 
-
 (defun eglot--signal-proof/goals (position)
   "Send proof/goals to server, requesting the list of goals at POSITION."
   (let ((server (eglot-current-server))
         (params `(:textDocument ,(eglot--TextDocumentIdentifier)
-                  :position ,position)))
+                                :position ,position)))
     (if server
         (let ((response (jsonrpc-request server :proof/goals params)))
           (if response
               (progn
-		(display-goals (plist-get response :goals))
-		(lp-display-logs (plist-get response :logs)))
+                (lp-display-goals (plist-get response :goals))
+                (lp-display-logs (plist-get response :logs)))
             (let ((goalsbuf (get-buffer-create "*Goals*"))
                   (logsbuf (get-buffer-create "*lp-logs*")))
               (with-current-buffer goalsbuf
@@ -163,86 +176,172 @@ make it clickable"
                 (erase-buffer)
                 (read-only-mode 1))))))))
 
-(defun lp-display-goals ()
-  "Display goals at cursor position."
-  (interactive)
-  (if (called-interactively-p 'any)
-      (let ((temp-pfline proof-line-position))
-        (setq proof-line-position (list :line (1+ (current-line)) :character 0
-                                        :buffer (current-buffer)))
-        (eglot--signal-proof/goals (eglot--pos-to-lsp-position))
-        (setq proof-line-position temp-pfline))
-    (eglot--signal-proof/goals (eglot--pos-to-lsp-position))))
-
-(defvar-local proof-line-position (list :line 0 :character 0 :buffer nil))
+(defvar-local proof-line-position (list :pos 0 :buffer nil))
 (defvar-local interactive-goals nil)
 
-(defun move-proof-line (move-fct)
-  (save-excursion
-    (let ((line (plist-get proof-line-position :line)))
-      (setq proof-line-position
-            (eglot--widening (list :line (funcall move-fct line) :character 0
-                                   :buffer (current-buffer))))
-      (goto-line line)
-      (hlt-unhighlight-region 0 (point-max))
-      (goto-line (funcall move-fct line))
-      (hlt-highlight-region 0 (1+ (line-end-position)))
-      (lp-display-goals))))
+(defun lp-move-proof-line (move-fct)
+  (let* ((pos (plist-get proof-line-position :pos))
+         (npos (funcall move-fct pos)))
+    (setq proof-line-position
+          (list :pos npos :buffer (current-buffer)))
+    (lp-prove-till npos)))
 
-(defun lp-get-next-proof-line (lineNo)
-  "returns line number of next proof"
+(defun lp-get-next-proof-pos (pos)
+  "returns position of next proof"
   (save-excursion
-    (goto-line (1+ lineNo))
-    (if (not (search-forward "begin" nil t))
-	(goto-char (point-max)))
-    (line-number-at-pos)))
+    (goto-char pos)
+    (if (search-forward "begin" nil t)
+        (1- (point))
+      pos)))
 
-(defun lp-get-prev-proof-line (lineNo)
-  "returns line number of previous proof"
+(defun lp-get-prev-proof-pos (pos)
+  "returns position of previous proof"
   (save-excursion
-    (goto-line lineNo)
-    (if (not (search-backward "begin" nil t))
-	(goto-char (point-min)))
-    (line-number-at-pos)))
+    (goto-char pos)
+    (if (search-backward "begin" nil t)
+        (+ (length "begin") (point) -1)
+      0)))
 
 (defun lp-jump-proof-forward ()
   "Move the proof cursor to the next proof"
   (interactive)
-  (move-proof-line #'lp-get-next-proof-line))
+  (lp-move-proof-line #'lp-get-next-proof-pos)
+  (recenter))
 
 (defun lp-jump-proof-backward ()
   "Move the proof cursor to the previous proof"
   (interactive)
-  (move-proof-line #'lp-get-prev-proof-line))
+  (lp-move-proof-line #'lp-get-prev-proof-pos)
+  (recenter))
 
 (defun lp-proof-forward ()
   "Move the proof cursor forward."
   (interactive)
-  (move-proof-line #'1+))
+  (lp-move-proof-line #'lp--next-command-pos)
+  (recenter))
 
 (defun lp-proof-backward ()
   "Move the proof cursor backward."
   (interactive)
-  (move-proof-line #'1-))
+  (lp-move-proof-line #'lp--prev-command-pos)
+  (recenter))
+
+(defun lp-prove-till-cursor ()
+  "Proves till the last complete command before cursor"
+  (interactive)
+  (save-excursion
+    (lp-prove-till
+     (lp--prev-command-pos
+      (min (point-max) (1+ (point)))))))
+
+(defun lp-prove-till (pos)
+  "Evaluates till pos and moves the cursor to the end of evaluated region"
+  (lp-highlight-till pos)
+  (setq proof-line-position
+        (list :pos pos :buffer (current-buffer)))
+  (goto-char pos)
+  (eglot--signal-proof/goals (eglot--pos-to-lsp-position)))
 
 (defun toggle-interactive-goals ()
   "Toggle the goals display between two modes: interactive and step by step."
   (interactive)
   (setq interactive-goals (not interactive-goals))
   (save-excursion
-    (let ((line (plist-get proof-line-position :line)))
-      (if interactive-goals
-          (progn
-              (setq proof-line-position
-                    (eglot--widening
-                     (list :line (line-number-at-pos) :character 0)))
-              (goto-line (line-number-at-pos))
-              (hlt-highlight-region 0 (1+ (line-end-position)))
-              (lp-display-goals))
-        (goto-line line)
-        (hlt-unhighlight-region 0 (point-max)))))
+    (if interactive-goals
+        (let ((prev-cmd-pos (lp--prev-command-pos)))
+          (lp-prove-till prev-cmd-pos))
+      (hlt-unhighlight-region 0 (point-max))))
+  ;; update the tool-bar icon
+  (define-key lambdapi-mode-map [tool-bar toggle-interactive-goals]
+    `(menu-item "Interactive" toggle-interactive-goals
+                :image
+                (image :type xpm :file ,(if interactive-goals
+                                            "connect.xpm" "disconnect.xpm"))
+                :help "Toggle interactive mode"))
+  (force-mode-line-update)
   (message (format "Interactive mode is %s"
                    (if interactive-goals "ON" "OFF"))))
+
+(defun lp--in-comment-p (&optional pos)
+  "Returns t if character at pos is in a comment.
+If pos is nil use (point)"
+  (unless pos (setq pos (point)))
+  (nth 4 (syntax-ppss)))
+
+(defun lp--prev-command-pos (&optional pos)
+  "Returns the position of the previous command's terminator
+and 0 if there is no previous command"
+  (unless pos (setq pos (point)))
+  (save-excursion
+    (let ((term-regex
+           (mapconcat
+            (lambda (s) (format "\\(%s\\)" s))
+            lambdapi-terminators "\\|")))
+      (goto-char pos)
+      (while
+          (progn
+            (setq pos (re-search-backward term-regex nil t))
+            (and pos (lp--in-comment-p pos)))
+        (goto-char pos))
+      (if pos
+          (let ((match-len ; length of matched terminator
+                 (seq-some
+                  (lambda (term)
+                    (if (looking-at-p term)
+                        (length term)
+                      nil))
+                  lambdapi-terminators)))
+            (1- (+ pos match-len)))
+        0))))
+
+(defun lp--next-command-pos (&optional pos)
+  "Returns the position of the next command's terminator
+and pos if there is no next command"
+  (setq npos (1+ (or pos (point))))
+  
+  (save-excursion
+    (let ((term-regex
+           (mapconcat
+            (lambda (s) (format "\\(%s\\)" s))
+            lambdapi-terminators "\\|")))
+      (goto-char npos)
+      (while
+          (progn
+            (setq npos (re-search-forward term-regex nil t))
+            (and npos (lp--in-comment-p npos)))
+        (goto-char npos))
+      (if npos (max (point-min) (1- npos)) pos))))
+
+(defun lp--post-self-insert-function ()
+  (if interactive-goals
+      (if (and (not (lp--in-comment-p))
+               (seq-find
+                (lambda (term)
+                  (message
+                   (format "term:%S substr: %S res : %S"
+                           term
+                           (buffer-substring-no-properties
+                            (max (point-min) (- (point) (length term)))
+                            (point))
+                           (equal (buffer-substring-no-properties
+                                   (max (point-min) (- (point) (length term)))
+                                   (point))
+                                  term)))
+                  (equal (buffer-substring-no-properties
+                          (max (point-min) (- (point) (length term)))
+                          (point))
+                         term))
+                lambdapi-terminators))
+          (progn
+            (eglot--signal-textDocument/didChange)
+            (lp-prove-till (point))))))
+
+(defun lp--after-change-function (beg end len)
+  (if (<= beg (plist-get proof-line-position :pos))
+      (progn
+        (eglot--signal-textDocument/didChange)
+        (lp-prove-till
+         (lp--prev-command-pos beg)))))
 
 (provide 'lambdapi-proofs)
 ;;; lambdapi-proofs.el ends here
