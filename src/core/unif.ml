f128c64fd6ef53a16b8af923f93a5d8161f3ade4
Frédéric Blanqui <frederic.blanqui@inria.fr>
2021-03-05 17:28:48 +0100

    details

--- src/core/unif.ml
+++ src/core/unif.ml
@@ -146,9 +146,9 @@ let instantiate : ctxt -> meta -> term array -> term -> constr list -> bool =
           match Infer.check_noexn [] ctx u typ_mts with
           | None ->
               if !log_enabled then log_unif "typing condition failed"; false
+          | Some [] -> do_instantiate()
           | Some cs ->
-              let is_eq c c' = LibTerm.cmp_constr c c' = 0 in
-              let is_initial c = List.exists (is_eq c) initial in
+              let is_initial c = List.exists (LibTerm.eq_constr c) initial in
               let cs = List.filter (fun c -> not (is_initial c)) cs in
               if cs = [] then do_instantiate()
               else
@@ -397,7 +397,7 @@ let imitate_prod : ctxt -> meta -> term -> term -> problem -> problem =
 
 (** [sym_sym_whnf ctx t1 s1 ts1 t2 s2 ts2 p] handles the case [s1(ts1) =
    s2(ts2); p] when [s1(ts1)] and [s2(ts2)] are in whnf. *)
-let sym_sym :
+let sym_sym_whnf :
       ctxt -> term -> sym -> term list -> term -> sym -> term list -> problem
       -> problem =
   fun ctx t1 s1 ts1 t2 s2 ts2 p ->
@@ -426,129 +426,128 @@ let rec solve : problem -> constr list = fun p ->
   | {to_solve = []; unsolved = cs; recompute = true} ->
      solve {empty_problem with to_solve = cs}
   | {to_solve = []; unsolved = cs; _} -> cs
-  | {to_solve = ((ctx,t1,t2)::to_solve as initial); _} ->
+  | {to_solve = (((ctx,t1,t2)::to_solve) as initial); _} ->
 
   (* We remove the first constraint from [p] for not looping. *)
   let p = {p with to_solve} in
 
-  let (h1, ts1) = LibTerm.get_args t1 in
-  let (h2, ts2) = LibTerm.get_args t2 in
+  let (h1, ts1) = LibTerm.get_args t1
+  and (h2, ts2) = LibTerm.get_args t2 in
 
-  match (h1, h2) with
-  | (Type, Type)
-  | (Kind, Kind) -> solve p
+  match h1, h2 with
+  | Type, Type
+  | Kind, Kind -> solve p
 
-  | (Prod(a1,b1), Prod(a2,b2))
-  | (Abst(a1,b1), Abst(a2,b2)) ->
+  | Prod(a1,b1), Prod(a2,b2)
+  | Abst(a1,b1), Abst(a2,b2) ->
      let (x,b1,b2) = Bindlib.unbind2 b1 b2 in
      let ctx' = (x,a1,None) :: ctx in
      (* [ts1] and [ts2] must be empty because of typing. *)
      solve {p with to_solve = (ctx,a1,a2)::(ctx',b1,b2)::to_solve}
 
-  | (Vari(x1), Vari(x2)) when Bindlib.eq_vars x1 x2 ->
+  | Vari x1, Vari x2 when Bindlib.eq_vars x1 x2 ->
       solve (decompose ctx t1 ts1 t2 ts2 p)
 
-  | (Symb(s1), Symb(s2))
+  | Symb s1, Symb s2
        when s1 == s2 && s1.sym_prop <> Defin && List.same_length ts1 ts2 ->
       solve (decompose ctx t1 ts1 t2 ts2 p)
-  | (Symb(s1), Symb(s2))
+  | Symb s1, Symb s2
        when s1 != s2 && s1.sym_prop = Const && s2.sym_prop = Const ->
       error t1 t2
 
-  | (Meta(m,ts), _) when ts1 = [] && instantiate ctx m ts t2 initial ->
+  | Meta(m,ts), _ when ts1 = [] && instantiate ctx m ts t2 initial ->
      solve {p with recompute = true}
-  | (_, Meta(m,ts)) when ts2 = [] && instantiate ctx m ts t1 initial ->
+  | _, Meta(m,ts) when ts2 = [] && instantiate ctx m ts t1 initial ->
      solve {p with recompute = true}
 
-  | (Meta(m,ts), Prod _)
+  | Meta(m,ts), Prod _
        when ts1 = [] && instantiation ctx m ts h2 <> None ->
       solve (imitate_prod ctx m h1 h2 p)
-  | (Prod _, Meta(m,ts))
+  | Prod _, Meta(m,ts)
        when ts2 = [] && instantiation ctx m ts h1 <> None ->
       solve (imitate_prod ctx m h1 h2 p)
 
-  | (Meta(m,_), _) when imitate_lam_cond h1 ts1 ->
+  | Meta(m,_), _ when imitate_lam_cond h1 ts1 ->
       let p = imitate_lam ctx m p in
       solve {p with to_solve = (ctx,t1,t2)::p.to_solve}
-  | (_, Meta(m,_)) when imitate_lam_cond h2 ts2 ->
+  | _, Meta(m,_) when imitate_lam_cond h2 ts2 ->
       let p = imitate_lam ctx m p in
       solve {p with to_solve = (ctx,t1,t2)::p.to_solve}
 
-  | (Type, (Kind|Prod(_)|Symb(_)|Vari(_)|Abst(_)))
-  | (Kind, (Type|Prod(_)|Symb(_)|Vari(_)|Abst(_)))
-  | (Prod(_), (Type|Kind|Vari(_)))
-  | (Vari(_), (Type|Kind|Vari(_)|Prod(_)))
+  | Type, (Kind|Prod _|Symb _|Vari _|Abst _)
+  | Kind, (Type|Prod _|Symb _|Vari _|Abst _)
+  | Prod _, (Type|Kind|Vari _)
+  | Vari _, (Type|Kind|Vari _|Prod _)
     -> error t1 t2
 
-  | (_, _) ->
+  | _, _ ->
 
   (* We reduce [t1] and [t2] and try again. *)
-  let t1 = Eval.whnf ctx t1 in
-  let t2 = Eval.whnf ctx t2 in
+  let t1 = Eval.whnf ctx t1 and t2 = Eval.whnf ctx t2 in
+  let (h1, ts1) = LibTerm.get_args t1
+  and (h2, ts2) = LibTerm.get_args t2 in
   let initial = (ctx,t1,t2)::to_solve in
-  let (h1, ts1) = LibTerm.get_args t1 in
-  let (h2, ts2) = LibTerm.get_args t2 in
 
-  match (h1, h2) with
-  | (Type, Type)
-  | (Kind, Kind) -> solve p
+  match h1, h2 with
+  | Type, Type
+  | Kind, Kind -> solve p
 
-  | (Prod(a1,b1), Prod(a2,b2))
-  | (Abst(a1,b1), Abst(a2,b2)) ->
+  | Prod(a1,b1), Prod(a2,b2)
+  | Abst(a1,b1), Abst(a2,b2) ->
      let (x,b1,b2) = Bindlib.unbind2 b1 b2 in
      let ctx' = (x,a1,None) :: ctx in
      (* [ts1] and [ts2] must be empty because of typing. *)
      solve {p with to_solve = (ctx,a1,a2)::(ctx',b1,b2)::to_solve}
 
-  | (Vari(x1), Vari(x2)) when Bindlib.eq_vars x1 x2 ->
+  | Vari x1, Vari x2 when Bindlib.eq_vars x1 x2 ->
       solve (decompose ctx t1 ts1 t2 ts2 p)
 
-  | (Symb(s1), Symb(s2)) -> solve (sym_sym ctx t1 s1 ts1 t2 s2 ts2 p)
+  | Symb s1, Symb s2 -> solve (sym_sym_whnf ctx t1 s1 ts1 t2 s2 ts2 p)
 
-  | (Meta(m,ts), _) when ts1 = [] && instantiate ctx m ts t2 initial ->
+  | Meta(m,ts), _ when ts1 = [] && instantiate ctx m ts t2 initial ->
      solve {p with recompute = true}
-  | (_, Meta(m,ts)) when ts2 = [] && instantiate ctx m ts t1 initial ->
+  | _, Meta(m,ts) when ts2 = [] && instantiate ctx m ts t1 initial ->
      solve {p with recompute = true}
 
-  | (Meta(m,ts), Prod _)
+  | Meta(m,ts), Prod _
        when ts1 = [] && instantiation ctx m ts h2 <> None ->
       solve (imitate_prod ctx m h1 h2 p)
-  | (Prod _, Meta(m,ts))
+  | Prod _, Meta (m,ts)
        when ts2 = [] && instantiation ctx m ts h1 <> None ->
       solve (imitate_prod ctx m h1 h2 p)
 
-  | (Meta(m,_), _) when imitate_lam_cond h1 ts1 ->
+  | Meta(m,_), _ when imitate_lam_cond h1 ts1 ->
       let p = imitate_lam ctx m p in
       solve {p with to_solve = (ctx,t1,t2)::p.to_solve}
-  | (_, Meta(m,_)) when imitate_lam_cond h2 ts2 ->
+  | _, Meta(m,_) when imitate_lam_cond h2 ts2 ->
       let p = imitate_lam ctx m p in
       solve {p with to_solve = (ctx,t1,t2)::p.to_solve}
 
-  | (Meta(m,ts), Symb(s)) ->
+  | Meta(m,ts), Symb s ->
       if imitate_inj ctx m ts ts1 s ts2 then
         solve {p with to_solve = (ctx,t1,t2)::to_solve}
       else solve (add_to_unsolved ctx t1 t2 p)
-  | (Symb(s), Meta(m,ts)) ->
+  | Symb s, Meta(m,ts) ->
       if imitate_inj ctx m ts ts2 s ts1 then
         solve {p with to_solve = (ctx,t1,t2)::to_solve}
       else solve (add_to_unsolved ctx t1 t2 p)
 
-  | (Meta _, _)
-  | (_, Meta _) -> solve (add_to_unsolved ctx t1 t2 p)
+  | Meta _, _
+  | _, Meta _ -> solve (add_to_unsolved ctx t1 t2 p)
 
-  | (Symb(s), _) when not (is_constant s) ->
+  | Symb s, _ when not (is_constant s) ->
       solve (add_inverse ctx t1 s ts1 t2 p)
-  | (_, Symb(s)) when not (is_constant s) ->
+  | _, Symb s when not (is_constant s) ->
       solve (add_inverse ctx t2 s ts2 t1 p)
 
-  | (Type, (Kind|Prod(_)|Symb(_)|Vari(_)|Abst(_)))
-  | (Kind, (Type|Prod(_)|Symb(_)|Vari(_)|Abst(_)))
-  | (Prod(_), (Type|Kind|Vari(_)|Abst(_)))
-  | (Vari(_), (Type|Kind|Vari(_)|Prod(_)))
-  | (Abst(_), (Type|Kind|Prod(_)))
+  | Type, (Kind|Prod _|Symb _|Vari _|Abst _)
+  | Kind, (Type|Prod _|Symb _|Vari _|Abst _)
+  | Prod _, (Type|Kind|Vari _|Abst _)
+  | Vari _, (Type|Kind|Vari _|Prod _)
+  | Abst _, (Type|Kind|Prod _)
     -> error t1 t2
 
-  | (_, _) -> solve (add_to_unsolved ctx t1 t2 p)
+  | _, _ -> solve (add_to_unsolved ctx t1 t2 p)
 
 (** [solve p] tries to solve the unification problem [p] and
     returns the constraints that could not be solved.
