15de9f64cd0408052d9186435160092c4d435db2
yannl35133 <40719961+yannl35133@users.noreply.github.com>
2021-05-12 07:42:36 +0200

    Don't colourise newlines (#668)

--- src/handle/command.ml
+++ src/handle/command.ml
@@ -135,7 +135,7 @@ let handle_rule : sig_state -> p_rule -> sym = fun ss r ->
       pp_sym sym;
   let rule = Tool.Sr.check_rule pr in
   Sign.add_rule ss.signature sym rule;
-  Console.out 3 (red "(rule) add %a\n") pp_rule (sym, rule);
+  Console.out 3 (red "(rule) add %a" ^^ "\n") pp_rule (sym, rule);
   sym
 
 (** [handle_rules ss rs] handles the rules [rs] in signature state [ss], and
@@ -171,7 +171,7 @@ let handle_inductive_symbol : sig_state -> expo -> prop -> match_strat
     (fatal_msg "The type of %a has unsolved metavariables.\n" pp_uid name;
      fatal pos "We have %a : %a." pp_uid name pp_term typ);
   (* Actually add the symbol to the signature and the state. *)
-  Console.out 3 (red "(symb) %a : %a\n") pp_uid name pp_term typ;
+  Console.out 3 (red "(symb) %a : %a" ^^ "\n") pp_uid name pp_term typ;
   let r = add_symbol ss expo prop mstrat false id typ impl None in
   Print.sig_state := fst r; r
 
@@ -292,7 +292,7 @@ let get_proof_data : compiler -> sig_state -> p_command ->
         if Sign.mem ss.signature rec_name then
           fatal pos "Symbol %a already exists." pp_uid rec_name;
         let (ss, rec_sym) =
-          Console.out 3 (red "(symb) %a : %a\n")
+          Console.out 3 (red "(symb) %a : %a" ^^ "\n")
             pp_uid rec_name pp_term rec_typ;
           let id = Pos.make pos rec_name in
           let r = add_symbol ss expo Defin Eager false id rec_typ [] None
@@ -443,7 +443,7 @@ let get_proof_data : compiler -> sig_state -> p_command ->
                   in List.fold_left admit_goal ss ps.proof_goals
             in
             (* Add the symbol in the signature with a warning. *)
-            Console.out 3 (red "(symb) add %a : %a\n")
+            Console.out 3 (red "(symb) add %a : %a" ^^ "\n")
               pp_uid id pp_term a;
             wrn pe.pos "Proof admitted.";
             let t = Option.map (fun t -> t.elt) t in
@@ -454,7 +454,7 @@ let get_proof_data : compiler -> sig_state -> p_command ->
               (Console.out 1 "%a" Proof.pp_goals ps;
                fatal pe.pos "The proof is not finished.");
             (* Add the symbol in the signature. *)
-            Console.out 3 (red "(symb) add %a : %a\n") pp_uid id pp_term a;
+            Console.out 3 (red "(symb) add %a : %a" ^^ "\n") pp_uid id pp_term a;
             let t = Option.map (fun t -> t.elt) t in
             fst (add_symbol ss expo prop mstrat opaq p_sym_nam a impl t)
       in
