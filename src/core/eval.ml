1d7fa606b91f1a2b151a9c6c3189e11a673e7e5a
Frédéric Blanqui <frederic.blanqui@inria.fr>
2021-05-31 19:02:03 +0200

    details

--- src/core/eval.ml
+++ src/core/eval.ml
@@ -134,12 +134,12 @@ let appl_to_tref : term -> term = fun t ->
 
 (** [whnf c t] computes a whnf of the term [t] wrt configuration [c]. *)
 let rec whnf : config -> term -> term = fun c t ->
-  if !log_enabled then log_eval "whnf %a" pp_term t;
+  (*if !log_enabled then log_eval "whnf %a" pp_term t;*)
   let s = Stdlib.(!steps) in
   let u, stk = whnf_stk c t [] in
   let r = if Stdlib.(!steps) <> s then add_args u stk else unfold t in
-  if !log_enabled then
-    log_eval "whnf %a%a = %a" pp_ctxt c.context pp_term t pp_term r;
+  (*if !log_enabled then
+    log_eval "whnf %a%a = %a" pp_ctxt c.context pp_term t pp_term r;*)
   r
 
 (** [whnf_stk l c t stk] computes a whnf of [add_args t stk] wrt
@@ -406,8 +406,8 @@ let whnf : ?rewrite:bool -> ctxt -> term -> term = fun ?(rewrite=true) c t ->
   Stdlib.(steps := 0);
   let u = whnf (cfg_of_ctx c rewrite) t in
   let r = if Stdlib.(!steps = 0) then unfold t else u in
-  (*if !log_enabled then
-    log_eval "whnf %a%a = %a" pp_ctxt c.context pp_term t pp_term r;*)
+  if !log_enabled then
+    log_eval "whnf %a%a = %a" pp_ctxt c pp_term t pp_term r;
   r
 
 (** [simplify t] computes a beta whnf of [t] belonging to the set S such that:
