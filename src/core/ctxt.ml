8393828208b4e65f0086bc46d053bec401b982af
Frédéric Blanqui <frederic.blanqui@inria.fr>
2021-11-30 18:58:39 +0100

    detail

--- src/core/ctxt.ml
+++ src/core/ctxt.ml
@@ -64,7 +64,7 @@ let sub : ctxt -> tvar array -> ctxt = fun ctx vs ->
   in
   List.fold_right f ctx []
 
-(** [unfold ctx t] behaves like {!val:Term.unfold} unless term[t] is of the
+(** [unfold ctx t] behaves like {!val:Term.unfold} unless term [t] is of the
     form [Vari(x)] with [x] defined in [ctx]. In this case, [t] is replaced by
     the definition of [x] in [ctx].  In particular, if no operation is carried
     out on [t], we have [unfold ctx t == t]. *)
