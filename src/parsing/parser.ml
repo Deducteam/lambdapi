e1cb5e51d3893cbef3248c42b441cc4a6c7d2299
Frédéric Blanqui <frederic.blanqui@inria.fr>
2021-12-15 10:40:21 +0100

    fix error message in Parser.qident/path_of_string (fix #577)

--- src/parsing/parser.ml
+++ src/parsing/parser.ml
@@ -132,10 +132,9 @@ let path_of_string : string -> Path.t = fun s ->
     begin match token lb () with
       | UID s, _, _ -> [s]
       | QID p, _, _ -> List.rev p
-      | _ -> Error.fatal_no_pos "Invalid path."
+      | _ -> Error.fatal_no_pos "\"%s\" is not a path." s
     end
-  with SyntaxError {elt;_} ->
-    Error.fatal_no_pos "Syntax error in path: %s." elt
+  with SyntaxError _ -> Error.fatal_no_pos "\"%s\" is not a path." s
 
 (** [qident_of_string s] converts the string [s] into a qident. *)
 let qident_of_string : string -> Core.Term.qident = fun s ->
@@ -143,10 +142,9 @@ let qident_of_string : string -> Core.Term.qident = fun s ->
   let lb = Sedlexing.Utf8.from_string s in
   try
     begin match token lb () with
-      | UID s, _, _ -> ([], s)
       | QID [], _, _ -> assert false
       | QID (s::p), _, _ -> (List.rev p, s)
-      | _ -> Error.fatal_no_pos "Invalid qualified identifier."
+      | _ -> Error.fatal_no_pos "\"%s\" is not a qualified identifier." s
     end
-  with SyntaxError {elt;_} ->
-    Error.fatal_no_pos "Syntax error in qualified identifier: %s." elt
+  with SyntaxError _ ->
+    Error.fatal_no_pos "\"%s\" is not a qualified identifier." s
