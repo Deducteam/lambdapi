6c799658f9898a236b0dbd07af728065cb6d1660
gabrielhdt <gabrielhondet@gmail.com>
2021-12-17 08:45:40 +0100

    Add regression tests for tools
    Test precisely the output to HRS, XTC and the decision tree export

---
+++ tests/regressions/hrs.ml
@@ -0,0 +1,10 @@
+(** HRS file generation *)
+open Common
+
+open Handle
+
+let () =
+  Library.set_lib_root (Some "/tmp");
+  Timed.(Console.verbose := 0);
+  let sign = Compile.Pure.compile_file "../OK/bool.lp" in
+  Tool.Hrs.to_HRS Format.std_formatter sign
