2c965af3a5090092f13f3f984f9f975140cb0243
Frédéric Blanqui <frederic.blanqui@inria.fr>
2021-12-23 19:14:17 +0100

    in src/export/ add dune and dk.ml

---
+++ src/export/dk.ml
@@ -0,0 +1,74 @@
+(** Export a Lambdapi signature to Dedukti. *)
+
+open Lplib.Base
+open Common
+open Core
+open Timed
+
+(** Dedukti keywords. *)
+let keyword_table = Hashtbl.create 59
+
+let is_keyword : string -> bool = Hashtbl.mem keyword_table
+
+let _ = let open Parsing.DkTokens in
+  let loc = Lexing.dummy_pos, Lexing.dummy_pos in
+  List.iter (fun (s, t) -> Hashtbl.add keyword_table s t)
+    [ ".", DOT
+    ; ",", COMMA
+    ; ":", COLON
+    ; "==", EQUAL
+    ; "[", LEFTSQU
+    ; "]", RIGHTSQU
+    ; "{", LEFTBRA
+    ; "}", RIGHTBRA
+    ; "(", LEFTPAR
+    ; ")", RIGHTPAR
+    ; "-->", LONGARROW
+    ; "->", ARROW
+    ; "=>", FATARROW
+    ; ":=", DEF
+    ; "_", UNDERSCORE loc
+    ; "Type", TYPE loc
+    ; "def", KW_DEF loc
+    ; "defac", KW_DEFAC loc
+    ; "defacu", KW_DEFACU loc
+    ; "injective", KW_INJ loc
+    ; "thm", KW_THM loc
+    ; "private", KW_PRV loc
+    ; "#NAME", NAME (loc, "")
+    ; "#REQUIRE", REQUIRE (loc, "")
+    ; "#EVAL", EVAL loc
+    ; "#INFER", INFER loc
+    ; "#CHECK", CHECK loc
+    ; "#CHECKNOT", CHECKNOT loc
+    ; "#ASSERT", ASSERT loc
+    ; "#ASSERTNOT", ASSERTNOT loc
+    ; "#PRINT", PRINT loc
+    ; "#GDT", GDT loc]
+
+let out = Format.fprintf
+
+let string = Format.pp_print_string
+
+let is_ident : string -> bool = fun s ->
+  Parsing.DkLexer.is_ident (Lexing.from_string s)
+
+let escape : string pp = fun ppf s -> out ppf "{|%s|}" s
+
+let ident : string pp = fun ppf s ->
+  if s = "" then escape ppf s
+  else if s.[0] = '{' then string ppf s
+  else if is_keyword s then escape ppf s
+  else if is_ident s then string ppf s
+  else escape ppf s
+
+let path : Path.t pp = fun ppf p ->
+  match p with
+  | [_root_path; m] -> out ppf "%s" m
+  | _ -> assert false
+
+let require : Path.t -> 'a -> unit = fun p _ ->
+  Format.printf "#REQUIRE %a." path p
+
+let sign : Sign.t -> unit = fun s ->
+  Path.Map.iter require !(s.sign_deps)
