4b2b37ecf7b70d511a9d6fa9e53677b6213041f1
Frédéric Blanqui <fblanqui@users.noreply.github.com>
2022-01-15 20:18:26 +0100

    fix export -o lp (ex beautify) (#816)

---
+++ tests/export_lp.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# Test lp export
+
+TIMEFORMAT="%E"
+root=`pwd`
+lambdapi='dune exec -- lambdapi'
+
+outdir=.tmp
+#rm -rf $outdir
+mkdir -p "$outdir/OK/a b"
+cp -f lambdapi.pkg $outdir
+
+# translate lp files
+translate() {
+    cd $root
+    echo translate lp files ...
+    for f in OK/*.lp 'OK/a b/escape file.lp'
+    do
+        out=$outdir/$f
+        echo "$f --> $out ..."
+        $lambdapi export -o lp -w -v 0 "$f" > "$out"
+        if test $? -ne 0; then echo KO; exit 1; fi
+    done
+}
+time translate
+
+# check lp files
+check() {
+    cd $outdir
+    echo check lp files ...
+    $lambdapi check -w -v 0 OK/*.lp
+    if test $? -ne 0; then echo KO; exit 1; fi
+}
+time check
+
+cd $root
+echo OK
