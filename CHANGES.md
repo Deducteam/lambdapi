a205bd0e8ba492a3a784ddd7a97c5167fce50675
Frédéric Blanqui <fblanqui@users.noreply.github.com>
2022-05-09 13:30:03 +0200

    Remove option --recompile and propagate recompile flag in dependencies (#858)
    * compile: propagate ~force
    * remove option --recompile

--- CHANGES.md
+++ CHANGES.md
@@ -3,6 +3,16 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/),
 and this project adheres to [Semantic Versioning](https://semver.org/).
 
+## Unreleased
+
+### Added
+
+- Propagate recompile flag to dependencies.
+
+### Removed
+
+- Option --recompile.
+
 ## 2.2.0 (2022-03-18)
 
 ### Added
--- src/handle/compile.mli
+++ src/handle/compile.mli
@@ -7,19 +7,15 @@ val gen_obj : bool Stdlib.ref
 (** [gen_obj] indicates whether we should generate object files when compiling
     source files. The default behaviour is not to generate them. *)
 
-val recompile : bool Stdlib.ref
-(** [recompile] indicates whether we should recompile files who have an object
-    file that is already up to date. Note that this flag only applies to files
-    that are given on the command line explicitly, not their dependencies. *)
+val compile : ?force:bool -> Path.t -> Sign.t
+(** [compile force mp] compiles module path [mp], forcing
+    recompilation of up-to-date files if [force] is true. *)
 
-val compile : bool -> Path.t -> Sign.t
-(** [compile force mp] compiles module path [mp] using [compile_with], forcing
-    compilation of up-to-date files if [force] is true. *)
-
-val compile_file : string -> Sign.t
-(** [compile_file fname] looks for a package configuration file for
-    [fname] and compiles [fname]. It is the main compiling function. It
-    is called from the main program exclusively. *)
+val compile_file : ?force:bool -> string -> Sign.t
+(** [compile_file force fname] looks for a package configuration file for
+   [fname] and compiles [fname], forcing recompilation of up-to-date files if
+   [force] is true. It is the main compiling function. It is called from the
+   main program exclusively. *)
 
 (** The functions provided in this module perform the same computations as the
    ones defined earlier, but restore the console state and the library
@@ -27,7 +23,7 @@ val compile_file : string -> Sign.t
    state can be passed as argument to change the settings. *)
 module PureUpToSign : sig
   val compile :
-    ?lm:Path.t*string -> ?st:Console.State.t -> bool -> Path.t -> Sign.t
+  ?lm:Path.t*string -> ?st:Console.State.t -> ?force:bool -> Path.t -> Sign.t
   val compile_file :
-    ?lm:Path.t*string -> ?st:Console.State.t -> string -> Sign.t
+  ?lm:Path.t*string -> ?st:Console.State.t -> ?force:bool -> string -> Sign.t
 end
