2f8e741c9fc4c4aa9b779663d3e32d7dc19e487e
Claudio Sacerdoti Coen <claudio.sacerdoticoen@unibo.it>
2023-01-27 17:24:15 +0100

    Generate lp files with requires automatically

--- out/compile.sh
+++ out/compile.sh
@@ -1,9 +1,20 @@
 #!/bin/sh
-set -e
-set -x
+#set -e
+#set -x
 
-export PATH=$PWD/_build/install/default/bin:$PATH
-cd out
-lambdapi check --lib-root . -c coq/cupicef.lp
-lambdapi check --lib-root . -c Coq__Init__Logic.lp
-lambdapi check --lib-root . -c Coq__Init__Datatypes.dk
+#export PATH=$PWD/_build/install/default/bin:$PATH
+
+# add the exports
+mkdir tmp
+for i in *.dk; do cat $i | sed "s/^#.*$//g" > tmp/$i; done
+touch tmp/cupicef.dk
+(cd tmp; for i in *.dk; do ../add_requires.sh $i > $i.imports ; done)
+
+cp cupicef.lp.handmade cupicef.lp
+
+for i in *.dk; do cat header tmp/$i.imports > `basename $i .dk`.lp && lambdapi export -o lp $i >> `basename $i .dk`.lp ; done
+lambdapi check -c cupicef.lp
+lambdapi check -c Coq__Init__Logic.lp
+lambdapi check -c Coq__Init__Datatypes.lp
+
+rm -rf tmp
