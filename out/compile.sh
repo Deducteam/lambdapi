5d965df3e3939e97a8d6d187e0aeb8b1dc5fc61f
Claudio Sacerdoti Coen <claudio.sacerdoticoen@unibo.it>
2023-01-27 18:10:10 +0100

    fix

--- out/compile.sh
+++ out/compile.sh
@@ -6,15 +6,19 @@
 
 # add the exports
 mkdir tmp
+for i in *.dk.gz; do gunzip -k $i; done
 for i in *.dk; do cat $i | sed "s/^#.*$//g" > tmp/$i; done
 touch tmp/cupicef.dk
 (cd tmp; for i in *.dk; do ../add_requires.sh $i > $i.imports ; done)
 
-cp cupicef.lp.handmade cupicef.lp
+# next line is to drop # directives temporarily
+cp tmp/*.dk .
 
 for i in *.dk; do cat header tmp/$i.imports > `basename $i .dk`.lp && lambdapi export -o lp $i >> `basename $i .dk`.lp ; done
+cp cupicef.lp.handmade cupicef.lp
+
 lambdapi check -c cupicef.lp
 lambdapi check -c Coq__Init__Logic.lp
 lambdapi check -c Coq__Init__Datatypes.lp
 
-rm -rf tmp
+rm -rf tmp *.dk
