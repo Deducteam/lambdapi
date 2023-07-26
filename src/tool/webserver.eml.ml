87779c0323c84e30b34eadf860379fb0b3fb0144
Claudio Sacerdoti Coen <claudio.sacerdoticoen@unibo.it>
2023-07-26 16:44:45 +0200

    form improved

--- src/tool/webserver.eml.ml
+++ src/tool/webserver.eml.ml
@@ -5,12 +5,22 @@ let show_form ?output request =
     <h1><a href="https://github.com/Deducteam/lambdapi">LambdaPi</a>
      Search Engine</h1>
 
+    <p>The <b>locate</b> button looks for symbols with a given name.<br>The
+    <b>search</b> button looks for symbols and rules that match a given
+    pattern.<br>The <b>query</b> button answers the query. Read the <a
+    href="https://lambdapi.readthedocs.io/en/latest/query_language.html">query
+    language specification</a> to learn about the query language.</p>
+
     <form method="POST" action="/">
       <%s! Dream.csrf_tag request %>
-      <input name="message" autofocus>
+      <p>
+      <input type="search" required="true" size="100"
+        name="message" autofocus></p>
+      <p>
       <input type="submit" value="locate" name="locate">
       <input type="submit" value="search" name="search">
       <input type="submit" value="query" name="query">
+      </p>
     </form>
 
 %   begin match output with
