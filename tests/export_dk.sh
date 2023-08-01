dc95771f5c8b79ff984c2f88380c706bb9b511bd
Frédéric Blanqui <fblanqui@users.noreply.github.com>
2023-08-01 15:01:49 +0200

    remove dune warnings in export_dk/lp scripts (#1008)

--- tests/export_dk.sh
+++ tests/export_dk.sh
@@ -2,37 +2,44 @@
 
 echo '############ test export -o dk ############'
 
+lambdapi=${LAMBDAPI:-_build/install/default/bin/lambdapi}
+dkcheck=${DKCHECK:-dk check}
+dkdep=${DKDEP:-dk dep}
+
 TIMEFORMAT="%Es"
+
 root=`pwd`
-lambdapi='dune exec -- lambdapi'
+
+outdir=/tmp/export_dk
+
+reset_outdir() {
+    rm -rf $outdir
+    mkdir -p $outdir
+}
+reset_outdir
 
 # compute lp files to test
-dir=tests/OK
-outdir=/tmp/`echo $dir | sed -e 's/\//_/g'`
-cd $dir
-for f in *.lp
+for f in tests/OK/*.lp
 do
     f=${f%.lp}
     case $f in
-        ac);; # because dedukti does not handle commutative and non associative symbols
-        π/utf_path);; # because dedukti does not accept unicode characters in module names
-        escape_path|'a b/escape file');; # because dedukti does not accept spaces in module names
-        262_private_in_lhs);; # because dedukti does not accept protected symbols in rule LHS arguments
-        273|813);; # because dedukti SR algorithm fails
-        file.with.dot|req.file.with.dot);; #FIXME
-        indind);; #FIXME
-        why3*);; #FIXME
-        *) lp_files="$dir/$f.lp $lp_files";
+        tests/OK/ac);; # because dedukti does not handle commutative and non associative symbols
+        tests/OK/π/utf_path);; # because dedukti does not accept unicode characters in module names
+        tests/OK/escape_path|'tests/OK/a b/escape file');; # because dedukti does not accept spaces in module names
+        tests/OK/262_private_in_lhs);; # because dedukti does not accept protected symbols in rule LHS arguments
+        tests/OK/273|tests/OK/813);; # because dedukti SR algorithm fails
+        tests/OK/file.with.dot|tests/OK/req.file.with.dot);; #FIXME
+        tests/OK/indind);; #FIXME
+        tests/OK/why3*);; #FIXME
+        *) lp_files="$f.lp $lp_files";
            f=`echo $f | sed -e 's/\//_/g'`;
-           dk_files="${outdir}_$f.dk $dk_files";
-           dko_files="${outdir}_$f.dko $dko_files";;
+           dk_files="$f.dk $dk_files";;
     esac
 done
 
-# compile lp files (optional)
+# compile lp files
 compile() {
-    cd $root
-    echo 'compile lp files (optional) ...'
+    echo 'compile lp files ...'
     #$lambdapi check -w -c $lp_files # does not work because of #802
     for f in $lp_files
     do
@@ -44,12 +51,11 @@ compile() {
 
 # translate lp files to dk files
 translate() {
-    cd $root
     echo 'translate lp files ...'
     for f in $lp_files
     do
         f=${f%.lp}
-        out=/tmp/`echo $f | sed -e 's/\//_/g'`
+        out=$outdir/`echo $f | sed -e 's/\//_/g'`
         echo "$f.lp --> $out.dk ..."
         $lambdapi export -w -v 0 -o dk $f.lp > $out.dk
         if test $? -ne 0; then echo KO; exit 1; fi
@@ -59,14 +65,15 @@ time translate
 
 # check dk files
 check() {
-    cd /tmp/
+    cd $outdir
     echo 'remove #REQUIRE commands (to be removed when https://github.com/Deducteam/Dedukti/issues/262 is fixed) ...'
-    sed -i -e 's/#REQUIRE.*$//' $dk_files    
-    echo check dk files using `which ${DKCHECK:-dk}` ...
-    ${DKCHECK:-dk check} -q -e `${DKDEP:-dk dep} -q -s $dk_files`
-    if test $? -ne 0; then echo KO; exit 1; fi
+    sed -i -e 's/#REQUIRE.*$//' $dk_files
+    dk_files=`$dkdep -q -s $dk_files`
+    echo $dkcheck -q -e $dk_files ...
+    $dkcheck -q -e $dk_files
+    res=$?
+    cd $root
+    if test $res -ne 0; then echo KO; else echo OK; fi
+    exit $res
 }
 time check
-
-echo OK
-cd $root
