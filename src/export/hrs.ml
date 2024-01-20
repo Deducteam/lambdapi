150ebc77bcebb90fcdbeef1a496b2eb71ebd69de
thiagofelicissimo <thiago.felicissimo@inria.fr>
2024-01-20 12:32:35 +0100

    improvements in hrs export

--- src/export/hrs.ml
+++ src/export/hrs.ml
@@ -19,24 +19,19 @@ Function symbols and variables are translated as symbols of type t.
 Pattern variables of arity n are translated as variables of type t -> ... -> t
    with n times ->.
 
-- In the hrs format, variable names must be distinct from function symbol
-   names. So bound variables are translated into positive integers and pattern
-   variables are prefixed by ["$"].
-
-- There is no clash between function symbol names and A, B, L, P because
-   function symbol names are fully qualified.
-
-- Function symbol names are fully qualified but ["."] is replaced by ["_"]
-   because ["."] cannot be used in identifiers (["."]  is used in lambda
-   abstractions).
+- Bound variables are translated into positive integers and pattern
+   variables are translated as XiNj where i is the number of the rule and j
+   is the indice of the variable. Function symbols are translated directly
+   by their unqualified names. If a function symbol name clashes with the
+   name of a variable, metavariable or a symbol declared in the base
+   signature, we prefix it with !. In order to do this, we assume that no
+   function symbol starts with !.
 
 - Two occurrences of the same pattern variable name may have two different
    arities (in two different rules). So pattern variable names are prefixed by
    the rule number.
 
 TO DO:
-
-- HRS does not accept unicode characters.
 
 - Optim: output only the symbols used in the rules. *)
 
@@ -60,9 +55,24 @@ end
 module VMap = Map.Make(V)
 let pvars = ref VMap.empty
 
+let sanitize_name : string -> string = fun s ->
+  (* we considere names starting with '!' as forbiden, so we can
+     use it as an escape character to prevent clashes *)
+  if s.[0] = '!' then assert false;
+  match s with
+  | "A" | "L" | "P" | "B" ->
+    (* prevents clashes with the names in the base signature *)
+    "!" ^ s
+  | _ ->
+    (* prevent clashes metavariable names *)
+    if s.[0] = 'X' then "!" ^ s
+    (* prevent clashes with variable names, which are just numbers *)
+    else if Str.string_match (Str.regexp "[0-9]+$") s 0 then "!" ^ s
+    else s (* ok names *)
+
 (** [sym_name s] translates the symbol name of [s]. *)
 let sym_name : sym pp = fun ppf s ->
-    out ppf "%a_%s" (List.pp string "_") s.sym_path s.sym_name
+    out ppf "%s" (sanitize_name s.sym_name)
 
 (** [add_sym] declares a Lambdapi symbol. *)
 let add_sym : sym -> unit = fun s ->
@@ -79,7 +89,7 @@ let add_bvar : tvar -> unit = fun v ->
 let bvar : tvar pp = fun ppf v -> int ppf (Bindlib.uid_of v)
 
 (** [pvar i] translates the pattern variable index [i]. *)
-let pvar : int pp = fun ppf i -> out ppf "$%d_%d" !nb_rules i
+let pvar : int pp = fun ppf i -> out ppf "X%dN%d" !nb_rules i
 
 (** [add_pvar i k] declares a pvar of index [i] and arity [k]. *)
 let add_pvar : int -> int -> unit = fun i k ->
@@ -151,16 +161,14 @@ let sign : Sign.t pp = fun ppf sign ->
   let pp_pvars = fun ppf pvars ->
     let typ : int pp = fun ppf k ->
       for _i=1 to k do string ppf "t -> " done; out ppf "t" in
-    let pvar_decl (n,i) k = out ppf "\n$%d_%d : %a" n i typ k in
+    let pvar_decl (n,i) k = out ppf "\nX%dN%d : %a" n i typ k in
     VMap.iter pvar_decl pvars in
   (* Function for printing the types of abstracted variables. *)
   let pp_bvars : IntSet.t pp = fun ppf bvars ->
     let bvar_decl : int pp = fun ppf n -> out ppf "\n%d : t" n in
     IntSet.iter (bvar_decl ppf) bvars
   in
-  (* Finally, generate the whole hrs file. Note that the variables $x, $y and
-     $z used in the rules for beta and let cannot clash with user-defined
-     pattern variables which are integers. *)
+  (* Finally, generate the whole hrs file. *)
   out ppf "\
 (FUN
 A : t -> t -> t
@@ -169,11 +177,11 @@ P : t -> (t -> t) -> t
 B : t -> t -> (t -> t) -> t%a
 )
 (VAR
-$x : t
-$y : t -> t
-$z : t%a%a
+X0 : t
+X1 : t -> t
+X2 : t%a%a
 )
 (RULES
-A(L($x,$y),$z) -> $y($z),
-B($x,$z,$y) -> $y($z)%s
+A(L(X0,X1),X2) -> X1(X2),
+B(X0,X2,X1) -> X1(X2)%s
 )\n" pp_syms !syms pp_pvars !pvars pp_bvars !bvars (Buffer.contents buf_rules)
