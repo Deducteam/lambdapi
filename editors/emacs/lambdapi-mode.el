92deb8d8c097634155e48ed21367cd1104bdd64a
Gabriel Hondet <koizel@tilde.club>
2024-04-24 09:28:32 +0200

    Downgrade minimal eglot to 1.6, fix pkg
    The lambdapi-pkg must contain a single form

--- editors/emacs/lambdapi-mode.el
+++ editors/emacs/lambdapi-mode.el
@@ -1,6 +1,6 @@
 ;;; lambdapi-mode.el --- A major mode for editing Lambdapi source code -*- lexical-binding: t; -*-
 
-;; Copyright (C) 2020 Deducteam
+;; Copyright (C) 2020-2024 Deducteam
 
 ;; Authors: Ashish Barnawal, Diego Riviero, Gabriel Hondet, Rodolphe Lepigre
 ;; Maintainer: Deducteam <dedukti-dev@inria.fr>
@@ -9,7 +9,7 @@
 ;; Homepage: https://github.com/Deducteam/lambdapi
 ;; Keywords: languages
 ;; Compatibility: GNU Emacs 26.1
-;; Package-Requires: ((emacs "26.1") (eglot "1.8") (math-symbol-lists "1.2.1") (highlight "20190710.1527"))
+;; Package-Requires: ((emacs "26.1") (eglot "1.6") (math-symbol-lists "1.2.1") (highlight "20190710.1527"))
 
 ;;; Commentary:
 
--- editors/emacs/lambdapi-pkg.el
+++ editors/emacs/lambdapi-pkg.el
@@ -1,30 +1,5 @@
-;;; lambdapi-pkg.el --- A major mode to edit Lambdapi files -*- lexical-binding: t; -*-
-;;
-;; Copyright (C) 2024 Gabriel Hondet
-;;
-;; Authors: Ashish Barnawal, Diego Riviero, Gabriel Hondet, Rodolphe Lepigre
-;; Maintainer: Deducteam <dedukti-dev@inria.fr>
-;; Created: April 23, 2024
-;; Modified: April 23, 2024
-;; Version: 1.1.0
-;; Keywords: languages
-;; SPDX-License-Identifier: CECILL-2.1
-;; Homepage: https://github.com/Deducteam/lambdapi
-;; Package-Requires: ((emacs "26.1") (eglot "1.8") (math-symbol-lists "1.2.1") (highlight "20190710.1527"))
-;;
-;; This file is not part of GNU Emacs.
-;;
-;;; Commentary:
-;;
-;; A major mode for the Lambdapi proof assistant.
-;;
-;;; Code:
-
 (define-package "lambdapi" "1.1.0"
   "A major mode to edit Lambdapi files."
-  '((eglot "1.8")
+  '((eglot "1.6")
     (math-symbol-lists "1.2.1")
     (highlight "20190710.1527")))
-
-(provide 'lambdapi-pkg)
-;;; lambdapi-pkg.el ends here
