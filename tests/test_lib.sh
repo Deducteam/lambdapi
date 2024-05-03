39da4b21de28a7d31f39634556b2b23a32f9e876
Abdelghani ALIDRA <alidrandco@yahoo.fr>
2024-05-03 10:58:04 +0200

    tests : rename variable in test_lib.sh, put back redirection to outwith simplification and check if repo has already been cloned to prevent clone from failing

--- tests/test_lib.sh
+++ tests/test_lib.sh
@@ -6,21 +6,27 @@ lambdapi='dune exec -- lambdapi check'
 options='-w'
 TIMEFORMAT="%Es"
 
+out=/tmp/lambdapi.output
+
 checkout_lib() {
     IFS='/' read -ra ADDR <<< "$1"
-    repo_name="${ADDR[-1]}"
-    IFS='.' read -ra ADDR <<< "$repo_name"
-    repo_name="/tmp/${ADDR[0]}"
-    git clone $1 $repo_name
+    repo_path="${ADDR[-1]}"
+    IFS='.' read -ra ADDR <<< "$repo_path"
+    repo_path="/tmp/${ADDR[0]}"
+    if [ -d "$repo_path" ] ; then
+        rm -fr "$repo_path"
+    fi
+
+    git clone $1 $repo_path
 }
 
 test_lib() {
     for f in $(find $1 -name *.lp)  $(find $1 -name *.dk)
     do
         echo lambdapi check $options $f ...
-        $lambdapi $options "$f"
+        $lambdapi "$f" > $out 2>&1 || (cat $out; exit 1)
     done
 }
 
 checkout_lib $1
-time test_lib $repo_name
+time test_lib $repo_path
