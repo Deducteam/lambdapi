5e05710e9c80f8cf39e34d5191bf9fee0078e216
gabrielhdt <7418676+gabrielhdt@users.noreply.github.com>
2025-02-12 10:27:18 +0100

    Update to pratter 5

--- src/lplib/list.ml
+++ src/lplib/list.ml
@@ -309,3 +309,15 @@ let pos : ('a -> bool) -> 'a list -> int = fun f ->
     | [] -> raise Not_found
     | x::xs -> if f x then k else pos (k+1) xs
   in pos 0
+
+(** The list monad interprets lists as some kind of probability monad. A
+    function that outputs in the monad can be considered to have several
+    probable values. *)
+module Monad = struct
+  let return : 'a -> 'a t = fun x -> [x]
+  let join : 'a t t -> 'a t = concat
+  let bind : 'a t -> ('a -> 'b t) -> 'b t = fun m f ->
+    join ((map f) m)
+
+  let ( let* ) = bind
+end
