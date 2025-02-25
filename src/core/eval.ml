Deducteam/lambdapi

commit 0d3899ad6d5b30643fcdbd09cf51b37547da07cf

Indexing2025 (#1201)
Several improvements to use the search engine functionalities over the translation of the HOL-Light library (https://github.com/Deducteam/coq-hol-light):
* normalize queries in websearch/search
* pre-load a file in websearch (e.g. to declare implicit args)
* paginate the output in the generated webpage
* allow to declare rewriting rules (e.g. alignments) over defined and constant symbols
* improve error message for overloaded symbols

src/core/eval.ml
@@ -220,11 +220,15 @@ and whnf_stk : config -> term -> stack -> term * stack = fun cfg t stk ->
     Stdlib.incr steps; whnf_stk cfg (subst u t) stk
   | (Symb s as h, stk) as r ->
     begin match !(s.sym_def) with
-    | Some t ->
+      (* The invariant that defined symbols are subject to no
+         rewriting rules is false during indexing for websearch;
+         that's the reason for the when in the next line *)
+    | Some t when cfg.dtree s = Tree_type.empty_dtree ->
       if !(s.sym_opaq) || not cfg.Config.expand_defs then r else
         (Stdlib.incr steps; whnf_stk cfg t stk)
     | None when not cfg.Config.rewrite -> r
-    | None ->
+    | _ ->
+      log_eval "trying to rewrite %a" term t ;
       (* If [s] is modulo C or AC, we put its arguments in whnf and reorder
          them to have a term in AC-canonical form. *)
       let stk =

.gitignore
@@ -20,5 +20,4 @@ libraries/zenon_modulo
 *.png
 .DS_Store
 *.map
-TODO.*
 *.log

src/cli/lambdapi.ml
@@ -20,16 +20,30 @@ module CLT = Cmdliner.Term
 module LPSearchMain =
 struct
 
-let search_cmd cfg s =
- Config.init cfg;
- let run () = out Format.std_formatter "%s@."
-   (Tool.Indexing.search_cmd_txt s) in
- Error.handle_exceptions run
+let sig_state_of_require =
+ function
+   None -> Core.Sig_state.dummy
+ | Some req ->
+    (* Search for a package from the current working directory. *)
+    Package.apply_config (Filename.concat (Sys.getcwd()) ".") ;
+    Core.Sig_state.of_sign
+     (Compile.compile (Parsing.Parser.path_of_string req))
 
-let websearch_cmd cfg port =
+let search_cmd cfg rules require s =
  Config.init cfg;
  let run () =
-  Tool.Websearch.start ~port () in
+  Tool.Indexing.load_rewriting_rules rules ;
+  let ss = sig_state_of_require require in
+  out Format.std_formatter "%s@."
+   (Tool.Indexing.search_cmd_txt ss s) in
+ Error.handle_exceptions run
+
+let websearch_cmd cfg rules port require =
+ Config.init cfg;
+ let run () =
+  Tool.Indexing.load_rewriting_rules rules ;
+  let ss = sig_state_of_require require in
+  Tool.Websearch.start ss ~port () in
  Error.handle_exceptions run
 
 let index_cmd cfg add_only rules files =
@@ -42,7 +56,8 @@ let index_cmd cfg add_only rules files =
   let handle file =
    Console.reset_default ();
    Time.restore time;
-   Tool.Indexing.index_sign ~rules (no_wrn Compile.compile_file file) in
+   Tool.Indexing.load_rewriting_rules rules;
+   Tool.Indexing.index_sign (no_wrn Compile.compile_file file) in
   List.iter handle files;
   Tool.Indexing.dump () in
  Error.handle_exceptions run
@@ -408,23 +423,29 @@ let rules_arg : string list CLT.t =
      multiple times to fetch rules from multiple files." in
   Arg.(value & opt_all string [] & info ["rules"] ~docv:"FILENAME" ~doc)
 
+let require_arg : string option CLT.t =
+  let doc =
+    "LP file to be required before starting the search engine." in
+  Arg.(value & opt (some string) None & info ["require"] ~docv:"PATH" ~doc)
+
 let index_cmd =
  let doc = "Index the given files." in
  Cmd.v (Cmd.info "index" ~doc ~man:man_pkg_file)
-  Cmdliner.Term.(const LPSearchMain.index_cmd $ Config.full $
-   add_only_arg $ rules_arg $ files)
+  Cmdliner.Term.(const LPSearchMain.index_cmd $ Config.full
+   $ add_only_arg $ rules_arg $ files)
 
 let search_cmd =
  let doc = "Run a search query against the index." in
  Cmd.v (Cmd.info "search" ~doc ~man:man_pkg_file)
   Cmdliner.Term.(const LPSearchMain.search_cmd $ Config.full
-   $ query_as_arg)
+   $ rules_arg $ require_arg $ query_as_arg)
 
 let websearch_cmd =
  let doc =
   "Starts a webserver for searching the library." in
  Cmd.v (Cmd.info "websearch" ~doc ~man:man_pkg_file)
-  Cmdliner.Term.(const LPSearchMain.websearch_cmd $ Config.full $ port_arg)
+  Cmdliner.Term.(const LPSearchMain.websearch_cmd $ Config.full
+   $ rules_arg $ port_arg $ require_arg )
 
 let _ =
   let t0 = Sys.time () in

src/handle/query.ml
@@ -160,7 +160,7 @@ let handle : Sig_state.t -> proof_state option -> p_query -> result =
   let ctxt = Env.to_ctxt env in
   let p = new_problem() in
   match elt with
-  | P_query_search s -> return string (Tool.Indexing.search_cmd_txt s)
+  | P_query_search s -> return string (Tool.Indexing.search_cmd_txt ss s)
   | P_query_debug(_,_)
   | P_query_verbose(_)
   | P_query_flag(_,_)

src/tool/indexing.ml
@@ -296,10 +296,10 @@ module DB = struct
    ~separator:"\n" ~sep:" and\n" ~delimiters:("","")
    ~lis:("* ","") ~pres:("","")
 
- let pp_item_set fmt set = pp_item_list fmt (ItemSet.bindings set)
+ let pp_results_list fmt l = pp_item_list fmt l
 
- let html_of_item_set fmt set =
-  Lplib.Base.out fmt "<ul>%a</ul>" html_of_item_list (ItemSet.bindings set)
+ let html_of_results_list from fmt l =
+  Lplib.Base.out fmt "<ol start=\"%d\">%a</ol>" from html_of_item_list l
 
  (* disk persistence *)
 
@@ -348,31 +348,27 @@ module DB = struct
 
 end
 
-let find_sym ~prt:_prt ~prv:_prv _sig_state {elt=(mp,name); pos} =
+exception Overloaded of string * DB.answer DB.ItemSet.t
+
+let find_sym ~prt ~prv sig_state ({elt=(mp,name); pos} as s) =
  let pos,mp =
   match mp with
     [] ->
      let res = DB.locate_name name in
      if DB.ItemSet.cardinal res > 1 then
-      Common.Error.fatal pos
-       "Overloaded symbol %s, search for \"name = %s\" to know how \
-        to disambiguate it"
-       name name ;
+       raise (Overloaded (name,res)) ;
      (match DB.ItemSet.choose_opt res with
        | None -> Common.Error.fatal pos "Unknown symbol %s." name
        | Some (((mp,_),sympos),[_,_,DB.Name]) -> sympos,mp
        | Some _ -> assert false) (* locate only returns DB.Name*)
   | _::_ -> None,mp
  in
-  Core.Term.create_sym mp Core.Term.Public Core.Term.Defin Core.Term.Sequen
-   false (Common.Pos.make pos name) None Core.Term.mk_Type []
-
-let search_pterm ~generalize ~mok env pterm =
- let sig_state = Core.Sig_state.dummy in
- let env = ("V#", (new_var "V#", Term.mk_Type, None))::env in
- let query =
-  Parsing.Scope.scope_search_pattern ~find_sym ~mok sig_state env pterm in
- DB.search ~generalize query
+ try
+  Core.Sig_state.find_sym ~prt ~prv sig_state s
+ with
+  Common.Error.Fatal _ ->
+   Core.Term.create_sym mp Core.Term.Public Core.Term.Defin Core.Term.Sequen
+    false (Common.Pos.make pos name) None Core.Term.mk_Type []
 
 module QNameMap =
  Map.Make(struct type t = sym_name let compare = Stdlib.compare end)
@@ -424,6 +420,16 @@ let normalize typ =
   try QNameMap.find (name_of_sym sym) (Lazy.force meta_rules)
   with Not_found -> Core.Tree_type.empty_dtree in
  Core.Eval.snf ~dtree ~tags:[`NoExpand] [] typ
+
+let search_pterm ~generalize ~mok ss env pterm =
+ let env =
+  ("V#",(new_var "V#",Term.mk_Type,None))::env in
+ let query =
+  Parsing.Scope.scope_search_pattern ~find_sym ~mok ss env pterm in
+ Dream.log "QUERY before: %a" Core.Print.term query ;
+ let query = normalize query in
+ Dream.log "QUERY after: %a" Core.Print.term query ;
+ DB.search ~generalize query
 
 let rec is_flexible t =
  match Core.Term.unfold t with
@@ -497,8 +503,11 @@ let insert_rigid t v =
 let index_term_and_subterms ~is_spine t item =
  let tn = normalize t in
  (*
- Format.printf "%a : %a REWRITTEN TO %a@."
-  pp_item (item Exact) Core.Print.term t Core.Print.term tn ;
+ let pp_item ppf (((p,n),_ ), _) =
+   Lplib.Base.out ppf "%a.%s" Core.Print.path p n in
+ Format.printf "%a :(%a) REWRITTEN TO (%a)@."
+  pp_item (item (DB.Conclusion DB.Exact))
+  Core.Print.term t Core.Print.term tn ;
  *)
  let cmp (where1,t1) (where2,t2) =
   let res = compare where1 where2 in
@@ -538,8 +547,13 @@ let index_sym sym =
  (* Rules *)
  List.iter (index_rule sym) Timed.(!(sym.Core.Term.sym_rules))
 
-let index_sign ~rules:rwrules sign =
- DB.rwpaths := rwrules ;
+let load_rewriting_rules rwrules =
+ DB.rwpaths := rwrules
+
+let index_sign sign =
+ (*Console.set_flag "print_domains" true ;
+ Console.set_flag "print_implicits" true ;
+ Common.Logger.set_debug true "e" ;*)
  let syms = Timed.(!(sign.Core.Sign.sign_symbols)) in
  let rules = Timed.(!(sign.Core.Sign.sign_deps)) in
  Lplib.Extra.StrMap.iter (fun _ sym -> index_sym sym) syms ;
@@ -587,11 +601,11 @@ module QueryLanguage = struct
          | _,_,Xhs (ins,side) -> match_opt insp ins && match_opt sidep side
          | _ -> false) positions
 
- let answer_base_query ~mok env =
+ let answer_base_query ~mok ss env =
   function
    | QName s -> locate_name s
    | QSearch (patt,generalize,constr) ->
-      let res = search_pterm ~generalize ~mok env patt in
+      let res = search_pterm ~generalize ~mok ss env patt in
       (match constr with
         | None -> res
         | Some constr -> ItemSet.filter (filter_constr constr) res)
@@ -616,10 +630,10 @@ module QueryLanguage = struct
        Lplib.String.is_prefix p (string_of_path p') in
   ItemSet.filter f set
 
- let answer_query ~mok env =
+ let answer_query ~mok ss env =
   let rec aux =
    function
-    | QBase bq -> answer_base_query ~mok env bq
+    | QBase bq -> answer_base_query ~mok ss env bq
     | QOpp (q1,op,q2) -> perform_op op (aux q1) (aux q2)
     | QFilter (q,f) -> filter (aux q) f
   in
@@ -632,28 +646,43 @@ include QueryLanguage
 
 module UserLevelQueries = struct
 
- let search_cmd_gen ~fail ~pp_results s =
+ let search_cmd_gen ss ~from ~how_many ~fail ~pp_results s =
   try
    let pstream = Parsing.Parser.Lp.parse_search_query_string "LPSearch" s in
    let pq = Stream.next pstream in
    let mok _ = None in
-   let items = answer_query ~mok [] pq in
-   Format.asprintf "%a@." pp_results items
+   let items = ItemSet.bindings (answer_query ~mok ss [] pq) in
+   let resultsno = List.length items in
+   let _,items = Lplib.List.cut items from in
+   let items,_ = Lplib.List.cut items how_many in
+   Format.asprintf "<h1>Number of results: %d</h1>%a@."
+    resultsno pp_results items
   with
    | Stream.Failure ->
       fail (Format.asprintf "Syntax error: a query was expected")
    | Common.Error.Fatal(_,msg) ->
       fail (Format.asprintf "Error: %s@." msg)
+   | Overloaded(name,res) ->
+      fail (Format.asprintf
+       "Overloaded symbol %s. Please rewrite the query replacing %s \
+        with a fully qualified identifier among the following: %a"
+        name name pp_results (ItemSet.bindings res))
+   | Stack_overflow ->
+      fail
+       (Format.asprintf
+         "Error: too many results. Please refine your query.@." )
    | exn ->
       fail (Format.asprintf "Error: %s@." (Printexc.to_string exn))
 
- let search_cmd_html s =
-  search_cmd_gen ~fail:(fun x -> "<font color=\"red\">" ^ x ^ "</font>")
-   ~pp_results:html_of_item_set s
+ let search_cmd_html ss ~from ~how_many s =
+  search_cmd_gen ss ~from ~how_many
+   ~fail:(fun x -> "<font color=\"red\">" ^ x ^ "</font>")
+   ~pp_results:(html_of_results_list from) s
 
- let search_cmd_txt s =
-  search_cmd_gen ~fail:(fun x -> Common.Error.fatal_no_pos "%s" x)
-  ~pp_results:pp_item_set s
+ let search_cmd_txt ss s =
+  search_cmd_gen ss ~from:0 ~how_many:999999
+   ~fail:(fun x -> Common.Error.fatal_no_pos "%s" x)
+   ~pp_results:pp_results_list s
 
 end
 

src/tool/indexing.mli
@@ -1,10 +1,12 @@
 (* indexing *)
 val empty : unit -> unit
-val index_sign : rules:string list -> Core.Sign.t -> unit
+val load_rewriting_rules: string list -> unit
+val index_sign : Core.Sign.t -> unit
 val dump : unit -> unit
 
 (* search command used by cli *)
-val search_cmd_txt: string -> string
+val search_cmd_txt: Core.Sig_state.sig_state -> string -> string
 
 (* search command used by websearch *)
-val search_cmd_html: string -> string
+val search_cmd_html:
+ Core.Sig_state.sig_state -> from:int -> how_many:int -> string -> string

src/tool/tree_graphviz.ml
@@ -26,7 +26,7 @@ type dot_term =
    performed to give birth to the child node. The label on the edge between a
    node and one of its children represents the term matched to generate the
    next pattern matrix (the one of the child node). *)
-let to_dot : Format.formatter -> sym -> unit = fun ppf s ->
+let tree_to_dot : Format.formatter -> _ dtree -> unit = fun ppf dtree ->
   let output_tree ppf tree =
     let dotterm : dot_term pp = fun ppf dh ->
       let var_px = "v" in
@@ -99,4 +99,7 @@ let to_dot : Format.formatter -> sym -> unit = fun ppf s ->
     end;
     out ppf "@.}@\n@?"
   in
-  output_tree ppf (Lazy.force (snd !(s.sym_dtree)))
+  output_tree ppf (Lazy.force (snd dtree))
+
+let to_dot : Format.formatter -> sym -> unit = fun ppf s ->
+ tree_to_dot ppf !(s.sym_dtree)

src/tool/websearch.eml.ml
@@ -1,4 +1,6 @@
-let show_form ?(message="") ?output request =
+(*
+let show_form_stream ?(message="") ?(output="") request response =
+  %% response
   <html>
   <body>
 
@@ -22,16 +24,63 @@ let show_form ?(message="") ?output request =
       </p>
     </form>
 
+    <%s! output %>
+
+  </body>
+  </html>
+*)
+
+let show_form ~from ?(message="") ?output request =
+  <html>
+  <body>
+    <script>
+    function incr(delta) {
+      document.getElementById("from").value =
+        Math.max(0,delta + Number(document.getElementById("from").value));
+      document.getElementById("search").click();
+    }
+    </script>
+
+    <h1><a href="https://github.com/Deducteam/lambdapi">LambdaPi</a>
+     Search Engine</h1>
+
+    <p>
+    The <b>search</b> button answers the query. Read the <a
+    href="https://lambdapi.readthedocs.io/en/latest/query_language.html">query
+    language specification</a> to learn about the query language.<br>
+    </p>
+
+    <form method="POST" action="/" id="form">
+      <%s! Dream.csrf_tag request %>
+      <p>
+      <input type="search" required="true" size="100"
+        name="message" value="<%s message %>"
+        onfocus="this.select();" autofocus></p>
+      <p>
+      <input type="submit" value="search" id="search" name="search">
+      </p>
+
 %   begin match output with
-%   | None -> ()
+%   | None ->
+       <input type="hidden" name="from" value="<%s string_of_int from %>">
 %   | Some output ->
+    <p>
+    <input type="number" required="true" style="width: 5em" min="0" id="from"
+      name="from" value="<%s string_of_int from %>" onfocus="this.select()">
+    <input type="button"
+      name="prev" value="Prev" onclick="incr(-100)">
+    <input type="button"
+      name="next" value="Next" onclick="incr(100)">
+    </p>
     <%s! output %>
 %   end;
+    </form>
 
   </body>
   </html>
 
-let start ~port () =
+let start ss ~port () =
+  (*Common.Logger.set_debug true "e" ;*)
   Dream.run ~port
   @@ Dream.logger
   @@ Dream.memory_sessions
@@ -39,15 +88,21 @@ let start ~port () =
 
     Dream.get  "/"
       (fun request ->
-        Dream.html (show_form request));
+        Dream.html (show_form ~from:0 request));
 
     Dream.post "/"
       (fun request ->
         match%lwt Dream.form request with
-        | `Ok [ "message", message; "search", _search ] ->
-          let output = Indexing.search_cmd_html message in
-          Dream.html (show_form ~message ~output request)
+        | `Ok [ "from", from; "message", message; "search", _search ] ->
+          Dream.log "from1 = %s" from ;
+          let from = int_of_string from in (* XXX CSC exception XXX *)
+          Dream.log "from2 = %d" from ;
+          let output =
+            Indexing.search_cmd_html ss ~from ~how_many:100 message in
+          Dream.html (show_form ~from ~message ~output request)
+          (*Dream.stream (show_form_stream ~message ~output request)*)
         | _ ->
+          Dream.log "no match" ;
           Dream.empty `Bad_Request);
 
   ]

commit 19dcf15152af655abafe47f157f6cdc3c45a0835

Use Lazy.force to test whether a dtree is empty (#1210)
fix https://github.com/Deducteam/lambdapi-stdlib/issues/32
introduced by #1201

src/core/eval.ml
@@ -215,7 +215,7 @@ and whnf_stk : config -> term -> stack -> term * stack = fun cfg t stk ->
       (* The invariant that defined symbols are subject to no
          rewriting rules is false during indexing for websearch;
          that's the reason for the when in the next line *)
-    | Some t when cfg.dtree s = Tree_type.empty_dtree ->
+    | Some t when Tree_type.is_empty (cfg.dtree s) ->
       if Timed.(!(s.sym_opaq)) || not cfg.Config.expand_defs then r else
         (Stdlib.incr steps; whnf_stk cfg t stk)
     | None when not cfg.Config.rewrite -> r

src/core/tree_type.ml
@@ -156,3 +156,6 @@ type 'a dtree = int Lazy.t * 'a tree Lazy.t
 
 (** [empty_dtree] is the empty decision tree. *)
 let empty_dtree : 'a dtree = (lazy 0, lazy Fail)
+
+(** [is_empty dt] tells whether [dt] is empty. *)
+let is_empty (dt:'a dtree) :bool = Lazy.force (snd dt) = Fail