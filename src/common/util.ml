5cf2afca804fbffe370390a8aa97834fb525fcb2
Abdelghani ALIDRA <alidrandco@yahoo.fr>
2025-05-14 18:39:57 +0200

    refactor transform_ascii_to_unicode

--- src/common/util.ml
+++
@@ -1,3 +0,0 @@
-let transform_ascii_to_unicode : string -> string = fun s ->
-  let s = Str.global_replace (Str.regexp_string " -> ") " → " s in
-  Str.global_replace (Str.regexp "\\bforall\\b") "Π" s
