d7a8df7fd294c8fb6aec5769d0e3e01916f2c8c7
Frédéric Blanqui <frederic.blanqui@inria.fr>
2025-07-04 20:32:27 +0200

    details

--- src/core/sig_state.ml
+++ src/core/sig_state.ml
@@ -98,7 +98,7 @@ let open_sign : sig_state -> Sign.t -> sig_state = fun ss sign ->
   {ss with in_scope; open_paths}
 
 (** [of_sign sign] creates a state from the signature [sign] and open it as
-   well as the ghost signatures. *)
+   well as the ghost signature. *)
 let of_sign : Sign.t -> sig_state = fun signature ->
   open_sign (open_sign {dummy with signature} Ghost.sign) signature
 
@@ -106,26 +106,26 @@ let of_sign : Sign.t -> sig_state = fun signature ->
     corresponding to a qualified / non qualified name *)
 type find_sym = prt:bool -> prv:bool -> sig_state -> qident loc -> sym
 
-(** [find_sym ~prt ~prv b st qid] returns the symbol corresponding to the
+(** [find_sym ~prt ~prv b ss qid] returns the symbol corresponding to the
     possibly qualified identifier [qid], or raises [Fatal]. The boolean [b]
     indicates if the error message should mention variables when [qid] is
     unqualified. [~prt] indicates whether {!constructor:Term.expo.Protec}
     symbols from other modules are allowed. [~prv] indicates whether
     {!constructor:Term.expo.Privat} symbols are allowed. *)
 let find_sym : find_sym =
-  fun ~prt ~prv st {elt = (mp, s); pos} ->
+  fun ~prt ~prv ss {elt = (mp, s); pos} ->
   let s =
     match mp with
     | [] -> (* Symbol in scope. *)
         begin
-          try StrMap.find s st.in_scope
+          try StrMap.find s ss.in_scope
           with Not_found -> fatal pos "Unknown object %s." s
         end
-    | [m] when StrMap.mem m st.alias_path -> (* Aliased module path. *)
+    | [m] when StrMap.mem m ss.alias_path -> (* Aliased module path. *)
         begin
           (* The signature must be loaded (alias is mapped). *)
           let sign =
-            try Path.Map.find (StrMap.find m st.alias_path) !loaded
+            try Path.Map.find (StrMap.find m ss.alias_path) !loaded
             with _ -> assert false (* Should not happen. *)
           in
           (* Look for the symbol. *)
@@ -135,8 +135,8 @@ let find_sym : find_sym =
     | _  -> (* Fully-qualified symbol. *)
         begin
           (* Check that the signature was required (or is the current one). *)
-          if mp <> st.signature.sign_path then
-            if not (Path.Map.mem mp !(st.signature.sign_deps)) then
+          if mp <> ss.signature.sign_path then
+            if not (Path.Map.mem mp !(ss.signature.sign_deps)) then
               fatal pos "No module [%a] required." Path.pp mp;
           (* The signature must have been loaded. *)
           let sign =
@@ -150,7 +150,7 @@ let find_sym : find_sym =
   in
   match (prt, prv, s.sym_expo) with
   | (false, _    , Protec) ->
-      if s.sym_path = st.signature.sign_path then s else
+      if s.sym_path = ss.signature.sign_path then s else
       (* Fail if symbol is not defined in the current module. *)
       fatal pos "Protected symbol not allowed here."
   | (_    , false, Privat) -> fatal pos "Private symbol not allowed here."
