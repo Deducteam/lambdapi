5a6a6cc33af426d7b44efaf04a04c3d71649f0da
Abdelghani ALIDRA <alidrandco@yahoo.fr>
2025-09-17 11:55:18 +0200

    make it possible to remove .emcs.d with clean.sh script

--- editors/emacs/clean.sh
+++ editors/emacs/clean.sh
@@ -6,3 +6,7 @@ sudo apt remove --purge emacs emacs-common
 sudo snap remove --purge emacs
 echo removing conf files
 rm -rf ~/.emacs.d ~/.emacs ~/.emacs.el ~/.config/emacs
+if [ "$1" = "--all" ]; then
+    echo "removing .emacs.d configuration forlder"
+    rm -fr ~/.emacs.d
+fi
