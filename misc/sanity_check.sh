9abeaa0d54befe0199312cc90407ca3ff1820a0f
Frédéric Blanqui <frederic.blanqui@inria.fr>
2025-12-01 10:43:55 +0100

    make ci fail if sanity_check fails

--- misc/sanity_check.sh
+++ misc/sanity_check.sh
@@ -2,15 +2,23 @@
 set -eu
 
 files=`find src -name \*.ml -o -name \*.ml[iy]`
+out=/tmp/sanity_check.log
+
+rm -f $out
+touch $out
 
 # Check for long lines.
-gawk '/@see/{next}length>78    {print "In " FILENAME ", line " FNR " more than 78 characters..."}' $files
+gawk '/@see/{next}length>78    {print "In " FILENAME ", line " FNR " more than 78 characters..."}' $files >> $out
 
 # Check for trailing spaces.
-gawk '/.*\s$/      {print "In " FILENAME ", line " FNR " has trailing spaces..."}' $files
+gawk '/.*\s$/      {print "In " FILENAME ", line " FNR " has trailing spaces..."}' $files >> $out
 
 # Check for tabulations.
-gawk '/.*\t.*/     {print "In " FILENAME ", line " FNR " contains tabulations..."}' `echo $files | sed -e 's|src/cli/init.ml||'`
+gawk '/.*\t.*/     {print "In " FILENAME ", line " FNR " contains tabulations..."}' `echo $files | sed -e 's|src/cli/init.ml||'` >> $out
 
 # Check for [Pervasives].
-gawk '/Pervasives/ {print "In " FILENAME ", line " FNR " use of [Pervasives] should be replaced by [Stdlib]..."}' $files
+gawk '/Pervasives/ {print "In " FILENAME ", line " FNR " use of [Pervasives] should be replaced by [Stdlib]..."}' $files >> $out
+
+cat $out
+
+if test -s $out; then exit 1; fi
