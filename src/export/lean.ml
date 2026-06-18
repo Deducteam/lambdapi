6965d49a4092afb4f00fdd18afc045f44f14c106
Ivan Martinez Comas <imcnavi20@gmail.com>
2026-06-18 16:47:48 +0200

    fix no parentehsis in type parameters

--- src/export/lean.ml
+++ src/export/lean.ml
@@ -64,7 +64,7 @@ let rec term oc t =
           let n = typ_arity h in
           if n <= 0 then ts else
             let l1,l2 = List.cut ts n in
-            l1 @ List.init (n-1) (fun _ -> P.wild) @ l2
+            l1 @ List.init (n-1) (fun _ -> {elt=P_Wild;pos=None}) @ l2
         in
         list paren " " oc ts
       in
@@ -152,9 +152,8 @@ let command oc {elt; pos} =
         | _ -> () (*FIXME?*)
       end
   | P_require_as _ -> wrn pos "Command not translated."
-  | P_symbol
-    { p_sym_mod; p_sym_nam; p_sym_arg; p_sym_typ;
-      p_sym_trm; p_sym_prf=_; p_sym_def } ->
+  | P_symbol { p_sym_mod; p_sym_nam; p_sym_arg; p_sym_typ; p_sym_trm;
+               p_sym_prf=_; p_sym_def } ->
       if not (StrSet.mem p_sym_nam.elt !erase) then
         let p_sym_arg =
           if !stt then
