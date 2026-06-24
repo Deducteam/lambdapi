2cdbdc7cc4cae977e090632c70528ad12bc92079
Frédéric Blanqui <frederic.blanqui@inria.fr>
2026-06-24 16:51:26 +0200

    wip

--- src/export/lean.ml
+++ src/export/lean.ml
@@ -116,19 +116,21 @@ and params oc ((ids,t,b) as x) =
       if !stt && t = None then List.iter (nonempty_param oc) ids
   | false, None -> param_ids oc ids
 
+and top_params oc ((ids,_,_) as x) =
+  params oc x; List.iter (nonempty_param oc) ids
+
 and nonempty_param oc id =
   string oc " [Nonempty "; param_id oc id; char oc ']'
 
-(* starts with a space if the list is not empty *)
 and params_list oc = List.iter (prefix " " params oc)
 
-(* starts with a space if the list is not empty *)
+and top_params_list oc = List.iter (prefix " " top_params oc)
+
 and params_list_in_abs oc l =
   match l with
   | [ids,t,false] -> char oc ' '; param_ids oc ids; typopt oc t
   | _ -> params_list oc l
 
-(* starts with a space if <> None *)
 and typopt oc t = Option.iter (prefix " : " term oc) t
 
 (** Translation of commands. *)
@@ -152,32 +154,23 @@ let command oc {elt; pos} =
   | P_symbol { p_sym_mod; p_sym_nam; p_sym_arg; p_sym_typ; p_sym_trm;
                p_sym_prf=_; p_sym_def } ->
       if not (StrSet.mem p_sym_nam.elt !erase) then
-        let p_sym_arg =
-          if !stt then
-            let pos = None in
-            (* Parameters with no type are assumed to be of type [Set]. *)
-            let _Set = {elt=P_Iden({elt=sym Set;pos},false);pos} in
-            List.map (function ids, None, b -> ids, Some _Set, b | x -> x)
-              p_sym_arg
-          else p_sym_arg
-        in
         begin match p_sym_def, p_sym_trm, p_sym_arg, p_sym_typ with
           | true, Some t, _, Some a when List.exists is_lem p_sym_mod ->
             string oc "theorem "; ident oc p_sym_nam;
-            params_list oc p_sym_arg; string oc " : "; term oc a;
+            top_params_list oc p_sym_arg; string oc " : "; term oc a;
             string oc " := by apply "; term oc t; string oc "\n"
           | true, Some t, _, _ ->
             if List.exists is_opaq p_sym_mod then string oc "opaque "
             else string oc "@[reducible]\nnoncomputable def ";
             ident oc p_sym_nam;
-            params_list oc p_sym_arg; typopt oc p_sym_typ;
+            top_params_list oc p_sym_arg; typopt oc p_sym_typ;
             string oc " := "; term oc t; string oc "\n"
           | false, _, [], Some t ->
             string oc "axiom "; ident oc p_sym_nam; string oc " : ";
             term oc t; string oc "\n"
           | false, _, _, Some t ->
             string oc "axiom "; ident oc p_sym_nam;
-            string oc " : ∀"; params_list oc p_sym_arg; string oc ", ";
+            string oc " : ∀"; top_params_list oc p_sym_arg; string oc ", ";
             term oc t; string oc "\n"
           | _ -> wrn pos "Command not translated."
         end
