1112705a4dd6744c4099a46ed88faaaff07ad6b5
Frédéric Blanqui <frederic.blanqui@inria.fr>
2026-06-29 14:42:41 +0200

    wip

--- src/cli/lambdapi.ml
+++ src/cli/lambdapi.ml
@@ -152,14 +152,17 @@ let export_cmd (cfg:Config.t) (output:output option) (encoding:string option)
       (requiring:string option) (no_implicits:bool) (use_notations:bool)
       (file:string) : unit =
   let run _ =
-    Config.init cfg (*{cfg with verbose = Some 0}*);
+    Config.init {cfg with verbose = Some 0};
     Export.Stt.use_implicits := not no_implicits;
     Export.Stt.use_notations := use_notations;
     Export.Stt.current_mp :=
       if Filename.extension file = lp_src_extension then
         begin
+          let t = Timed.Time.save() in
           Package.apply_config file;
-          List.tl (Library.path_of_file LpLexer.escape file)
+          let p = Library.path_of_file LpLexer.escape file in
+          Timed.Time.restore t;
+          List.tl p
         end
       else [Filename.basename (Filename.chop_extension file)];
     match output with
