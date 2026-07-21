3e61fc2ba04587861ad9bae95202bc284b5bec02
Frédéric Blanqui <frederic.blanqui@inria.fr>
2026-07-21 18:55:11 +0200

    factorize cloning code

--- editors/emacs/installAndLaunch.sh
+++ editors/emacs/installAndLaunch.sh
@@ -4,19 +4,13 @@ set -e
 NAME="$1" # lambdapi
 VERSION="$2" # defined in lambdapi-mode.el
 BIN="$3" # emacs binary, e.g. /snap/bin/emacs
-EGLOT_V="$4"
-MATH_SYMB_V="$5"
-HIGHLIGHT_V="$6"
-
-# For using the latest commit of a library just use "0". For instance: ./installAndLaunch.sh lambdapi-mode 1.1.0 /snap/bin/emacs 0 1.3 0
-
-# "./installAndLaunch.sh lambdapi-mode 1.1.0 /snap/bin/emacs 1.5 1.3 20210318.2248" does not work because eglot.1.5 is too old
-# "./installAndLaunch.sh lambdapi-mode 1.1.0 /snap/bin/emacs 1.18 1.3 20210318.2248" does not work because eglot stoped using tags in 2012
+EGLOT_V="$4" # "0" to use the latest version
+MATH_SYMB_V="$5" # "0" to use the latest version
+HIGHLIGHT_V="$6" # "0" to use the latest version
 
 convertVersionToCommitDate() {
   local input="$1"
   local date_part=${input%%.*}
-
   printf "%s-%s-%s %s:%s\n" \
     "${date_part:0:4}" \
     "${date_part:4:2}" \
@@ -41,46 +35,35 @@ cat <<'EOF' > ~/.emacs.d/init.el
 (add-to-list 'load-path (expand-file-name "~/.emacs.d/elpa/highlight/"))
 (require 'highlight)
 EOF
+
 echo "Creating ~/.emacs.d/elpa/ ..."
 mkdir -p ~/.emacs.d/elpa/
 
-echo "Cloning dependencies repos"
-if [ ! -d ~/.emacs.d/elpa/eglot ]; then
-  if [[ ${EGLOT_V} == "0" ]]; then  # ignore branch
-    git clone --depth 1 https://github.com/joaotavora/eglot.git ~/.emacs.d/elpa/eglot
-    EGLOT_V=1.9
-  else
-    git clone --depth 1 --branch ${EGLOT_V} https://github.com/joaotavora/eglot.git ~/.emacs.d/elpa/eglot
-  fi
-  echo "Eglot cloned to " ~/.emacs.d/elpa/eglot
-else
-  echo "Eglot is already cloned. Skipping"
-fi
+clone() {
+    branch=$1
+    url=$2
+    name=`basename $url .git`
+    dir=~/.emacs.d/elpa/eglot/$name
+    if [ -d $dir ]; then
+        echo "$name already cloned. Skipping."
+    else
+        if [[ $branch == "0" ]]; then
+            git clone --depth 1 $url $dir
+        else
+            git clone --depth 1 --branch $branch $url $dir
+        fi
+        echo "$name cloned to $dir."
+    fi
+}
 
-if [ ! -d ~/.emacs.d/elpa/math-symbol-lists ]; then
-  if [[ ${MATH_SYMB_V} == "0" ]]; then
-    git clone --depth 1 https://github.com/vspinu/math-symbol-lists.git ~/.emacs.d/elpa/math-symbol-lists
-    MATH_SYMB_V=1.3
-  else
-    git clone --depth 1 --branch v${MATH_SYMB_V} https://github.com/vspinu/math-symbol-lists.git ~/.emacs.d/elpa/math-symbol-lists
-  fi
-  echo "math-symbol-lists cloned to " ~/.emacs.d/elpa/math-symbol-lists
-else
-  echo "math-symbol-lists is already cloned. Skipping"
-fi
+clone $EGLOT_V https://github.com/joaotavora/eglot.git
+if [[ $EGLOT_V == "0" ]]; then EGLOT_V="1.9"; fi
 
-if [ ! -d ~/.emacs.d/elpa/highlight ]; then
-  commit_date=$(convertVersionToCommitDate ${HIGHLIGHT_V})
-  if [[ ${HIGHLIGHT_V} == "0" ]]; then
-    git clone https://github.com/emacsmirror/highlight.git ~/.emacs.d/elpa/highlight
-    HIGHLIGHT_V=20250815.1830
-  else
-    git -C ~/.emacs.d/elpa/highlight checkout $(git -C ~/.emacs.d/elpa/highlight rev-list -n 1 --after="${commit_date}" master)
-  fi
-  echo "highlight cloned to " ~/.emacs.d/elpa/highlight
-else
-  echo "Highlight is already cloned. Skipping"
-fi
+clone $MATH_SYMB_V https://github.com/vspinu/math-symbol-lists.git
+if [[ $MATH_SYMB_V == "0" ]]; then MATH_SYMB_V="1.3"; fi
+
+clone $HIGHLIGHT_V https://github.com/emacsmirror/highlight.git
+if [[ $HIGHLIGHT_V == "0" ]]; then HIGHLIGHT_V="20250815.1830"; fi
 
 echo "updating version in Elpa"
 echo "(define-package \"highlight\" \"${HIGHLIGHT_V}\")" > ~/.emacs.d/elpa/highlight/highlight-pkg.el
